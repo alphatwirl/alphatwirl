17f97c9a88c94918042bb20f56ace90631d7376c
Tai Sakuma <tai.sakuma@gmail.com>
2015-02-19 23:53:33 +0000

    update AlphaTwirl.Heppy.Heppy and its test

--- alphatwirl/Heppy.py
+++ alphatwirl/Heppy.py
@@ -4,31 +4,41 @@
 import os
 
 ##____________________________________________________________________________||
-class Components(object):
+class Heppy(object):
     def __init__(self, path):
         self.path = path
         excludeList = ('Chunks', 'failed')
-        self.names = [n for n in os.listdir(path) if os.path.isdir(path + '/' + n) and n not in excludeList]
+        self.componentNames = [n for n in os.listdir(path) if os.path.isdir(os.path.join(path, n)) and n not in excludeList]
 
-    def __getitem__(self, i):
-        return Component(self.path, self.names[i])
+        self._compDict = { }
+
+    def __getattr__(self, name):
+        if name not in self._compDict:
+            if name not in self.componentNames:
+                raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
+            compPath = os.path.join(self.path, name)
+            self._compDict[name] = Component(compPath)
+        return self._compDict[name]
+
+    def components(self):
+        return [getattr(self, n) for n in self.componentNames]
 
 ##____________________________________________________________________________||
 class Component(object):
-    def __init__(self, path, name):
-        self.path = path + '/' + name
-        self.name = name
-        self.analyzerNames = [n for n in os.listdir(self.path) if os.path.isdir(self.path + '/' + n)]
+    def __init__(self, path):
+        self.path = path
+        self.name = os.path.basename(path)
+        self.analyzerNames = [n for n in os.listdir(self.path) if os.path.isdir(os.path.join(self.path, n))]
 
     def __getattr__(self, name):
         if name not in self.analyzerNames:
-            raise AttributeError("no attribute '" + name + "'")
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, name))
         return Analyzer(self.path, name)
 
 ##____________________________________________________________________________||
 class Analyzer(object):
     def __init__(self, path, name):
-        self.path = path + '/' + name
+        self.path = os.path.join(path, name)
 
 ##____________________________________________________________________________||
 if __name__ == '__main__':
--- tests/test_Heppy.py
+++ tests/test_Heppy.py
@@ -1,11 +1,101 @@
 #!/usr/bin/env python
-import alphatwirl
+import os
+from alphatwirl import Heppy
 import unittest
+
+##____________________________________________________________________________||
+def mock_listdir(path):
+    heppyDir = 'dir/201522_SingleMu'
+    filesInHeppyDir = ['failed', 'QCD_HT_100To250', 'QCD_HT_250To500', 'TTJets', 'Chunks', 'versionInfo.txt']
+    if path == heppyDir:
+        return filesInHeppyDir
+
+    components = ['QCD_HT_100To250', 'QCD_HT_250To500', 'TTJets']
+    filesInComponent = ['PileUpAnalyzer', 'config.pck', 'config.txt', 'skimAnalyzerCount','treeProducerSusyAlphaT']
+    componentDirs = [os.path.join(heppyDir, c) for c in components]
+    if path in componentDirs:
+        return filesInComponent
+
+    return [ ]
+
+##____________________________________________________________________________||
+def mock_isdir(path):
+    heppyDir = 'dir/201522_SingleMu'
+    dirsInHeppy = ['failed', 'QCD_HT_100To250', 'QCD_HT_250To500', 'TTJets', 'Chunks']
+    dirsInHeppy = [os.path.join(heppyDir, d) for d in dirsInHeppy]
+    if path in dirsInHeppy: return True
+
+    components = ['QCD_HT_100To250', 'QCD_HT_250To500', 'TTJets']
+    componentDirs = [os.path.join(heppyDir, c) for c in components]
+    dirsInComponent = ['PileUpAnalyzer', 'skimAnalyzerCount','treeProducerSusyAlphaT']
+    dirsInComponent = [os.path.join(c, d) for c in componentDirs for d in dirsInComponent]
+    if path in dirsInComponent: return True
+    return False
+
+##____________________________________________________________________________||
+class TestHeppy(unittest.TestCase):
+
+    def setUp(self):
+        self.listdir_org = Heppy.os.listdir
+        self.isdir_org = Heppy.os.path.isdir
+        Heppy.os.listdir = mock_listdir
+        Heppy.os.path.isdir = mock_isdir
+
+        path = 'dir/201522_SingleMu'
+        self.heppy = Heppy.Heppy(path)
+
+    def tearDown(self):
+        Heppy.os.listdir = self.listdir_org
+        Heppy.os.path.isdir = self.isdir_org
+
+    def test_init(self):
+        self.assertEqual('dir/201522_SingleMu', self.heppy.path)
+
+    def test_componentNames(self):
+        expected = ['QCD_HT_100To250', 'QCD_HT_250To500', 'TTJets']
+        self.assertEqual(expected, self.heppy.componentNames)
+
+    def test_components_theSameObject(self):
+        comp1 = self.heppy.QCD_HT_100To250
+        comp2 = self.heppy.QCD_HT_100To250
+        self.assertIs(comp1, comp2)
+
+    def test_getattr(self):
+        self.assertRaises(AttributeError, self.heppy.__getattr__, 'WrongName')
+
+    def test_components(self):
+        expected = [self.heppy.QCD_HT_100To250, self.heppy.QCD_HT_250To500, self.heppy.TTJets]
+        self.assertEqual(expected, self.heppy.components())
+
+
+##____________________________________________________________________________||
+class TestComponent(unittest.TestCase):
+
+    def setUp(self):
+        self.listdir_org = Heppy.os.listdir
+        self.isdir_org = Heppy.os.path.isdir
+        Heppy.os.listdir = mock_listdir
+        Heppy.os.path.isdir = mock_isdir
+
+        path = 'dir/201522_SingleMu/QCD_HT_100To250'
+        self.component = Heppy.Component(path)
+
+    def tearDown(self):
+        Heppy.os.listdir = self.listdir_org
+        Heppy.os.path.isdir = self.isdir_org
+
+    def test_init(self):
+        self.assertEqual('dir/201522_SingleMu/QCD_HT_100To250', self.component.path)
+        self.assertEqual('QCD_HT_100To250', self.component.name)
+
+    def test_analyzerNames(self):
+        expected = ['PileUpAnalyzer', 'skimAnalyzerCount','treeProducerSusyAlphaT']
+        self.assertEqual(expected, self.component.analyzerNames)
 
 ##____________________________________________________________________________||
 class TestAnalyzer(unittest.TestCase):
 
     def test_init(self):
-        a = alphatwirl.Heppy.Analyzer("path", "name")
+        a = Heppy.Analyzer("path", "name")
 
 ##____________________________________________________________________________||
