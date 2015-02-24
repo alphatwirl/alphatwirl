79ad607edb8b09a86b176db2d1cb98a4fdaa60dc
Tai Sakuma <tai.sakuma@gmail.com>
2015-02-24 09:34:24 +0000

    add key composers

--- AlphaTwirl/Counter.py
+++ AlphaTwirl/Counter.py
@@ -28,39 +28,68 @@ class WeightCalculatorOne(object):
     def __call__(self, event):
         return 1.0
 
+
+##____________________________________________________________________________||
+class keyComposer_alphaT(object):
+    def __init__(self):
+        tbl_bin = pandas.read_table("tbl/tbl_bin_alphaT.txt", delim_whitespace=True)
+        self.binning = buildBinningFromTbl(tbl_bin)
+
+    def __call__(self, event):
+        alphaT = event.alphaT
+        alphaT_bin = self.binning(alphaT)
+        return (alphaT_bin, )
+
+##____________________________________________________________________________||
+class keyComposer_met(object):
+    def __init__(self):
+        tbl_bin = pandas.read_table("tbl/tbl_bin.txt", delim_whitespace=True)
+        self.binning = buildBinningFromTbl(tbl_bin)
+
+    def __call__(self, event):
+        met_pt = event.met_pt
+        met_bin = self.binning(met_pt)
+        return (met_bin, )
+
+##____________________________________________________________________________||
+class keyComposer_nvtx(object):
+    def __call__(self, event):
+        nVert = event.nVert
+        return (nVert, )
+
+##____________________________________________________________________________||
+class keyComposer_met_nvtx(object):
+    def __init__(self):
+        tbl_bin = pandas.read_table("tbl/tbl_bin.txt", delim_whitespace=True)
+        self.binning = buildBinningFromTbl(tbl_bin)
+
+    def __call__(self, event):
+        met_pt = event.met_pt
+        met_bin = self.binning(met_pt)
+        nVert = event.nVert
+        return (met_bin, nVert)
+
 ##____________________________________________________________________________||
 class Counter_alphaT(CounterBase):
     def __init__(self):
         CounterBase.__init__(self)
         self._countMethod = AlphaTwirl.Counts()
-        tbl_bin = pandas.read_table("tbl/tbl_bin_alphaT.txt", delim_whitespace=True)
-        self.binning = buildBinningFromTbl(tbl_bin)
 
         self._keynames = ('alphaT_bin', )
 
+        self._keyComposer = keyComposer_alphaT()
         self._weightCalculator = WeightCalculatorOne()
-
-    def _keyComposer(self, event):
-        alphaT = event.alphaT
-        alphaT_bin = self.binning(alphaT)
-        return (alphaT_bin, )
 
 ##____________________________________________________________________________||
 class Counter_met(CounterBase):
     def __init__(self):
         CounterBase.__init__(self)
         self._countMethod = AlphaTwirl.Counts()
-        tbl_bin = pandas.read_table("tbl/tbl_bin.txt", delim_whitespace=True)
-        self.binning = buildBinningFromTbl(tbl_bin)
 
         self._keynames = ('met_bin', )
 
+        self._keyComposer = keyComposer_met()
         self._weightCalculator = WeightCalculatorOne()
-
-    def _keyComposer(self, event):
-        met_pt = event.met_pt
-        met_bin = self.binning(met_pt)
-        return (met_bin, )
 
 ##____________________________________________________________________________||
 class Counter_nvtx(CounterBase):
@@ -70,28 +99,18 @@ class Counter_nvtx(CounterBase):
 
         self._keynames = ('nvtx', )
 
+        self._keyComposer = keyComposer_nvtx()
         self._weightCalculator = WeightCalculatorOne()
-
-    def _keyComposer(self, event):
-        nVert = event.nVert
-        return (nVert, )
 
 ##____________________________________________________________________________||
 class Counter_met_nvtx(CounterBase):
     def __init__(self):
         CounterBase.__init__(self)
         self._countMethod = AlphaTwirl.Counts()
-        tbl_bin = pandas.read_table("tbl/tbl_bin.txt", delim_whitespace=True)
-        self.binning = buildBinningFromTbl(tbl_bin)
 
         self._keynames = ('met_bin', 'nvtx', )
 
+        self._keyComposer = keyComposer_met_nvtx()
         self._weightCalculator = WeightCalculatorOne()
-
-    def _keyComposer(self, event):
-        met_pt = event.met_pt
-        met_bin = self.binning(met_pt)
-        nVert = event.nVert
-        return (met_bin, nVert)
 
 ##____________________________________________________________________________||
