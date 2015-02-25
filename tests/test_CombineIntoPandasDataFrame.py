2209f2c026859c41b8752afa532935b4541b29be
Tai Sakuma <tai.sakuma@gmail.com>
2015-02-25 13:43:10 +0000

    add TestCombineIntoPandasDataFrame

--- tests/test_CombineIntoPandasDataFrame.py
+++ tests/test_CombineIntoPandasDataFrame.py
@@ -9,6 +9,17 @@ def assertDataFrameEqual(df1, df2, **kwds):
     return assert_frame_equal(df1.sort(axis = 1), df2.sort(axis = 1), check_names = True)
 
 ##____________________________________________________________________________||
+class MockReader(object):
+    def __init__(self, results):
+        self._results = results
+
+    def keynames(self):
+        return ('v1', )
+
+    def results(self):
+        return self._results
+
+##____________________________________________________________________________||
 class TestCountsToDataFrame(unittest.TestCase):
     def setUp(self):
         self.addTypeEqualityFunc(pandas.core.frame.DataFrame, assertDataFrameEqual)
@@ -57,5 +68,65 @@ class TestCountsToDataFrame(unittest.TestCase):
         expected = pandas.DataFrame({'v1': [ ], 'n': [ ], 'nvar': [ ]})
         columns = ("v1", )
         self.assertEqual(expected, countsToDataFrame(counts, columns))
+
+
+##____________________________________________________________________________||
+class TestCombineIntoPandasDataFrame(unittest.TestCase):
+    def setUp(self):
+        self.addTypeEqualityFunc(pandas.core.frame.DataFrame, assertDataFrameEqual)
+
+    def test_combine_oreReader(self):
+
+        counts  = {
+            (1, ): {'n': 4.0, 'nvar': 6.0},
+            (2, ): {'n': 3.0, 'nvar': 9.0},
+            (3, ): {'n': 2.0, 'nvar': 3.0},
+            }
+
+        reader = MockReader(counts)
+        datasetReaderPairs = [('data1', reader), ]
+
+        expected = pandas.DataFrame(
+            {
+                'component': ['data1', 'data1', 'data1'],
+                'v1': [1, 2, 3],
+                'n': [4.0, 3.0, 2.0],
+                'nvar': [6.0, 9.0, 3.0],
+                }
+            )
+
+        combine = CombineIntoPandasDataFrame()
+        self.assertEqual(expected, combine.combine(datasetReaderPairs))
+
+    def test_combine_twoReaders(self):
+
+        counts1  = {
+            (1, ): {'n': 4.0, 'nvar': 6.0},
+            (2, ): {'n': 3.0, 'nvar': 9.0},
+            (3, ): {'n': 2.0, 'nvar': 3.0},
+            }
+
+        reader1 = MockReader(counts1)
+
+        counts2  = {
+            (2, ): {'n': 3.0, 'nvar': 6.0},
+            (4, ): {'n': 2.0, 'nvar': 2.0},
+            }
+
+        reader2 = MockReader(counts2)
+
+        datasetReaderPairs = [('data1', reader1), ('data2', reader2)]
+
+        expected = pandas.DataFrame(
+            {
+                'component': ['data1', 'data1', 'data1', 'data2', 'data2'],
+                'v1': [1, 2, 3, 2, 4],
+                'n': [4.0, 3.0, 2.0, 3.0, 2.0],
+                'nvar': [6.0, 9.0, 3.0, 6.0, 2.0],
+                }
+            )
+
+        combine = CombineIntoPandasDataFrame()
+        self.assertEqual(expected, combine.combine(datasetReaderPairs))
 
 ##____________________________________________________________________________||
