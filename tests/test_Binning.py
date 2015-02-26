d76302b402f2874f8e2b3d54c7a6b52eb519ca96
Tai Sakuma <tai.sakuma@gmail.com>
2015-02-26 14:18:24 +0000

    add a test TestRound

--- tests/test_Binning.py
+++ tests/test_Binning.py
@@ -1,4 +1,4 @@
-from AlphaTwirl.Binning import Binning, Echo
+from AlphaTwirl.Binning import Binning, Echo, Round
 import unittest
 
 ##____________________________________________________________________________||
@@ -62,6 +62,110 @@ class TestBinning(unittest.TestCase):
         lows = (10.0, 20.0, 30.0, 45.0)
         ups = (20.0, 30.0, 40.0, 50.0)
         self.assertRaises(ValueError, Binning, lows = lows, ups = ups)
+
+##____________________________________________________________________________||
+class TestRound(unittest.TestCase):
+
+    def test_boundary(self):
+        binning = Round()
+        self.assertEqual(0.5, binning.aBoundary)
+
+        binning = Round(2)
+        self.assertEqual(1, binning.aBoundary)
+
+        binning = Round(10)
+        self.assertEqual(5, binning.aBoundary)
+
+        binning = Round(2, 0)
+        self.assertEqual(0, binning.aBoundary)
+
+        binning = Round(2, -0.5)
+        self.assertEqual(1.5, binning.aBoundary)
+
+        binning = Round(2, 0.5)
+        self.assertEqual(0.5, binning.aBoundary)
+
+        binning = Round(2, 19.5)
+        self.assertEqual(1.5, binning.aBoundary)
+
+        binning = Round(2, 20.5)
+        self.assertEqual(0.5, binning.aBoundary)
+
+        binning = Round(2, -2.5)
+        self.assertEqual(1.5, binning.aBoundary)
+
+        binning = Round(2, -20.5)
+        self.assertEqual(1.5, binning.aBoundary)
+
+    def test_shift(self):
+        binning = Round(2)
+        self.assertEqual(0, binning.shift)
+
+        binning = Round(2, 0)
+        self.assertEqual(1, binning.shift)
+
+        binning = Round(2, 2)
+        self.assertEqual(1, binning.shift)
+
+        binning = Round(2, 20)
+        self.assertEqual(1, binning.shift)
+
+        binning = Round(2, -20)
+        self.assertEqual(1, binning.shift)
+
+        binning = Round(2, 0.5)
+        self.assertEqual(0.5, binning.shift)
+
+        binning = Round(2, 2.5)
+        self.assertEqual(0.5, binning.shift)
+
+        binning = Round(2, 4.5)
+        self.assertEqual(0.5, binning.shift)
+
+    def test_call(self):
+        binning = Round()
+        self.assertEqual(1, binning(0.5))
+        self.assertEqual(1, binning(1.4))
+        self.assertEqual(105, binning(104.5))
+        self.assertEqual(0, binning(-0.4))
+        self.assertEqual(-1, binning(-0.5))
+        self.assertEqual(-1, binning(-1.4))
+        self.assertEqual(-2, binning(-1.5))
+
+    def test_call_with_list(self):
+        binning = Round()
+        self.assertEqual([1, 1, 105, 0, -1, -1, -2], binning((0.5, 1.4, 104.5, -0.4, -0.5, -1.4, -1.5 )))
+
+    def test_call_width_2(self):
+        binning = Round(2)
+        self.assertEqual(1, binning.aBoundary)
+        self.assertEqual(-2, binning(-2.9))
+        self.assertEqual(-2, binning(-2))
+        self.assertEqual(-2, binning(-1.1))
+        self.assertEqual(0, binning(-0.9))
+        self.assertEqual(0, binning(0))
+        self.assertEqual(0, binning(0.9))
+        self.assertEqual(2, binning(1.1))
+        self.assertEqual(2, binning(2))
+        self.assertEqual(2, binning(2.9))
+
+    def test_call_width_2_aboundary_0(self):
+        binning = Round(2, 0)
+        self.assertEqual(0, binning.aBoundary)
+        self.assertEqual(-1, binning(-1.9))
+        self.assertEqual(-1, binning(-1))
+        self.assertEqual(-1, binning(-0.1))
+        self.assertEqual(1, binning(0.1))
+        self.assertEqual(1, binning(1))
+        self.assertEqual(1, binning(1.9))
+        self.assertEqual(3, binning(2.1))
+        self.assertEqual(3, binning(3))
+        self.assertEqual(3, binning(3.9))
+
+    def test_call_decimal_width(self):
+        binning = Round(0.02, 0.005)
+        self.assertEqual([0.015, 0.035, 0.075, -0.045], binning((0.006, 0.0251, 0.081, -0.048)))
+
 
 ##____________________________________________________________________________||
 class TestEcho(unittest.TestCase):
