02deb0b8a5cc30c4eb1a6ae58545df5c8398316e
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-01 18:00:22 +0100

    split Counter.py into two files

--- AlphaTwirl/Counter/Counter.py
+++ AlphaTwirl/Counter/Counter.py
@@ -1,5 +1,4 @@
 # Tai Sakuma <sakuma@fnal.gov>
-import AlphaTwirl
 import itertools
 
 ##____________________________________________________________________________||
@@ -73,37 +72,5 @@ class KeyMaxKeeper(object):
         ret = list(itertools.product(*newBins))
         ret.remove(self._keyMax)
         return ret
-
-##____________________________________________________________________________||
-class KeyComposer_SingleVariable(object):
-    def __init__(self, varName, binning):
-        self._varName = varName
-        self._binning = binning
-
-    def __call__(self, event):
-        var = getattr(event, self._varName)
-        var_bin = self._binning(var)
-        return (var_bin, )
-
-    def binnings(self):
-        return (self._binning, )
-
-##____________________________________________________________________________||
-class KeyComposer_TwoVariables(object):
-    def __init__(self, varName1, binning1, varName2, binning2):
-        self._varName1 = varName1
-        self._binning1 = binning1
-        self._varName2 = varName2
-        self._binning2 = binning2
-
-    def __call__(self, event):
-        var1 = getattr(event, self._varName1)
-        var1_bin = self._binning1(var1)
-        var2 = getattr(event, self._varName2)
-        var2_bin = self._binning2(var2)
-        return (var1_bin, var2_bin)
-
-    def binnings(self):
-        return (self._binning1, self._binning2)
 
 ##____________________________________________________________________________||
--- AlphaTwirl/Counter/__init__.py
+++ AlphaTwirl/Counter/__init__.py
@@ -1,1 +1,2 @@
 from Counter import *
+from KeyComposer import *
