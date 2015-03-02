03e9bfe8e6fc6e0abac85d32c09e9e8f6c851dd3
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-02 18:57:49 +0100

    add Echo.next()

--- tests/test_Binning.py
+++ tests/test_Binning.py
@@ -205,6 +205,9 @@ class TestRound(unittest.TestCase):
         self.assertEqual([-0.015, 0.005, 0.025, 0.045, 0.065], binning.next([-0.035, -0.015, 0.005, 0.025, 0.045]))
 
 ##____________________________________________________________________________||
+def plus2(val): return val + 2
+
+##____________________________________________________________________________||
 class TestEcho(unittest.TestCase):
     def test_call(self):
         binning = Echo()
@@ -217,5 +220,15 @@ class TestEcho(unittest.TestCase):
         binning = Echo()
         self.assertEqual([-5, 0, 1, 3, 10], binning([-5, 0, 1, 3, 10]))
         self.assertEqual([-5, 0, 1, 3, 10], binning((-5, 0, 1, 3, 10)))
+
+    def test_next(self):
+        binning = Echo()
+        self.assertEqual([-4, 1, 2, 4, 11], binning.next([-5, 0, 1, 3, 10]))
+
+        binning = Echo(nextFunc = plus2)
+        self.assertEqual([-3, 2, 3, 5, 12], binning.next([-5, 0, 1, 3, 10]))
+
+        binning = Echo(nextFunc = lambda x: x + 0.1)
+        self.assertEqual([-4.9, 0.1, 1.1, 3.1, 10.1], binning.next([-5, 0, 1, 3, 10]))
 
 ##____________________________________________________________________________||
