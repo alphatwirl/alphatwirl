af80e9dba5295f8514807e7cf816dc024a18878e
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-02 21:49:15 +0100

    split test_Counter.py into two files

--- tests/test_Counter.py
+++ tests/test_Counter.py
@@ -6,38 +6,15 @@ class MockEvent(object):
     pass
 
 ##____________________________________________________________________________||
-class MockBinning(object):
-    def __call__(self, val):
-        return val
-
-    def next(self, val):
-        return val + 1
-
-##____________________________________________________________________________||
 class MockCounts(object):
     def __init__(self):
         self._counts = [ ]
-        self._keys = [ ]
 
     def count(self, key, weight):
         self._counts.append((key, weight))
 
-    def addKeys(self, keys):
-        self._keys.append(keys)
-
     def results(self):
         return self._counts
-
-##____________________________________________________________________________||
-class MockKeyMaxKeeper(object):
-    def __init__(self):
-        self.keys = [ ]
-        self.updates = [ ]
-        pass
-
-    def update(self, key):
-        self.keys.append(key)
-        return self.updates.pop()
 
 ##____________________________________________________________________________||
 class MockWeightCalculator(object):
@@ -48,13 +25,9 @@ class MockWeightCalculator(object):
 class MockKeyComposer(object):
     def __init__(self, keys = [ ]):
         self.keys = keys
-        self.binning = MockBinning()
 
     def __call__(self, event):
         return self.keys.pop()
-
-    def binnings(self):
-        return (self.binning, )
 
 ##____________________________________________________________________________||
 class TestMockKeyComposer(unittest.TestCase):
@@ -89,40 +62,6 @@ class TestCounter(unittest.TestCase):
     def test_keynames(self):
         counter = Counter.Counter(('var', ), MockKeyComposer(), MockCounts(), MockWeightCalculator())
         self.assertEqual(('var', ), counter.keynames())
-
-##____________________________________________________________________________||
-class TestCountsWithEmptyKeysInGap(unittest.TestCase):
-
-    def test_count(self):
-        counts = MockCounts()
-        keys = [(14, ), (11, )]
-        keyMaxKeeper = MockKeyMaxKeeper()
-        keyMaxKeeper.updates = [[(11, ), (12, ), (13, ), (14, )], [()]]
-        countsWEKIG = Counter.CountsWithEmptyKeysInGap(counts, keyMaxKeeper)
-
-        countsWEKIG.count((11, ), 1)
-        self.assertEqual([(11,)], keyMaxKeeper.keys)
-        self.assertEqual([((11,), 1.0)], counts._counts)
-        self.assertEqual([[()]], counts._keys)
-
-        countsWEKIG.count((14, ), 1)
-        self.assertEqual([(11, ), (14, )], keyMaxKeeper.keys)
-        self.assertEqual([((11,), 1.0), ((14,), 1.0)], counts._counts)
-        self.assertEqual([[()], [(11, ), (12, ), (13, ), (14, )]], counts._keys)
-
-##____________________________________________________________________________||
-class TestCountsWithEmptyKeysInGapBulder(unittest.TestCase):
-
-    def test_call(self):
-        builder = Counter.CountsWithEmptyKeysInGapBulder(MockCounts, MockKeyMaxKeeper)
-        counts1 = builder()
-        counts2 = builder()
-        self.assertIsInstance(counts1, Counter.CountsWithEmptyKeysInGap)
-        self.assertIsInstance(counts1._countMethod, MockCounts)
-        self.assertIsInstance(counts1._keyMaxKeeper, MockKeyMaxKeeper)
-        self.assertIsNot(counts1, counts2)
-        self.assertIsNot(counts1._countMethod, counts2._countMethod)
-        self.assertIsNot(counts1._keyMaxKeeper, counts2._keyMaxKeeper)
 
 ##____________________________________________________________________________||
 class TestCounterBuilder(unittest.TestCase):
---
+++ tests/test_CountsWithEmptyKeysInGap.py
@@ -0,0 +1,61 @@
+import AlphaTwirl.Counter as Counter
+import unittest
+
+##____________________________________________________________________________||
+class MockCounts(object):
+    def __init__(self):
+        self._counts = [ ]
+        self._keys = [ ]
+
+    def count(self, key, weight):
+        self._counts.append((key, weight))
+
+    def addKeys(self, keys):
+        self._keys.append(keys)
+
+##____________________________________________________________________________||
+class MockKeyMaxKeeper(object):
+    def __init__(self):
+        self.keys = [ ]
+        self.updates = [ ]
+        pass
+
+    def update(self, key):
+        self.keys.append(key)
+        return self.updates.pop()
+
+##____________________________________________________________________________||
+class TestCountsWithEmptyKeysInGap(unittest.TestCase):
+
+    def test_count(self):
+        counts = MockCounts()
+        keys = [(14, ), (11, )]
+        keyMaxKeeper = MockKeyMaxKeeper()
+        keyMaxKeeper.updates = [[(11, ), (12, ), (13, ), (14, )], [()]]
+        countsWEKIG = Counter.CountsWithEmptyKeysInGap(counts, keyMaxKeeper)
+
+        countsWEKIG.count((11, ), 1)
+        self.assertEqual([(11,)], keyMaxKeeper.keys)
+        self.assertEqual([((11,), 1.0)], counts._counts)
+        self.assertEqual([[()]], counts._keys)
+
+        countsWEKIG.count((14, ), 1)
+        self.assertEqual([(11, ), (14, )], keyMaxKeeper.keys)
+        self.assertEqual([((11,), 1.0), ((14,), 1.0)], counts._counts)
+        self.assertEqual([[()], [(11, ), (12, ), (13, ), (14, )]], counts._keys)
+
+##____________________________________________________________________________||
+class TestCountsWithEmptyKeysInGapBulder(unittest.TestCase):
+
+    def test_call(self):
+        builder = Counter.CountsWithEmptyKeysInGapBulder(MockCounts, MockKeyMaxKeeper)
+        counts1 = builder()
+        counts2 = builder()
+        self.assertIsInstance(counts1, Counter.CountsWithEmptyKeysInGap)
+        self.assertIsInstance(counts1._countMethod, MockCounts)
+        self.assertIsInstance(counts1._keyMaxKeeper, MockKeyMaxKeeper)
+        self.assertIsNot(counts1, counts2)
+        self.assertIsNot(counts1._countMethod, counts2._countMethod)
+        self.assertIsNot(counts1._keyMaxKeeper, counts2._keyMaxKeeper)
+
+##____________________________________________________________________________||
