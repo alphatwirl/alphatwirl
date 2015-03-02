0d59f40130d5708ee8e07730085de387399dc74b
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-02 22:26:06 +0100

    add retvalue to the arguments of buildBinningFromTbl()

--- AlphaTwirl/buildBinningFromTbl.py
+++ AlphaTwirl/buildBinningFromTbl.py
@@ -3,8 +3,11 @@ import AlphaTwirl
 import pandas
 
 ##____________________________________________________________________________||
-def buildBinningFromTbl(path):
+def buildBinningFromTbl(path, retvalue = 'number'):
     tbl = pandas.read_table(path, delim_whitespace=True)
-    return AlphaTwirl.Binning.Binning(bins = tbl.bin.tolist(), lows = tbl.low.tolist(), ups = tbl.up.tolist())
+    if retvalue == 'number':
+        return AlphaTwirl.Binning.Binning(bins = tbl.bin.tolist(), lows = tbl.low.tolist(), ups = tbl.up.tolist(), retvalue = retvalue)
+    if retvalue == 'lowedge':
+        return AlphaTwirl.Binning.Binning(lows = tbl.low.tolist(), ups = tbl.up.tolist(), retvalue = retvalue)
 
 ##____________________________________________________________________________||
