af471c2e6e8de64f4b53fdb0041b238b9aa9c3fb
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-08 15:48:40 +0100

    add a new class RoundLog

--- AlphaTwirl/Binning.py
+++ AlphaTwirl/Binning.py
@@ -1,5 +1,6 @@
 # Tai Sakuma <sakuma@fnal.gov>
 import decimal
+import math
 
 ##____________________________________________________________________________||
 class Binning(object):
@@ -127,6 +128,27 @@ class Round(object):
         theBin = self._callImpDecimal(bin)
         theNextBin = theBin + self.width
         return float(theNextBin)
+
+##____________________________________________________________________________||
+class RoundLog(object):
+    def __init__(self, width = 0.1, aBoundary = 0, retvalue = 'center'):
+        self._round = Round(width = width, aBoundary = aBoundary, retvalue = retvalue)
+
+    def __call__(self, val):
+        try:
+            return [self.__call__(v) for v in val]
+        except TypeError:
+            pass
+        val = math.log10(val)
+        return 10**self._round(val)
+
+    def next(self, bin):
+        try:
+            return [self.next(v) for v in bin]
+        except TypeError:
+            pass
+        bin = math.log10(bin)
+        return 10**self._round.next(bin)
 
 ##____________________________________________________________________________||
 class Echo(object):
--- tests/test_Binning.py
+++ tests/test_Binning.py
@@ -1,4 +1,4 @@
-from AlphaTwirl.Binning import Binning, Echo, Round
+from AlphaTwirl.Binning import Binning, Echo, Round, RoundLog
 import unittest
 
 ##____________________________________________________________________________||
@@ -202,6 +202,34 @@ class TestRound(unittest.TestCase):
 
         binning = Round(0.02, 0.005, retvalue = 'lowedge')
         self.assertEqual([-0.015, 0.005, 0.025, 0.045, 0.065], binning.next([-0.035, -0.015, 0.005, 0.025, 0.045]))
+
+##____________________________________________________________________________||
+class TestRoundLog(unittest.TestCase):
+
+    def test_init(self):
+        RoundLog(retvalue = 'center')
+        RoundLog(retvalue = 'lowedge')
+        self.assertRaises(ValueError, RoundLog, retvalue = 'yyy')
+
+    def test_call(self):
+        binning = RoundLog()
+        self.assertEqual([2.2387211385683394, 22.3872113856834, 223.872113856834], binning((2, 20, 200)))
+
+    def test_call_lowedge(self):
+        binning = RoundLog(retvalue = 'lowedge')
+        self.assertEqual([1.9952623149688795, 19.952623149688797, 199.52623149688787], binning((2, 20, 200)))
+
+    def test_next(self):
+        binning = RoundLog()
+        self.assertEqual([2.8183829312644537, 28.183829312644534, 281.8382931264455], binning.next([2.2387211385683394, 22.3872113856834, 223.872113856834]))
+
+    def test_call_zero(self):
+        binning = RoundLog()
+        self.assertRaises(ValueError, binning, 0)
+
+    def test_call_negative(self):
+        binning = RoundLog()
+        self.assertRaises(ValueError, binning, -1)
 
 ##____________________________________________________________________________||
 def plus2(val): return val + 2
