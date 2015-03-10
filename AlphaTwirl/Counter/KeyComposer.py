bafca8598872156328598f5be0b0a1596fb0c5d5
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-10 11:56:49 +0100

    remove KeyComposer_SingleVariable and KeyComposer_TwoVariables

--- AlphaTwirl/Counter/KeyComposer.py
+++ AlphaTwirl/Counter/KeyComposer.py
@@ -19,25 +19,3 @@ class GenericKeyComposer(object):
         return self._binnings
 
 ##____________________________________________________________________________||
-class KeyComposer_SingleVariable(object):
-    def __init__(self, varName, binning):
-        self._composer = GenericKeyComposer((varName, ), (binning, ))
-
-    def __call__(self, event):
-        return self._composer(event)
-
-    def binnings(self):
-        return self._composer.binnings()
-
-##____________________________________________________________________________||
-class KeyComposer_TwoVariables(object):
-    def __init__(self, varName1, binning1, varName2, binning2):
-        self._composer = GenericKeyComposer((varName1, varName2), (binning1, binning2))
-
-    def __call__(self, event):
-        return self._composer(event)
-
-    def binnings(self):
-        return self._composer.binnings()
-
-##____________________________________________________________________________||
--- tests/Counter/test_KeyComposer.py
+++ tests/Counter/test_KeyComposer.py
@@ -22,48 +22,40 @@ class MockBinningNone(object):
         return val + 1
 
 ##____________________________________________________________________________||
-class TestKeyComposer_SingleVariable(unittest.TestCase):
+class TestGenericKeyComposer(unittest.TestCase):
 
-    def test_call(self):
-        keyComposer = Counter.KeyComposer_SingleVariable('var1', MockBinningEcho())
+    def test_call_1var(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningEcho(), ))
 
         event = MockEvent()
         event.var1 = 12
         self.assertEqual((12, ), keyComposer(event))
 
-    def test_None_key(self):
-        keyComposer = Counter.KeyComposer_SingleVariable('var1', MockBinningNone())
-
-        event = MockEvent()
-        event.var1 = 12
-        self.assertIsNone(keyComposer(event))
-
-    def test_binnings(self):
-        binning1 = MockBinningEcho()
-        keyComposer = Counter.KeyComposer_SingleVariable('var1', binning1)
-        self.assertEqual((binning1, ), keyComposer.binnings())
-
-##____________________________________________________________________________||
-class TestKeyComposer_TwoVariables(unittest.TestCase):
-
-    def test_call(self):
-        keyComposer = Counter.KeyComposer_TwoVariables('var1', MockBinningEcho(), 'var2', MockBinningEcho())
+    def test_call_2vars(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningEcho(), MockBinningEcho()))
 
         event = MockEvent()
         event.var1 = 15
         event.var2 = 22
         self.assertEqual((15, 22), keyComposer(event))
 
-    def test_None_key_var1(self):
-        keyComposer = Counter.KeyComposer_TwoVariables('var1', MockBinningNone(), 'var2', MockBinningEcho())
+    def test_None_key_1var(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningNone(), ))
+
+        event = MockEvent()
+        event.var1 = 12
+        self.assertIsNone(keyComposer(event))
+
+    def test_None_key_2vars_var1(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningNone(), MockBinningEcho()))
 
         event = MockEvent()
         event.var1 = 15
         event.var2 = 22
         self.assertIsNone(keyComposer(event))
 
-    def test_None_key_var2(self):
-        keyComposer = Counter.KeyComposer_TwoVariables('var1', MockBinningEcho(), 'var2', MockBinningNone())
+    def test_None_key_2vars_var2(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningEcho(), MockBinningNone()))
 
         event = MockEvent()
         event.var1 = 15
@@ -72,8 +64,12 @@ class TestKeyComposer_TwoVariables(unittest.TestCase):
 
     def test_binnings(self):
         binning1 = MockBinningEcho()
+        keyComposer = Counter.GenericKeyComposer(('var1', ), (binning1, ))
+        self.assertEqual((binning1, ), keyComposer.binnings())
+
+        binning1 = MockBinningEcho()
         binning2 = MockBinningEcho()
-        keyComposer = Counter.KeyComposer_TwoVariables('var1', binning1, 'var2', binning2)
+        keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (binning1, binning2))
         self.assertEqual((binning1, binning2), keyComposer.binnings())
 
 ##____________________________________________________________________________||
