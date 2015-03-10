7ec34affaf9386530ada3548b687d16427c493b7
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-10 14:26:27 +0100

    add to support indices in GenericKeyComposer

--- AlphaTwirl/Counter/KeyComposer.py
+++ AlphaTwirl/Counter/KeyComposer.py
@@ -2,14 +2,18 @@
 
 ##____________________________________________________________________________||
 class GenericKeyComposer(object):
-    def __init__(self, varNames, binnings):
+    def __init__(self, varNames, binnings, indices = None):
         self._varNames = varNames
         self._binnings = binnings
+        self._indices = indices if indices is not None else [None]*len(self._varNames)
 
     def __call__(self, event):
         ret = [ ]
-        for varName, binning in zip(self._varNames, self._binnings):
+        for varName, binning, index in zip(self._varNames, self._binnings, self._indices):
             var = getattr(event, varName)
+            if index is not None:
+                if len(var) <= index: return None
+                var = var[index]
             var_bin = binning(var)
             if var_bin is None: return None
             ret.append(var_bin)
--- tests/Counter/test_KeyComposer.py
+++ tests/Counter/test_KeyComposer.py
@@ -72,4 +72,27 @@ class TestGenericKeyComposer(unittest.TestCase):
         keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (binning1, binning2))
         self.assertEqual((binning1, binning2), keyComposer.binnings())
 
+    def test_indices_1(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningEcho(), ), indices = (0, ))
+        event = MockEvent()
+        event.var1 = (12, )
+        self.assertEqual((12, ), keyComposer(event))
+
+    def test_indices_2(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningEcho(), ), indices = (1, ))
+        event = MockEvent()
+        event.var1 = (12, 8, 6)
+        self.assertEqual((8, ), keyComposer(event))
+
+        event.var1 = (3, )
+        self.assertIsNone(keyComposer(event))
+
+    def test_indices_3(self):
+        keyComposer = Counter.GenericKeyComposer(('var1', 'var2', 'var3'), (MockBinningEcho(), MockBinningEcho(), MockBinningEcho()), indices = (1, None, 2))
+        event = MockEvent()
+        event.var1 = (12, 8, 6)
+        event.var2 = 232
+        event.var3 = (111, 222, 333)
+        self.assertEqual((8, 232, 333), keyComposer(event))
+
 ##____________________________________________________________________________||
