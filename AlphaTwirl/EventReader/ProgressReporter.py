d8c28d358aca1361f32d524741d231bfdad8dcba
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-14 20:27:37 +0100

    add new mothods to ProgressReporter to improve testability

--- AlphaTwirl/EventReader/ProgressReporter.py
+++ AlphaTwirl/EventReader/ProgressReporter.py
@@ -9,21 +9,24 @@ class ProgressReporter(object):
     def __init__(self, queue, pernevents = 1000):
         self.queue = queue
         self.pernevents = pernevents
-        self.lastReportTime = time.time()
         self.interval = 0.02 # [second]
+        self._readTime()
 
     def report(self, event, component):
         if not self.needToReport(event, component): return
         done = event.iEvent + 1
         report = ProgressReport(name = component.name, done = done, total = event.nEvents)
         self.queue.put(report)
-        self.lastReportTime = time.time()
+        self._readTime()
 
     def needToReport(self, event, component):
         iEvent = event.iEvent + 1 # add 1 because event.iEvent starts from 0
-        if time.time() - self.lastReportTime > self.interval: return True
+        if self._time() - self.lastTime > self.interval: return True
         if iEvent % self.pernevents == 0: return True
         if iEvent == event.nEvents: return True
         return False
+
+    def _time(self): return time.time()
+    def _readTime(self): self.lastTime = self._time()
 
 ##____________________________________________________________________________||
