025064a44d0858071d59afad7470d8f7d3b3bc41
Tai Sakuma <tai.sakuma@gmail.com>
2015-03-27 12:23:24 +0100

    split a test into four in TestEventReaderEventReaderBundle

--- tests/EventReader/test_EventReaderBundle.py
+++ tests/EventReader/test_EventReaderBundle.py
@@ -7,6 +7,9 @@ class MockEventBuilder(object):
         return component._events
 
 ##____________________________________________________________________________||
+class MockEventSelection(object): pass
+
+##____________________________________________________________________________||
 class MockReader(object):
     def __init__(self, name):
         self.name = name
@@ -52,51 +55,75 @@ class MockEventLoopRunner(object):
 ##____________________________________________________________________________||
 class TestEventReaderEventReaderBundle(unittest.TestCase):
 
-    def setUp(self):
-        self.eventBuilder = MockEventBuilder()
-        self.eventLoopRunner = MockEventLoopRunner()
-        self.bundle = EventReaderBundle(self.eventBuilder, self.eventLoopRunner)
+    def test_eventBuilder_passed_to_EventLoop(self):
+        eventBuilder = MockEventBuilder()
+        eventLoopRunner = MockEventLoopRunner()
+        bundle = EventReaderBundle(eventBuilder, eventLoopRunner)
 
-    def test_begin_read_end(self):
+        component1 = MockComponent()
+        bundle.read(component1)
+        self.assertIs(eventBuilder, eventLoopRunner.eventLoop.eventBuilder)
+
+    def test_eventSelection_passed_to_EventLoop(self):
+        eventBuilder = MockEventBuilder()
+        eventLoopRunner = MockEventLoopRunner()
+        eventSelection = MockEventSelection()
+        bundle = EventReaderBundle(eventBuilder, eventLoopRunner, eventSelection)
+
+        component1 = MockComponent()
+        bundle.read(component1)
+        self.assertIs(eventSelection, eventLoopRunner.eventLoop.eventSelection)
+
+    def test_eventLoopRunner_called(self):
+        eventBuilder = MockEventBuilder()
+        eventLoopRunner = MockEventLoopRunner()
+        bundle = EventReaderBundle(eventBuilder, eventLoopRunner)
+
+        self.assertFalse(eventLoopRunner.began)
+        bundle.begin()
+        self.assertTrue(eventLoopRunner.began)
+
+        self.assertIsNone(eventLoopRunner.eventLoop)
+        component1 = MockComponent()
+        bundle.read(component1)
+        self.assertIs(component1, eventLoopRunner.eventLoop.component)
+        self.assertIsInstance(eventLoopRunner.eventLoop, EventLoop)
+
+        self.assertFalse(eventLoopRunner.ended)
+        bundle.end()
+        self.assertTrue(eventLoopRunner.ended)
+
+
+    def test_packages_read_and_collected(self):
+        eventBuilder = MockEventBuilder()
+        eventLoopRunner = MockEventLoopRunner()
+        bundle = EventReaderBundle(eventBuilder, eventLoopRunner)
 
         package1 = MockReaderPackage()
-        self.bundle.addReaderPackage(package1)
+        bundle.addReaderPackage(package1)
 
         package2 = MockReaderPackage()
-        self.bundle.addReaderPackage(package2)
+        bundle.addReaderPackage(package2)
 
-        self.assertFalse(self.eventLoopRunner.began)
-        self.bundle.begin()
-        self.assertTrue(self.eventLoopRunner.began)
+        bundle.begin()
 
         component1 = MockComponent()
         component1.name = "compName1"
-
-        self.assertIsNone(self.eventLoopRunner.eventLoop)
-        self.bundle.read(component1)
-
-        self.assertIsInstance(self.eventLoopRunner.eventLoop, EventLoop)
-        self.assertIs(self.eventBuilder, self.eventLoopRunner.eventLoop.eventBuilder)
-        self.assertIs(component1, self.eventLoopRunner.eventLoop.component)
-        self.assertIs("compName1", self.eventLoopRunner.eventLoop.component.name)
-        self.assertEqual([package1.reader, package2.reader], self.eventLoopRunner.eventLoop.readers)
+        bundle.read(component1)
+        self.assertIs("compName1", eventLoopRunner.eventLoop.component.name)
+        self.assertEqual([package1.reader, package2.reader],eventLoopRunner.eventLoop.readers)
 
         component2 = MockComponent()
         component2.name = "compName2"
-        self.bundle.read(component2)
+        bundle.read(component2)
+        self.assertIs("compName2", eventLoopRunner.eventLoop.component.name)
+        self.assertEqual([package1.reader, package2.reader],eventLoopRunner.eventLoop.readers)
 
-        self.assertIsInstance(self.eventLoopRunner.eventLoop, EventLoop)
-        self.assertIs(self.eventBuilder, self.eventLoopRunner.eventLoop.eventBuilder)
-        self.assertIs(component2, self.eventLoopRunner.eventLoop.component)
-        self.assertIs("compName2", self.eventLoopRunner.eventLoop.component.name)
-        self.assertEqual([package1.reader, package2.reader], self.eventLoopRunner.eventLoop.readers)
-
-        self.assertFalse(self.eventLoopRunner.ended)
         self.assertFalse(package1.collected)
         self.assertFalse(package2.collected)
-        self.bundle.end()
-        self.assertTrue(self.eventLoopRunner.ended)
+        bundle.end()
         self.assertTrue(package1.collected)
         self.assertTrue(package2.collected)
+
 
 ##____________________________________________________________________________||
