190e47ef616a892951662a2d20aa466ae8f44816
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-08 14:34:17 +0200

    make TblNevt use HeppyResult.ReadCounter, add a column to the output

--- AlphaTwirl/TblNevt.py
+++ AlphaTwirl/TblNevt.py
@@ -7,34 +7,32 @@ from HeppyResult import ReadCounter
 class TblNevt(object):
     def __init__(self, outPath):
         self._outPath = outPath
-        self._tbl = pandas.DataFrame(columns = ('component', 'nevt'))
-
-        self._getNEventsFor = getNEventsFor
+        self._tbl = pandas.DataFrame()
+        self.levels = ('All Events', 'Sum Weights')
+        self.columnNames = ('nevt', 'nevt_sumw')
+        self._readCounter = ReadCounter()
 
     def begin(self): pass
 
     def read(self, component):
-        nevt = self._getNEventsFor(component)
-        self._tbl = self._tbl.append(pandas.DataFrame({'component': (component.name, ), 'nevt': (nevt, )}))
+        path = os.path.join(component.skimAnalyzerCount.path, 'SkimReport.txt')
+        counter = self._readCounter(path)
+        df_data = {'component': (component.name, )}
+        for level, column in zip(self.levels, self.columnNames):
+            df_data[column] = (counter[level]['count'], )
+        df = pandas.DataFrame(df_data)
+        self._tbl = self._tbl.append(df)
 
     def end(self):
         f = self._open(self._outPath)
         if len(self._tbl.index) == 0:
-            f.write(" ".join([i for i in self._tbl.columns]) + "\n")
+            f.write('component ' + " ".join(self.columnNames) + '\n')
         else:
-            self._tbl.nevt = self._tbl.nevt.apply(lambda x: '%.3f' % x)
-            self._tbl.to_string(f, index = False)
+            self._tbl.to_string(f, index = False, float_format = '{:.3f}'.format)
             f.write('\n')
         self._close(f)
 
     def _open(self, path): return open(path, 'w')
     def _close(self, file): file.close()
-
-##____________________________________________________________________________||
-def getNEventsFor(component):
-    readCounter = ReadCounter()
-    path = os.path.join(component.skimAnalyzerCount.path, 'SkimReport.txt')
-    counter = readCounter(path)
-    return counter['Sum Weights']['count']
 
 ##____________________________________________________________________________||
--- tests/test_TblNevt.py
+++ tests/test_TblNevt.py
@@ -1,6 +1,7 @@
 from AlphaTwirl import TblNevt
 import unittest
 import cStringIO
+import collections
 
 ##____________________________________________________________________________||
 class MockOpen(object):
@@ -11,15 +12,16 @@ class MockOpen(object):
 def mockClose(file): pass
 
 ##____________________________________________________________________________||
-class MockComponent(object):
-    def __init__(self, name, nevt):
-        self.name = name
-        self.nevt = nevt
+class MockAnalyzer(object):
+    def __init__(self, path): self.path = path
 
 ##____________________________________________________________________________||
-class MockGetNEventsFor(object):
-    def __init__(self): self._nevt = 0
-    def __call__(self, component): return component.nevt
+class MockComponent(object):
+    def __init__(self, name): self.name = name
+
+##____________________________________________________________________________||
+class MockReadCounter(object):
+    def __call__(self, path): return self.counter
 
 ##____________________________________________________________________________||
 class TestTblNevt(unittest.TestCase):
@@ -31,31 +33,41 @@ class TestTblNevt(unittest.TestCase):
         tblnevt._open = MockOpen(out)
         tblnevt._close = mockClose
 
-        getNEventsFor = MockGetNEventsFor()
-        tblnevt._getNEventsFor = getNEventsFor
+        readCounter = MockReadCounter()
+        tblnevt._readCounter = readCounter
 
         tblnevt.begin()
 
-        component = MockComponent("QCD_HT_100To250", 4123612)
+        component = MockComponent("QCD_HT_100To250")
+        component.skimAnalyzerCount = MockAnalyzer('path/to/analyzer')
+        readCounter.counter = collections.OrderedDict([('All Events', {'count': 4123612, 'eff2': 1.0, 'eff1': 1.0}), ('Sum Weights', {'count': 4123612.0, 'eff2': 1.0, 'eff1': 1.0})])
         tblnevt.read(component)
 
-        component = MockComponent("TTJets", 25446993)
+        component = MockComponent("TTJets")
+        component.skimAnalyzerCount = MockAnalyzer('path/to/analyzer')
+        readCounter.counter = collections.OrderedDict([('All Events', {'count': 25446993, 'eff2': 1.0, 'eff1': 1.0}), ('Sum Weights', {'count': 25446993.0, 'eff2': 1.0, 'eff1': 1.0})])
         tblnevt.read(component)
 
-        component = MockComponent("TBarToLeptons_sch", 320855.887262)
+        component = MockComponent("TBarToLeptons_sch")
+        component.skimAnalyzerCount = MockAnalyzer('path/to/analyzer')
+        readCounter.counter = collections.OrderedDict([('All Events', {'count': 250000, 'eff2': 1.0, 'eff1': 1.0}), ('Sum Weights', {'count': 320855.887262, 'eff2': 1.2834, 'eff1': 1.28})])
         tblnevt.read(component)
 
-        component = MockComponent("TBarToLeptons_tch", 50734279.1235)
+        component = MockComponent("TBarToLeptons_tch")
+
+        component.skimAnalyzerCount = MockAnalyzer('path/to/analyzer')
+        readCounter.counter = collections.OrderedDict([('All Events', {'count': 1999800, 'eff2': 1.0, 'eff1': 1.0}), ('Sum Weights', {'count': 50734279.1235, 'eff2': 25.3697, 'eff1': 25.37})])
         tblnevt.read(component)
 
         tblnevt.end()
 
         expected = '\n'.join([
-            '         component          nevt',
-            '   QCD_HT_100To250   4123612.000',
-            '            TTJets  25446993.000',
-            ' TBarToLeptons_sch    320855.887',
-            ' TBarToLeptons_tch  50734279.123']) + '\n'
+            '         component      nevt    nevt_sumw',
+            '   QCD_HT_100To250   4123612  4123612.000',
+            '            TTJets  25446993 25446993.000',
+            ' TBarToLeptons_sch    250000   320855.887',
+            ' TBarToLeptons_tch   1999800 50734279.123']) + '\n'
+
         self.assertEqual(expected, out.getvalue())
 
 
@@ -66,13 +78,10 @@ class TestTblNevt(unittest.TestCase):
         tblnevt._open = MockOpen(out)
         tblnevt._close = mockClose
 
-        getNEventsFor = MockGetNEventsFor()
-        tblnevt._getNEventsFor = getNEventsFor
-
         tblnevt.begin()
         tblnevt.end()
 
-        expected = 'component nevt\n'
+        expected = 'component nevt nevt_sumw\n'
         self.assertEqual(expected, out.getvalue())
 
 
