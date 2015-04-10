3cfd28389c7c4319532569d9920be41b520b4360
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-10 15:24:30 +0200

    add a try clause in case pandas is not available

--- AlphaTwirl/__init__.py
+++ AlphaTwirl/__init__.py
@@ -4,11 +4,15 @@ from HeppyResultReader import HeppyResultReader
 from Events import Events
 import Binning
 import Counter
-from CombineIntoPandasDataFrame import CombineIntoPandasDataFrame, countsToDataFrame
-from WritePandasDataFrameToFile import WritePandasDataFrameToFile
 import EventReader
 import ProgressBar
 from EventBuilder import EventBuilder
-from buildBinningFromTbl import buildBinningFromTbl
 from TblXsec import TblXsec
 from TblNevt import TblNevt
+
+try:
+    from CombineIntoPandasDataFrame import CombineIntoPandasDataFrame, countsToDataFrame
+    from WritePandasDataFrameToFile import WritePandasDataFrameToFile
+    from buildBinningFromTbl import buildBinningFromTbl
+except ImportError:
+    pass
