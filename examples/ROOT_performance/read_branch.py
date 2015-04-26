69cd26dd6deddbe949f4ab7441bd0e6e7cc3f3ac
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-26 15:31:32 +0200

    add example files in examples/ROOT_performance/

---
+++ examples/ROOT_performance/read_branch.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Tai Sakuma <tai.sakuma@cern.ch>
+
+##__________________________________________________________________||
+import os, sys
+import timeit
+import array
+import ROOT
+
+##__________________________________________________________________||
+inputPath = '/Users/sakuma/work/cms/c150130_RA1_data/c150130_01_PHYS14/20150331_SingleMu/TTJets/treeProducerSusyAlphaT/tree.root'
+treeName = 'tree'
+
+##__________________________________________________________________||
+def simplest_way():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        tree.met_pt
+
+##__________________________________________________________________||
+def use_SetBranchStatus():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("met_pt", 1)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        tree.met_pt
+
+##__________________________________________________________________||
+def use_GetLeaf():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("met_pt", 1)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        tree.GetLeaf('met_pt').GetValue()
+
+##__________________________________________________________________||
+def use_SetBranchAddress():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("met_pt", 1)
+    met_pt = array.array('d', [ 0 ])
+    tree.SetBranchAddress("met_pt" , met_pt)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        met_pt[0]
+
+##__________________________________________________________________||
+ways = ['simplest_way', 'use_SetBranchStatus', 'use_GetLeaf', 'use_SetBranchAddress']
+
+for w in ways:
+    print w, ':',
+    print timeit.timeit(w + '()', number = 1, setup = 'from __main__ import ' + w)
+
+##__________________________________________________________________||
---
+++ examples/ROOT_performance/read_branch_array.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Tai Sakuma <tai.sakuma@cern.ch>
+
+##__________________________________________________________________||
+import os, sys
+import timeit
+import array
+import ROOT
+
+##__________________________________________________________________||
+inputPath = '/Users/sakuma/work/cms/c150130_RA1_data/c150130_01_PHYS14/20150331_SingleMu/TTJets/treeProducerSusyAlphaT/tree.root'
+treeName = 'tree'
+
+##__________________________________________________________________||
+def simplest_way():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        v = tree.jet_pt
+        for i in range(len(v)):
+            v[i]
+
+##__________________________________________________________________||
+def use_SetBranchStatus():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("jet_pt", 1)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        v = tree.jet_pt
+        for i in range(len(v)):
+            v[i]
+
+##__________________________________________________________________||
+def use_GetLeaf():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("jet_pt", 1)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        l = tree.GetLeaf('jet_pt')
+        for i in range(l.GetNdata()):
+            l.GetValue(i)
+
+##__________________________________________________________________||
+def use_SetBranchAddress():
+    inputFile = ROOT.TFile.Open(inputPath)
+    tree = inputFile.Get(treeName)
+    tree.SetBranchStatus("*", 0)
+    tree.SetBranchStatus("njet", 1)
+    tree.SetBranchStatus("jet_pt", 1)
+    maxn = 65536
+    njet = array.array('i',[ 0 ])
+    jet_pt = array.array('d', maxn*[ 0 ])
+    tree.SetBranchAddress("njet" , njet)
+    tree.SetBranchAddress("jet_pt" , jet_pt)
+    for i in xrange(tree.GetEntries()):
+        if tree.GetEntry(i) <= 0: break
+        for i in range(njet[0]):
+            jet_pt[i]
+
+##__________________________________________________________________||
+ways = ['simplest_way', 'use_SetBranchStatus', 'use_GetLeaf', 'use_SetBranchAddress']
+
+for w in ways:
+    print w, ':',
+    print timeit.timeit(w + '()', number = 1, setup = 'from __main__ import ' + w)
+
+##__________________________________________________________________||
