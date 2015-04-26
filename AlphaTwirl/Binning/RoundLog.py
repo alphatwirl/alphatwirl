127ef5453ca8b57a274923fd94f13c3a9e50d4cd
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-26 11:48:53 +0200

    stop supporting lists in RoundLog as hasattr is slow

--- AlphaTwirl/Binning/RoundLog.py
+++ AlphaTwirl/Binning/RoundLog.py
@@ -9,24 +9,15 @@ def returnTrue(x): return True
 class RoundLog(object):
     def __init__(self, width = 0.1, aBoundary = 0, retvalue = 'lowedge', valid = returnTrue):
         self._round = Round(width = width, aBoundary = aBoundary, retvalue = retvalue)
-
         self.valid = valid
 
     def __call__(self, val):
-
-        if hasattr(val, "__iter__"):
-            return [self.__call__(v) for v in val]
-
         if not self.valid(val): return None
         if val <= 0: return None
         val = math.log10(val)
         return 10**self._round(val)
 
     def next(self, bin):
-
-        if hasattr(bin, "__iter__"):
-            return [self.next(v) for v in bin]
-
         bin = math.log10(bin)
         return 10**self._round.next(bin)
 
--- tests/unit/Binning/test_RoundLog.py
+++ tests/unit/Binning/test_RoundLog.py
@@ -11,27 +11,21 @@ class TestRoundLog(unittest.TestCase):
 
     def test_call(self):
         binning = RoundLog()
-        expected = [1.9952623149688795, 19.952623149688797, 199.52623149688787]
-        actual = binning((2, 20, 200))
-        self.assertEqual(len(expected), len(actual))
-        for e, a in zip(expected, actual):
-            self.assertAlmostEqual(e, a)
+        self.assertAlmostEqual( 1.9952623149688, binning(   2))
+        self.assertAlmostEqual( 19.952623149688, binning(  20))
+        self.assertAlmostEqual( 199.52623149688, binning( 200))
 
     def test_call_center(self):
         binning = RoundLog(retvalue = 'center')
-        expected = [2.2387211385683394, 22.3872113856834, 223.872113856834]
-        actual = binning((2, 20, 200))
-        self.assertEqual(len(expected), len(actual))
-        for e, a in zip(expected, actual):
-            self.assertAlmostEqual(e, a)
+        self.assertAlmostEqual( 2.23872113856834, binning(   2))
+        self.assertAlmostEqual( 22.3872113856834, binning(  20))
+        self.assertAlmostEqual( 223.872113856834, binning( 200))
 
     def test_next(self):
         binning = RoundLog(retvalue = 'center')
-        expected = [2.8183829312644537, 28.183829312644534, 281.8382931264455]
-        actual = binning.next([2.2387211385683394, 22.3872113856834, 223.872113856834])
-        self.assertEqual(len(expected), len(actual))
-        for e, a in zip(expected, actual):
-            self.assertAlmostEqual(e, a)
+        self.assertAlmostEqual( 2.818382931264, binning.next(2.23872113856834))
+        self.assertAlmostEqual( 28.18382931264, binning.next(22.3872113856834))
+        self.assertAlmostEqual( 281.8382931264, binning.next(223.872113856834))
 
     def test_call_zero(self):
         binning = RoundLog()
@@ -43,10 +37,8 @@ class TestRoundLog(unittest.TestCase):
 
     def test_valid(self):
         binning = RoundLog(valid = lambda x: x >= 10)
-        expected = [12.589254117941675, 10.0, None]
-        actual = binning((13, 10, 7))
-        self.assertEqual(len(expected), len(actual))
-        for e, a in zip(expected, actual):
-            self.assertAlmostEqual(e, a)
+        self.assertAlmostEqual( 12.589254117941675, binning(13))
+        self.assertAlmostEqual( 10.0, binning(10))
+        self.assertIsNone(binning(7))
 
 ##____________________________________________________________________________||
