7d4da014bdbafa58d5d8a95d6d1442f8ce49d072
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-27 09:10:49 +0200

    add an option to create EventsWithBranchAddressAccess in EventBuilder

--- AlphaTwirl/HeppyResult/EventBuilder.py
+++ AlphaTwirl/HeppyResult/EventBuilder.py
@@ -2,22 +2,28 @@
 import os
 import ROOT
 from ..Events import Events
+from ..EventsWithBranchAddressAccess import EventsWithBranchAddressAccess
 
 ##____________________________________________________________________________||
 class EventBuilder(object):
-    def __init__(self, analyzerName, fileName, treeName, maxEvents = -1, brancheNames = None):
+    def __init__(self, analyzerName, fileName, treeName, maxEvents = -1,
+                 brancheNames = None, branchAddressAccess = False):
         self._analyzerName = analyzerName
         self._fileName = fileName
         self._treeName = treeName
         self._maxEvents = maxEvents
         self._brancheNames = brancheNames
+        self._branchAddressAccess = branchAddressAccess
 
     def build(self, component):
         inputPath = os.path.join(getattr(component, self._analyzerName).path, self._fileName)
         file = ROOT.TFile.Open(inputPath)
         tree = file.Get(self._treeName)
         if self._brancheNames is not None: self._disableAllUnnecessaryBranches(tree)
-        return Events(tree, self._maxEvents)
+        if not self._branchAddressAccess:
+            return Events(tree, self._maxEvents)
+        else:
+            return EventsWithBranchAddressAccess(tree, self._brancheNames, self._maxEvents)
 
     def _disableAllUnnecessaryBranches(self, tree):
         tree.SetBranchStatus('*', 0)
