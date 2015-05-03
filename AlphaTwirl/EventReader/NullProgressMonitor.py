b258fe9d049780df58ea98805a888f74c0e69d88
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-03 18:50:33 +0200

    make NullProgressMonitor.createReporter() return None

--- AlphaTwirl/EventReader/NullProgressMonitor.py
+++ AlphaTwirl/EventReader/NullProgressMonitor.py
@@ -1,12 +1,8 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 
 ##____________________________________________________________________________||
-class NullProgressReporter(object):
-    def report(self, report): pass
-
-##____________________________________________________________________________||
 class NullProgressMonitor(object):
-    def createReporter(self): return NullProgressReporter()
+    def createReporter(self): return None
     def addWorker(self, worker): pass
     def monitor(self): pass
     def last(self): pass
