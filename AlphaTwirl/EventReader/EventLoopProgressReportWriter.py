0fe76a38aff965aca9f672b48ed4a4933ee10161
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-03 20:25:42 +0200

    add an argument taskid to EventLoopProgressReportWriter.write()

--- AlphaTwirl/EventReader/EventLoopProgressReportWriter.py
+++ AlphaTwirl/EventReader/EventLoopProgressReportWriter.py
@@ -3,7 +3,12 @@ from AlphaTwirl.ProgressBar import ProgressReport
 
 ##____________________________________________________________________________||
 class EventLoopProgressReportWriter(object):
-    def write(self, component, event):
-        return ProgressReport(name = component.name, done = event.iEvent + 1, total = event.nEvents)
+    def write(self, taskid, component, event):
+        return ProgressReport(
+            name = component.name,
+            done = event.iEvent + 1,
+            total = event.nEvents,
+            taskid = taskid
+        )
 
 ##____________________________________________________________________________||
