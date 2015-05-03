26e67424a087e45037b069d18f15eff142dcdabf
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-03 20:24:23 +0200

    add an optional argument taskid, add docstrings in ProgressReport

--- AlphaTwirl/ProgressBar/ProgressReport.py
+++ AlphaTwirl/ProgressBar/ProgressReport.py
@@ -2,7 +2,20 @@
 
 ##____________________________________________________________________________||
 class ProgressReport(object):
-    def __init__(self, name, done, total):
+    def __init__(self, name, done, total, taskid = None):
+        """a progress report
+
+        Args:
+
+          name (str): the name of the task. if taskid is None, used to
+              identify the task
+          done (int): the number of the iterations done so far
+          total (int): the total iterations to be done
+          taskid (immutable, optional): if given, used to identify the
+              task. useful if multiple tasks have the same name
+
+        """
+        self.taskid = taskid if taskid is not None else name
         self.name = name
         self.done = done
         self.total = total
