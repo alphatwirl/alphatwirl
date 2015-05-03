eeb5326cc9a85e57f180573599c0c1569b475d12
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-03 20:22:44 +0200

    update examples/progressbar.py, have the multiple tasks with the same name

--- examples/progressbar.py
+++ examples/progressbar.py
@@ -3,6 +3,7 @@
 from AlphaTwirl.ProgressBar import ProgressBar, MPProgressMonitor, ProgressReport
 from AlphaTwirl.EventReader import MPEventLoopRunner
 import time, random
+import uuid
 
 ##____________________________________________________________________________||
 class EventLoop(object):
@@ -11,10 +12,11 @@ class EventLoop(object):
         self.readers = [ ]
     def __call__(self, progressReporter = None):
         n = random.randint(5, 1000000)
+        taskid = uuid.uuid4()
         time.sleep(random.randint(0, 3))
         for i in xrange(n):
             time.sleep(0.0001)
-            report = ProgressReport(name = self.name, done = i + 1, total = n)
+            report = ProgressReport(name = self.name, done = i + 1, total = n, taskid = taskid)
             progressReporter.report(report)
         return self.readers
     def firstReportProgress(self, progressReporter): pass
@@ -32,6 +34,7 @@ runner.run(EventLoop("l"))
 runner.run(EventLoop("loop6"))
 runner.run(EventLoop("loop7"))
 runner.run(EventLoop("loop8"))
+runner.run(EventLoop("loop6"))
 runner.end()
 
 ##____________________________________________________________________________||
