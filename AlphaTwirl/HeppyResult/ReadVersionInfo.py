688d9a3ae013e136cc71697ac5a72e7208196f78
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-13 19:22:28 +0200

    add comment in ReadVersionInfo

--- AlphaTwirl/HeppyResult/ReadVersionInfo.py
+++ AlphaTwirl/HeppyResult/ReadVersionInfo.py
@@ -8,6 +8,9 @@ class ReadVersionInfo(object):
 
     def _readImp(self, file):
         full = file.read()
+        # versionInfo.txt is written as
+        # https://github.com/CMSRA1/cmg-cmssw-private/blob/RA1cmg_v2.4_patch1/CMGTools/Production/scripts/heppyBatchAlphaT.py#L91
+        # the second line will be the tag
         tag = full.split('\n')[1]
         return dict(full = full, tag = tag)
 
