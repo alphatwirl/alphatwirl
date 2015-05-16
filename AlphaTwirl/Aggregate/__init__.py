4f28c36839b788f4a3350f83cbceca4b71805189
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-16 16:45:16 +0200

    rename normalizeToXsec combine_MC_yields_in_datasets_into_xsec_in_processes, make it return xsec instead of yield

--- AlphaTwirl/Aggregate/__init__.py
+++ AlphaTwirl/Aggregate/__init__.py
@@ -1,6 +1,6 @@
 try:
     import pandas as pd
-    from functions import normalizeToXsec
+    from functions import combine_MC_yields_in_datasets_into_xsec_in_processes
     from functions import sumOverCategories
 except ImportError:
     pass
