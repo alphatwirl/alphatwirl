3884b9f36a5c9bf9bfb9df9d50d11c6343ec5900
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-19 17:42:18 +0200

    make GenericKeyComposer return a list of keys instead of a key

--- AlphaTwirl/Counter/GenericKeyComposer.py
+++ AlphaTwirl/Counter/GenericKeyComposer.py
@@ -14,12 +14,12 @@ class GenericKeyComposer(object):
         for varName, binning, index in zip(self._varNames, self._binnings, self._indices):
             var = getattr(event, varName)
             if index is not None:
-                if len(var) <= index: return None
+                if len(var) <= index: return ()
                 var = var[index]
             var_bin = binning(var)
-            if var_bin is None: return None
+            if var_bin is None: return ()
             ret.append(var_bin)
-        return tuple(ret)
+        return (tuple(ret), )
 
     def next(self, key):
         ret = [ ]
--- tests/unit/Counter/test_GenericKeyComposer.py
+++ tests/unit/Counter/test_GenericKeyComposer.py
@@ -29,7 +29,7 @@ class TestGenericKeyComposer(unittest.TestCase):
 
         event = MockEvent()
         event.var1 = 12
-        self.assertEqual((12, ), keyComposer(event))
+        self.assertEqual(((12, ), ), keyComposer(event))
 
     def test_call_2vars(self):
         keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningEcho(), MockBinningEcho()))
@@ -37,30 +37,30 @@ class TestGenericKeyComposer(unittest.TestCase):
         event = MockEvent()
         event.var1 = 15
         event.var2 = 22
-        self.assertEqual((15, 22), keyComposer(event))
+        self.assertEqual(((15, 22), ), keyComposer(event))
 
-    def test_None_key_1var(self):
+    def test_empty_key_1var(self):
         keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningNone(), ))
 
         event = MockEvent()
         event.var1 = 12
-        self.assertIsNone(keyComposer(event))
+        self.assertEqual(( ), keyComposer(event))
 
-    def test_None_key_2vars_var1(self):
+    def test_empty_key_2vars_var1(self):
         keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningNone(), MockBinningEcho()))
 
         event = MockEvent()
         event.var1 = 15
         event.var2 = 22
-        self.assertIsNone(keyComposer(event))
+        self.assertEqual(( ), keyComposer(event))
 
-    def test_None_key_2vars_var2(self):
+    def test_empty_key_2vars_var2(self):
         keyComposer = Counter.GenericKeyComposer(('var1', 'var2'), (MockBinningEcho(), MockBinningNone()))
 
         event = MockEvent()
         event.var1 = 15
         event.var2 = 22
-        self.assertIsNone(keyComposer(event))
+        self.assertEqual(( ) ,keyComposer(event))
 
     def test_binnings(self):
         binning1 = MockBinningEcho()
@@ -76,16 +76,16 @@ class TestGenericKeyComposer(unittest.TestCase):
         keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningEcho(), ), indices = (0, ))
         event = MockEvent()
         event.var1 = (12, )
-        self.assertEqual((12, ), keyComposer(event))
+        self.assertEqual(((12, ), ), keyComposer(event))
 
     def test_indices_2(self):
         keyComposer = Counter.GenericKeyComposer(('var1', ), (MockBinningEcho(), ), indices = (1, ))
         event = MockEvent()
         event.var1 = (12, 8, 6)
-        self.assertEqual((8, ), keyComposer(event))
+        self.assertEqual(((8, ), ), keyComposer(event))
 
         event.var1 = (3, )
-        self.assertIsNone(keyComposer(event))
+        self.assertEqual(( ), keyComposer(event))
 
     def test_indices_3(self):
         keyComposer = Counter.GenericKeyComposer(('var1', 'var2', 'var3'), (MockBinningEcho(), MockBinningEcho(), MockBinningEcho()), indices = (1, None, 2))
@@ -93,7 +93,7 @@ class TestGenericKeyComposer(unittest.TestCase):
         event.var1 = (12, 8, 6)
         event.var2 = 232
         event.var3 = (111, 222, 333)
-        self.assertEqual((8, 232, 333), keyComposer(event))
+        self.assertEqual(((8, 232, 333), ), keyComposer(event))
 
     def test_next(self):
         binnings = (MockBinningEcho(), MockBinningEcho(), MockBinningEcho())
