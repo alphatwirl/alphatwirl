057d4921e522fb4360d09e684f7d985abb340a09
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-21 18:54:53 +0200

    import NextKeyComposer in Counter/__init__.py

--- AlphaTwirl/Counter/__init__.py
+++ AlphaTwirl/Counter/__init__.py
@@ -1,6 +1,7 @@
 from Counter import Counter, CounterBuilder
 from GenericKeyComposer import GenericKeyComposer, GenericKeyComposerBuilder
 from GenericKeyComposerB import GenericKeyComposerB, GenericKeyComposerBBuilder
+from NextKeyComposer import NextKeyComposer, NextKeyComposerBuilder
 from Counts import Counts
 from CountsBase import CountsBase
 from CountsWithEmptyNextKeys import CountsWithEmptyNextKeys, CountsWithEmptyNextKeysBuilder
