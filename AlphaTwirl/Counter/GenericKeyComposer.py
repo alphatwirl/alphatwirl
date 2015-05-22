6dd23a8ce1ae5ca85475c16d7a85d842a8acd7f9
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-22 16:13:07 +0200

    add a docstring in GenericKeyComposer

--- AlphaTwirl/Counter/GenericKeyComposer.py
+++ AlphaTwirl/Counter/GenericKeyComposer.py
@@ -2,6 +2,37 @@
 
 ##____________________________________________________________________________||
 class GenericKeyComposer(object):
+    """This class compose a key for the event.
+
+    (this docstring is under development.)
+
+    A key is a tuple of bins. The class returns a tuple of keys, i.e.,
+    a tuple of tuples. Currently, the tuple has at most one element
+    (one key).
+
+    __init__ takes two mandatory arguments branchNames and binnings
+    and one optional argument indices. These arguments are arrays or
+    tuples with the same length.
+
+    This class assumes a branch can be accessed as an attribute of the
+    event, e.g., event.jet_pt.
+
+    A branch can contain either a value or an array of values. If at
+    least one of the branches contains an array, indices need to be
+    given. The indices are a list of indices of arrays. An index for a
+    branch that is not for an array should be None. For example, if
+    indices are (0, None), the class assumes the first branch is an
+    array and the second is a value. It then accesses the first
+    element of the first branch and the value of the second branch.
+
+    If the array is not long enough to contain the index specified,
+    the class returns an empty tuple.
+
+
+    The class uses the binnings to obtain bins for the values in the
+    branches.
+
+    """
     def __init__(self, branchNames, binnings, indices = None):
         self.branchNames = branchNames
         self.binnings = binnings
