2b94c8aad030ff4387fe3bf83ffa37021636c230
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-25 17:12:05 +0200

    move EventReaderPackageBundle to a separate file

--- AlphaTwirl/EventReader/EventReaderBundle.py
+++ AlphaTwirl/EventReader/EventReaderBundle.py
@@ -1,32 +1,10 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
-from ..ProgressBar import ProgressReport
 from EventLoop import EventLoop
+from EventReaderPackageBundle import EventReaderPackageBundle
 
 ##____________________________________________________________________________||
 class AllEvents(object):
     def __call__(self, event): return True
-
-##____________________________________________________________________________||
-class EventReaderPackageBundle(object):
-
-    def __init__(self, progressBar = None):
-        self._packages = [ ]
-        self.progressBar = progressBar
-
-    def add(self, package):
-        self._packages.append(package)
-
-    def make(self, datasetName):
-        readers = [package.make(datasetName) for package in self._packages]
-        return readers
-
-    def collect(self):
-        for i, package in enumerate(self._packages):
-            if self.progressBar is not None:
-                report = ProgressReport(name = "collecting results", done = i + 1, total = len(self._packages))
-                self.progressBar.present(report)
-            package.collect()
-
 
 ##____________________________________________________________________________||
 class EventReaderBundle(object):
