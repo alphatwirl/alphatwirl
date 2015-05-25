9e06e1f7fd56f157b650a1c872a3253d42612bab
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-25 14:00:10 +0200

    add docstring in EventReaderPackage

--- AlphaTwirl/EventReader/EventReaderPackage.py
+++ AlphaTwirl/EventReader/EventReaderPackage.py
@@ -2,6 +2,22 @@
 
 ##____________________________________________________________________________||
 class EventReaderPackage(object):
+
+    """This class relates event readers and a result collector.
+
+    An instance of this class is initialized with a reader class and a
+    collector.
+
+    When the method ``make()`` is called with a data set name, this
+    class creates a reader, provide the collector with the pair of
+    the data set name and the reader, and returns the reader.
+
+    After the readers read events, the mothod ``collect`` is called.
+    This method, in turn, calls the method ``collect`` of the
+    collector.
+
+    """
+
     def __init__(self, ReaderClass, resultCollector = None):
         self._ReaderClass = ReaderClass
         self._resultCollector = resultCollector if resultCollector is not None else NullCollector()
