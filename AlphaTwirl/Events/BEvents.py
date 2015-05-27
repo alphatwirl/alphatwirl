67549727e4294be32b40f807e4f4c86218818a48
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-27 16:09:41 +0200

    use BranchAddressManagerForVector in BEvents

--- AlphaTwirl/Events/BEvents.py
+++ AlphaTwirl/Events/BEvents.py
@@ -2,9 +2,11 @@
 from Branch import Branch
 from Events import Events
 from BranchAddressManager import BranchAddressManager
+from BranchAddressManagerForVector import BranchAddressManagerForVector
 
 ##____________________________________________________________________________||
 branchAddressManager = BranchAddressManager()
+branchAddressManagerForVector = BranchAddressManagerForVector()
 
 ##____________________________________________________________________________||
 class BEvents(Events):
@@ -15,10 +17,21 @@ class BEvents(Events):
 
     def __getattr__(self, name):
         if name in self.branches: return self.branches[name]
-        itsArray, itsCountArray = branchAddressManager.getArrays(self.tree, name)
-        if itsArray is None: raise AttributeError("'" + str(self) + "' has no attribute '" + name + "'")
-        self.branches[name] = Branch(name, itsArray, itsCountArray)
+        branch = self._buildBranch(self.tree, name)
+        if branch is None: raise AttributeError("'" + str(self) + "' has no attribute '" + name + "'")
+        self.branches[name] = branch
         if self.iEvent >= 0: self.tree.GetEntry(self.iEvent)
         return self.branches[name]
+
+    def _buildBranch(self, tree, name):
+        itsArray, itsCountArray = branchAddressManager.getArrays(tree, name)
+        if itsArray is not None:
+            branch = Branch(name, itsArray, itsCountArray)
+            return branch
+        itsVector = branchAddressManagerForVector.getVector(tree, name)
+        if itsVector is not None:
+            return itsVector # this can be used at a branch
+        return None
+
 
 ##____________________________________________________________________________||
