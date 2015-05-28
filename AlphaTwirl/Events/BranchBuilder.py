bd9cf9ad0963337d5ecef2cfa4338da853bab643
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-28 15:03:53 +0200

    add docstring, split a method in BranchBuilder

--- AlphaTwirl/Events/BranchBuilder.py
+++ AlphaTwirl/Events/BranchBuilder.py
@@ -9,7 +9,25 @@ branchAddressManagerForVector = BranchAddressManagerForVector()
 
 ##__________________________________________________________________||
 class BranchBuilder(object):
+    """This class builds a branch.
+
+    A branch is an object with data whose address is set in a tree.
+
+    All instances of this class share the data.
+
+    """
+
+    itsdict = { }
+
     def __call__(self, tree, name):
+        if (tree, name) in self.__class__.itsdict:
+            return self.__class__.itsdict[(tree, name)]
+
+        branch = self.imp(tree, name)
+        self.__class__.itsdict[(tree, name)] = branch
+        return branch
+
+    def imp(self, tree, name):
         itsArray, itsCountArray = branchAddressManager.getArrays(tree, name)
         if itsArray is not None:
             branch = Branch(name, itsArray, itsCountArray)
