d0fb7736f6b9598ed0fc13b4ef29f8b982dd89f0
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-29 20:51:48 +0200

    update comments in MPEventLoopRunner

--- AlphaTwirl/EventReader/MPEventLoopRunner.py
+++ AlphaTwirl/EventReader/MPEventLoopRunner.py
@@ -2,6 +2,57 @@
 
 ##__________________________________________________________________||
 class MPEventLoopRunner(object):
+
+    """This class (concurrently) runs instances of ``EventLoop``.
+
+    An instance of this class needs to be initialized with a
+    ``CommunicationChannel``, a communication channel with workers
+    that actually run the ``EventLoop``::
+
+        runner = MPEventLoopRunner(communicationChannel)
+
+    The method ``begin()`` does nothing in the current version::
+
+        runner.begin()
+
+    In older versions, multiple processes are forked in this method.
+
+
+    Then, you can give an ``EventLoop`` with the method ``run()``::
+
+        runner.run(eventLoop1)
+
+    This class will send the event loop to a worker through the
+    communication channel. The worker, then, runs the event loop.
+
+    You can call the method ``run()`` mutiple times::
+
+        runner.run(eventLoop2)
+        runner.run(eventLoop3)
+        runner.run(eventLoop4)
+
+    If workers are in the background, this method immediately returns.
+    Worker are running the event loops concurrently in the background.
+    If the worker is in the foreground, this method won't return until
+    the worker finishes running the event loop. In this case, a
+    progress bar might be shown on the screen if the communication
+    channel is configured so. Whether workers are in the background or
+    foreground depends on the communication channel with which this
+    class is initialized.
+
+    After giving all event loops that you need to run to this class,
+    you need to call the method ``end()``::
+
+        runner.end()
+
+    If workers are in the background, this method will wait until
+    workers finish running all event loops. While waiting progress
+    bars will be shown on the screen if the communication channel is
+    configured so. If the worker is in the background, this method
+    immediately returns.
+
+    """
+
     def __init__(self, communicationChannel):
         self.communicationChannel = communicationChannel
         self._original_readers = [ ]
@@ -13,17 +64,18 @@ class MPEventLoopRunner(object):
         self.communicationChannel.put(eventLoop)
 
     def end(self):
-        # If eventLoops were executed in other processes, the readers
-        # in the main process do not read the events; therefore, they
-        # don't have the results. The readers in other processes read
-        # the events. They have the results. The readers in other
-        # process are pickled and sent back to the main process.
-        # However, these returned readers are no longer the same
-        # objects as the original readers in the main process.
+        """If eventLoops were executed in other processes, the readers
+        in the main process do not read the events; therefore, they
+        don't have the results. The readers in other processes read
+        the events. They have the results. The readers in other
+        process are pickled and sent back to the main process.
+        However, these returned readers are no longer the same
+        objects as the original readers in the main process.
 
-        # The following lines of the code copy the results in the
-        # returned readers to the original readers if they are
-        # different objects.
+        The method copy the results in the returned readers to the
+        original readers if they are different objects.
+
+        """
 
         returned_readers = self.communicationChannel.receive()
 
