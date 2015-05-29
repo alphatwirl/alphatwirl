d31a11e5b418869fd5cf7ebddbe4f8a04f8abb84
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-29 18:31:13 +0200

    add comments in  MPEventLoopRunner

--- AlphaTwirl/EventReader/MPEventLoopRunner.py
+++ AlphaTwirl/EventReader/MPEventLoopRunner.py
@@ -13,6 +13,18 @@ class MPEventLoopRunner(object):
         self.communicationChannel.put(eventLoop)
 
     def end(self):
+        # If eventLoops were executed in other processes, the readers
+        # in the main process do not read the events; therefore, they
+        # don't have the results. The readers in other processes read
+        # the events. They have the results. The readers in other
+        # process are pickled and sent back to the main process.
+        # However, these returned readers are no longer the same
+        # objects as the original readers in the main process.
+
+        # The following lines of the code copy the results in the
+        # returned readers to the original readers if they are
+        # different objects.
+
         returned_readers = self.communicationChannel.receive()
         for original, returned in zip(self._original_readers, returned_readers):
             if original is returned: continue
