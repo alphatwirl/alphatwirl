5c3b7618ae02d3e38ed06b9f42a3510a7ef17dbb
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-31 00:39:27 +0200

    specify an interval to update in ProgressBar

--- AlphaTwirl/ProgressBar/ProgressBar.py
+++ AlphaTwirl/ProgressBar/ProgressBar.py
@@ -1,20 +1,25 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
+import time
 import sys, collections
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
 class ProgressBar(object):
     def __init__(self):
         self.reports = collections.OrderedDict()
         self.lines = [ ]
+        self.interval = 0.1 # [second]
+        self._readTime()
 
     def nreports(self):
         return len(self.reports)
 
     def present(self, report):
         self.reports[report.taskid] = report
+        if not self._need_to_update(report): return
         self._delete_previous_lines()
         self._create_lines()
         self._print_lines()
+        self._readTime()
 
     def _delete_previous_lines(self):
         if len(self.lines) >= 1:
@@ -46,4 +51,12 @@ class ProgressBar(object):
         name = report.name[0:nameFieldLength]
         return " {3:6.2f}% {2:s} | {4:7d} / {5:7d} |:  {0:<{1}s} ".format(name, nameFieldLength, bar, percent, report.done, report.total)
 
-##____________________________________________________________________________||
+    def _need_to_update(self, report):
+        if self._time() - self.lastTime > self.interval: return True
+        if report.done == report.total: return True
+        return False
+
+    def _time(self): return time.time()
+    def _readTime(self): self.lastTime = self._time()
+
+##__________________________________________________________________||
