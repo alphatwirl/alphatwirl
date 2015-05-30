473d7aa2f687bb1a63686aeb321e95a019f684a9
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-30 20:36:17 +0200

    add new classes BProgressMonitor and ProgressReportTray

---
+++ AlphaTwirl/ProgressBar/BProgressMonitor.py
@@ -0,0 +1,65 @@
+# Tai Sakuma <tai.sakuma@cern.ch>
+import multiprocessing
+import time
+from ProgressReporter import ProgressReporter
+
+##__________________________________________________________________||
+class BProgressMonitor(object):
+    def __init__(self, presentation):
+        self.queue = multiprocessing.Queue()
+        self.presentation = presentation
+
+    def begin(self):
+        self.bg = ProgressReportTray(self.queue, self.presentation)
+        self.bg.start()
+
+    def end(self):
+        self.queue.put(None)
+        self.bg.join()
+
+    def monitor(self):
+        pass
+
+    def last(self):
+        pass
+
+    def createReporter(self):
+        return ProgressReporter(self.queue)
+
+##__________________________________________________________________||
+class ProgressReportTray(multiprocessing.Process):
+    def __init__(self, queue, presentation):
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        self.presentation = presentation
+        self.lastWaitTime = 1.0 # [second]
+
+    def run(self):
+        self._run_until_the_end_order_arrives()
+        self._run_until_reports_stop_coming()
+
+    def _run_until_the_end_order_arrives(self):
+        end_order_arrived = False
+        while True:
+            while not self.queue.empty():
+                report = self.queue.get()
+                if report is None: # the end order
+                    end_order_arrived = True
+                    continue
+                self.presentation.present(report)
+            if end_order_arrived: break
+
+    def _run_until_reports_stop_coming(self):
+        self._readTime()
+        while self.presentation.nreports() > 0:
+            if self._time() - self.lastTime > self.lastWaitTime: break
+            while not self.queue.empty():
+                report = self.queue.get()
+                if report is None: continue
+                self._readTime()
+                self.presentation.present(report)
+
+    def _time(self): return time.time()
+    def _readTime(self): self.lastTime = self._time()
+
+##__________________________________________________________________||
