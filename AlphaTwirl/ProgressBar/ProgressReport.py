a88bc3ee674e624506e4f2224b07c4f4ae78c298
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-30 12:01:11 +0200

    update docstring in ProgressReport

--- AlphaTwirl/ProgressBar/ProgressReport.py
+++ AlphaTwirl/ProgressBar/ProgressReport.py
@@ -2,19 +2,16 @@
 
 ##____________________________________________________________________________||
 class ProgressReport(object):
+    """A progress report
+
+    Args:
+        name (str): the name of the task. if ``taskid`` is ``None``, used to identify the task
+        done (int): the number of the iterations done so far
+        total (int): the total iterations to be done
+        taskid (immutable, optional): if given, used to identify the task. useful if multiple tasks have the same name
+    """
+
     def __init__(self, name, done, total, taskid = None):
-        """a progress report
-
-        Args:
-
-          name (str): the name of the task. if taskid is None, used to
-              identify the task
-          done (int): the number of the iterations done so far
-          total (int): the total iterations to be done
-          taskid (immutable, optional): if given, used to identify the
-              task. useful if multiple tasks have the same name
-
-        """
         self.taskid = taskid if taskid is not None else name
         self.name = name
         self.done = done
