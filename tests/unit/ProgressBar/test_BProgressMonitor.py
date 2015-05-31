c4ceb0b79aedbd3845b9928fcdd097c6a697db26
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-31 14:23:33 +0200

    add new test cases TestProgressReportPickup, TestBProgressMonitor

---
+++ tests/unit/ProgressBar/test_BProgressMonitor.py
@@ -0,0 +1,27 @@
+from AlphaTwirl.ProgressBar import ProgressReporter, BProgressMonitor
+import unittest
+
+##__________________________________________________________________||
+class MockPresentation(object):
+    def __init__(self): self.reports = [ ]
+    def present(self, report): self.reports.append(report)
+    def nreports(self): return 0
+
+##__________________________________________________________________||
+class MockReport(object): pass
+
+##__________________________________________________________________||
+class TestBProgressMonitor(unittest.TestCase):
+
+    def test_begin_end(self):
+        presentation = MockPresentation()
+        monitor = BProgressMonitor(presentation)
+        monitor.begin()
+        monitor.end()
+
+    def test_createReporter(self):
+        presentation = MockPresentation()
+        monitor = BProgressMonitor(presentation)
+        self.assertIsInstance(monitor.createReporter(), ProgressReporter)
+
+##__________________________________________________________________||
