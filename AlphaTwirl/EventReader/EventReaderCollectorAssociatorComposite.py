96f14085bd3520ab2317dbe89127a718decdcdb5
Tai Sakuma <tai.sakuma@gmail.com>
2015-05-31 18:08:10 +0200

    remove the method collect(), add an attribute collector, update docstring in EventReaderCollectorAssociatorComposite

--- AlphaTwirl/EventReader/EventReaderCollectorAssociatorComposite.py
+++ AlphaTwirl/EventReader/EventReaderCollectorAssociatorComposite.py
@@ -1,30 +1,52 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 from ..ProgressBar import ProgressReport
 from .EventReaderComposite import EventReaderComposite
+from .CollectorComposite import CollectorComposite
 
 ##__________________________________________________________________||
 class EventReaderCollectorAssociatorComposite(object):
 
+    """A composite of `EventReaderCollectorAssociator`.
+
+    This class is a composite in the composite pattern.
+
+    Attributes:
+      collector (CollectorComposite): a composite of collectors
+      components: a list of the associators, the components of the composite
+
+    """
+
     def __init__(self, progressReporter = None):
-        self.associators = [ ]
-        self.progressReporter = progressReporter
+        self.collector = CollectorComposite(progressReporter)
+        self.components = [ ]
 
     def add(self, associator):
-        self.associators.append(associator)
+        """add an associator.
+
+
+        Args:
+            associator: the associator to be added
+
+        """
+        self.components.append(associator)
+        self.collector.add(associator.collector)
 
     def make(self, datasetName):
+        """make an `EventReaderComposite`.
+
+
+        Args:
+            datasetName (str): the name of the data set that the components of the `EventReaderComposite` will read
+
+        Returns:
+            an instance of `EventReaderComposite`
+
+        """
+
         readerComposite = EventReaderComposite()
-        for associator in self.associators:
+        for associator in self.components:
             reader = associator.make(datasetName)
             readerComposite.add(reader)
         return readerComposite
-
-    def collect(self):
-        for i, associator in enumerate(self.associators):
-            if self.progressReporter is not None:
-                report = ProgressReport(name = "collecting results", done = i + 1, total = len(self.associators))
-                self.progressReporter.report(report)
-            associator.collect()
-
 
 ##__________________________________________________________________||
--- tests/unit/EventReader/test_EventReaderCollectorAssociatorComposite.py
+++ tests/unit/EventReader/test_EventReaderCollectorAssociatorComposite.py
@@ -5,49 +5,51 @@ import unittest
 class MockReader(object):
     def __init__(self, name):
         self.name = name
-        self._eventIds = [ ]
-
-    def event(self, event):
-        self._eventIds.append(event.id)
 
 ##__________________________________________________________________||
-class MockReaderPackage(object):
+class MockCollector(object): pass
+
+##__________________________________________________________________||
+class MockCollectorComposite(object):
+    def __init__(self):
+        self.added = [ ]
+
+    def add(self, collector):
+        self.added.append(collector)
+
+##__________________________________________________________________||
+class MockAssociator(object):
     def __init__(self):
         self.reader = None
-        self.collected = False
+        self.collector = MockCollector()
 
     def make(self, name):
         self.reader = MockReader(name)
         return self.reader
 
-    def collect(self):
-        self.collected = True
-
 ##__________________________________________________________________||
 class TestEventReaderCollectorAssociatorComposite(unittest.TestCase):
 
-    def test_packages_read_and_collected(self):
+    def test_associators_add_and_make(self):
 
-        bundle = EventReaderCollectorAssociatorComposite()
+        associatorComposite = EventReaderCollectorAssociatorComposite()
+        collectorComposite = MockCollectorComposite()
+        associatorComposite.collector = collectorComposite
 
-        package1 = MockReaderPackage()
-        bundle.add(package1)
+        associator1 = MockAssociator()
+        associatorComposite.add(associator1)
+        self.assertIs(associator1.collector, collectorComposite.added[0])
 
-        package2 = MockReaderPackage()
-        bundle.add(package2)
+        associator2 = MockAssociator()
+        associatorComposite.add(associator2)
+        self.assertIs(associator2.collector, collectorComposite.added[1])
 
-        actual = bundle.make("compName1") # need to evaluate actual before expected.
-        expected = [package1.reader, package2.reader]
+        actual = associatorComposite.make("compName1") # need to evaluate actual before expected.
+        expected = [associator1.reader, associator2.reader]
         self.assertEqual(expected, actual.readers)
 
-        actual = bundle.make("compName2")
-        expected = [package1.reader, package2.reader]
+        actual = associatorComposite.make("compName2")
+        expected = [associator1.reader, associator2.reader]
         self.assertEqual(expected, actual.readers)
-
-        self.assertFalse(package1.collected)
-        self.assertFalse(package2.collected)
-        bundle.collect()
-        self.assertTrue(package1.collected)
-        self.assertTrue(package2.collected)
 
 ##__________________________________________________________________||
