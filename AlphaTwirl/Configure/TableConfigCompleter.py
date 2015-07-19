15e2a250e3e4f6004c8e7eed88234d04c36e114c
Tai Sakuma <tai.sakuma@gmail.com>
2015-07-19 15:37:36 +0200

    add a new variable defaultWeight to TableConfigCompleter

--- AlphaTwirl/Configure/TableConfigCompleter.py
+++ AlphaTwirl/Configure/TableConfigCompleter.py
@@ -18,6 +18,7 @@ def createOutFileName(columnNames, indices, prefix = 'tbl_component_', suffix = 
 class TableConfigCompleter(object):
     def __init__(self, defaultCountsClass, outDir):
         self.defaultCountsClass = defaultCountsClass
+        self.defaultWeight = WeightCalculatorOne()
         self.outDir = outDir
 
     def complete(self, tblcfg):
@@ -26,7 +27,7 @@ class TableConfigCompleter(object):
         if 'indices' not in ret: ret['indices'] = None
         if 'countsClass' not in ret: ret['countsClass'] = self.defaultCountsClass
         if 'outFile' not in ret: ret['outFile'] = True
-        if 'weight' not in ret: ret['weight'] = WeightCalculatorOne()
+        if 'weight' not in ret: ret['weight'] = self.defaultWeight
         if ret['outFile']:
             if 'outFileName' not in ret: ret['outFileName'] = createOutFileName(ret['outColumnNames'], ret['indices'])
             if 'outFilePath' not in ret: ret['outFilePath'] = os.path.join(self.outDir, ret['outFileName'])
