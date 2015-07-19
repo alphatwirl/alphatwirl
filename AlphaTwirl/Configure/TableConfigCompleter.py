377591c1b16b3a10e23d79c470e3d53a0a8bf870
Tai Sakuma <tai.sakuma@gmail.com>
2015-07-19 16:00:56 +0200

    promote the function createOutFileName to the class TableFileNameComposer

--- AlphaTwirl/Configure/TableConfigCompleter.py
+++ AlphaTwirl/Configure/TableConfigCompleter.py
@@ -2,18 +2,28 @@
 import os
 
 ##__________________________________________________________________||
-def createOutFileName(columnNames, indices, prefix = 'tbl_component_', suffix = '.txt'):
-    # for example, if columnNames = ('var1', 'var2', 'var3') and indices = (1, None, 2),
-    # l will be ['var1', '1', 'var2', 'var3', '2']
-    l = columnNames if indices is None else [str(e) for sublist in zip(columnNames, indices) for e in sublist if e is not None]
-    ret = prefix + '_'.join(l) + suffix # e.g. "tbl_component_var1_1_var2_var3_2.txt"
-    return ret
+class TableFileNameComposer(object):
+    """Compose a name of a file to store the table from the column names and indices.
+
+
+    For example, if column names are 'var1', 'var2', and 'var3' and
+    indices are 1, None and 2, the file name will be
+    'tbl_component_var1_1_var2_var3_2.txt'
+
+    """
+    def __call__(self, columnNames, indices, prefix = 'tbl_component_', suffix = '.txt'):
+        # for example, if columnNames = ('var1', 'var2', 'var3') and indices = (1, None, 2),
+        # l will be ['var1', '1', 'var2', 'var3', '2']
+        l = columnNames if indices is None else [str(e) for sublist in zip(columnNames, indices) for e in sublist if e is not None]
+        ret = prefix + '_'.join(l) + suffix # e.g. "tbl_component_var1_1_var2_var3_2.txt"
+        return ret
 
 ##__________________________________________________________________||
 class TableConfigCompleter(object):
     def __init__(self, defaultCountsClass, defaultWeight, outDir):
         self.defaultCountsClass = defaultCountsClass
         self.defaultWeight = defaultWeight
+        self.createOutFileName = TableFileNameComposer()
         self.outDir = outDir
 
     def complete(self, tblcfg):
@@ -24,7 +34,7 @@ class TableConfigCompleter(object):
         if 'outFile' not in ret: ret['outFile'] = True
         if 'weight' not in ret: ret['weight'] = self.defaultWeight
         if ret['outFile']:
-            if 'outFileName' not in ret: ret['outFileName'] = createOutFileName(ret['outColumnNames'], ret['indices'])
+            if 'outFileName' not in ret: ret['outFileName'] = self.createOutFileName(ret['outColumnNames'], ret['indices'])
             if 'outFilePath' not in ret: ret['outFilePath'] = os.path.join(self.outDir, ret['outFileName'])
             return ret
 
