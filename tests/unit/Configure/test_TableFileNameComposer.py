4aba8a90075089a3587683d8735de7d206b38b04
Tai Sakuma <tai.sakuma@gmail.com>
2015-07-19 16:09:33 +0200

    add a new test case TestTableFileNameComposer

---
+++ tests/unit/Configure/test_TableFileNameComposer.py
@@ -0,0 +1,15 @@
+from AlphaTwirl.Configure import TableFileNameComposer
+import unittest
+
+##__________________________________________________________________||
+class TestTableFileNameComposer(unittest.TestCase):
+
+    def test_one(self):
+        compose = TableFileNameComposer()
+        actual = compose(
+            columnNames = ('var1', 'var2', 'var3'),
+            indices = (1, None, 2)
+        )
+        self.assertEqual("tbl_component_var1_1_var2_var3_2.txt", actual)
+
+##__________________________________________________________________||
