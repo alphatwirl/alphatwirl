5db5e9626f47e33616809e38c4c9450ac66786bc
Tai Sakuma <tai.sakuma@gmail.com>
2015-07-21 15:34:51 +0200

    update TestCombine

--- tests/unit/Binning/test_Combine.py
+++ tests/unit/Binning/test_Combine.py
@@ -14,6 +14,13 @@ class TestCombine(unittest.TestCase):
         self.assertEqual(80, binning(90))
         self.assertEqual(100, binning(100))
 
+        low = Round(10.0, 50)
+        high = RoundLog(0.1, 50)
+        binning = Combine(low =low, high = high, at = 50)
+        self.assertEqual(10, binning(11))
+        self.assertEqual(40, binning(40))
+        self.assertAlmostEqual(50, binning(50))
+
     def test_next(self):
         low = Round(20.0, 100)
         high = RoundLog(0.1, 100)
@@ -21,5 +28,12 @@ class TestCombine(unittest.TestCase):
         self.assertEqual(20, binning.next(10))
         self.assertEqual(100, binning.next(90))
         self.assertEqual(125.89254117941675, binning.next(100))
+
+        low = Round(10.0, 50)
+        high = RoundLog(0.1, 50)
+        binning = Combine(low =low, high = high, at = 50)
+        self.assertEqual(20, binning.next(10))
+        self.assertAlmostEqual(50, binning.next(45))
+        self.assertEqual(62.94627058970836, binning.next(50))
 
 ##____________________________________________________________________________||
