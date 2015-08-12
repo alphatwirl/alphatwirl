8d298d712ec7f985830994b9de846343196ae8d2
Tai Sakuma <tai.sakuma@gmail.com>
2015-08-12 17:33:42 +0200

    remove NextKeyComposerFactory

--- AlphaTwirl/Counter/__init__.py
+++ AlphaTwirl/Counter/__init__.py
@@ -2,5 +2,5 @@ from Counter import Counter
 from WeightCalculatorOne import WeightCalculatorOne
 from GenericKeyComposer import GenericKeyComposer
 from GenericKeyComposerB import GenericKeyComposerB
-from NextKeyComposer import NextKeyComposer, NextKeyComposerFactory
+from NextKeyComposer import NextKeyComposer
 from Counts import Counts
