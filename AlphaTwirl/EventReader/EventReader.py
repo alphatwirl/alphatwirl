4f5963f6497b528f6e187e3d6b3931884a15a319
Tai Sakuma <tai.sakuma@gmail.com>
2016-02-29 21:10:59 +0100

    add a new optiona 'maxEventsPerRun' to EventReader

--- AlphaTwirl/EventReader/EventReader.py
+++ AlphaTwirl/EventReader/EventReader.py
@@ -2,25 +2,56 @@
 from .EventLoop import EventLoop
 from .Associator import Associator
 
+import math
+
 ##__________________________________________________________________||
 class EventReader(object):
-    def __init__(self, eventBuilder, eventLoopRunner, reader, collector):
+    def __init__(self, eventBuilder, eventLoopRunner, reader, collector, maxEventsPerRun = -1):
+
+        if maxEventsPerRun == 0:
+            raise ValueError("maxEventsPerRun cannot be 0")
+
         self.eventBuilder = eventBuilder
         self.eventLoopRunner = eventLoopRunner
         self.associator = Associator(reader, collector)
         self.collector = collector
+        self.maxEventsPerRun = maxEventsPerRun
         self.EventLoop = EventLoop
 
     def begin(self):
         self.eventLoopRunner.begin()
 
     def read(self, dataset):
-        reader = self.associator.make(dataset.name)
-        eventLoop = self.EventLoop(self.eventBuilder, dataset, reader)
-        self.eventLoopRunner.run(eventLoop)
+        if self.maxEventsPerRun < 0:
+            self._run_one_eventLoop_for_the_dataset(dataset)
+        else:
+            self._split_the_dataset_run_multiple_eventLoops(dataset)
 
     def end(self):
         self.eventLoopRunner.end()
         return self.collector.collect()
+
+    def _run_one_eventLoop_for_the_dataset(self, dataset):
+        reader = self.associator.make(dataset.name)
+        eventLoop = self.EventLoop(self.eventBuilder, dataset, reader)
+        self.eventLoopRunner.run(eventLoop)
+
+    def _split_the_dataset_run_multiple_eventLoops(self, dataset):
+        nTotal = self.eventBuilder.getNumberOfEventsInDataset(dataset)
+        nPerRun = self.maxEventsPerRun
+        starts = self._create_starting_event_list(nTotal, nPerRun)
+        for start in starts:
+            reader = self.associator.make(dataset.name)
+            eventLoop = self.EventLoop(
+                self.eventBuilder, dataset, reader,
+                start = start, nEvents = nPerRun
+            )
+            self.eventLoopRunner.run(eventLoop)
+
+    def _create_starting_event_list(self, nTotal, nPerRun):
+        nLoops = nTotal/nPerRun
+        if nTotal % nPerRun > 0: nLoops += 1
+        starts = [i*nPerRun for i in range(nLoops)]
+        return starts
 
 ##__________________________________________________________________||
--- tests/unit/EventReader/test_EventReader.py
+++ tests/unit/EventReader/test_EventReader.py
@@ -2,7 +2,12 @@ from AlphaTwirl.EventReader import EventReader
 import unittest
 
 ##__________________________________________________________________||
-class MockEventBuilder(object): pass
+class MockEventBuilder(object):
+    def __init__(self, nEntries = None):
+        self.nEntries = nEntries
+
+    def getNumberOfEventsInDataset(self, dataset):
+        return self.nEntries
 
 ##__________________________________________________________________||
 class MockReader(object): pass
@@ -12,11 +17,14 @@ class MockCollectorReturn(object): pass
 
 ##__________________________________________________________________||
 class MockCollector(object):
-    def __init__(self, ret):
+    def __init__(self, ret = None):
         self.collected = False
         self.ret = ret
 
+        self.pairs = [ ]
+
     def addReader(self, datasetName, reader):
+        self.pairs.append((datasetName, reader))
         self.datasetName = datasetName
         self.reader = reader
 
@@ -32,23 +40,25 @@ class MockEventLoopRunner(object):
     def __init__(self):
         self.began = False
         self.ended = False
-        self.eventLoop = None
+        self.eventLoops = [ ]
 
     def begin(self):
         self.began = True
 
     def run(self, eventLoop):
-        self.eventLoop = eventLoop
+        self.eventLoops.append(eventLoop)
 
     def end(self):
         self.ended = True
 
 ##__________________________________________________________________||
 class MockEventLoop(object):
-    def __init__(self, eventBuilder, dataset, reader):
+    def __init__(self, eventBuilder, dataset, reader, start = None, nEvents = None):
         self.eventBuilder = eventBuilder
         self.dataset = dataset
         self.reader = reader
+        self.start = start
+        self.nEvents = nEvents
 
 ##__________________________________________________________________||
 class TestEventReader(unittest.TestCase):
@@ -70,11 +80,14 @@ class TestEventReader(unittest.TestCase):
         dataset1 = MockDataset()
         dataset1.name = "dataset1"
         obj.read(dataset1)
-        eventLoop1 =  eventLoopRunner.eventLoop
+        self.assertEqual(1, len(eventLoopRunner.eventLoops))
+        eventLoop1 =  eventLoopRunner.eventLoops[0]
         self.assertIsInstance(eventLoop1, MockEventLoop)
         self.assertIs(eventBuilder, eventLoop1.eventBuilder)
         self.assertIs(dataset1, eventLoop1.dataset)
         self.assertIsInstance(eventLoop1.reader, MockReader)
+        self.assertIsNone(eventLoop1.start)
+        self.assertIsNone(eventLoop1.nEvents)
 
         self.assertEqual("dataset1", collector.datasetName)
         self.assertIs(eventLoop1.reader, collector.reader)
@@ -85,5 +98,77 @@ class TestEventReader(unittest.TestCase):
         self.assertIs(collector.ret, obj.end())
         self.assertTrue(eventLoopRunner.ended)
         self.assertTrue(collector.collected)
+
+    def test_init_raise(self):
+        eventBuilder = MockEventBuilder()
+        eventLoopRunner = MockEventLoopRunner()
+        reader = MockReader()
+        collector = MockCollector(MockCollectorReturn())
+        self.assertRaises(ValueError, EventReader, eventBuilder, eventLoopRunner, reader, collector, maxEventsPerRun = 0)
+
+    def test_split_dataset(self):
+        eventBuilder = MockEventBuilder(nEntries = 25)
+        eventLoopRunner = MockEventLoopRunner()
+        reader = MockReader()
+        collector = MockCollector(MockCollectorReturn())
+        obj = EventReader(eventBuilder, eventLoopRunner, reader, collector, maxEventsPerRun = 10)
+        obj.EventLoop = MockEventLoop
+
+        # begin
+        self.assertFalse(eventLoopRunner.began)
+        obj.begin()
+        self.assertTrue(eventLoopRunner.began)
+
+        # read
+        dataset1 = MockDataset()
+        dataset1.name = "dataset1"
+        obj.read(dataset1)
+
+        self.assertEqual(3, len(eventLoopRunner.eventLoops))
+        self.assertEqual(3, len(collector.pairs))
+
+        eventLoop =  eventLoopRunner.eventLoops[0]
+        self.assertIsInstance(eventLoop, MockEventLoop)
+        self.assertIs(eventBuilder, eventLoop.eventBuilder)
+        self.assertIs(dataset1, eventLoop.dataset)
+        self.assertIsInstance(eventLoop.reader, MockReader)
+        self.assertEqual(0, eventLoop.start)
+        self.assertEqual(10, eventLoop.nEvents)
+
+        self.assertEqual(("dataset1", eventLoop.reader), collector.pairs[0])
+
+        eventLoop =  eventLoopRunner.eventLoops[1]
+        self.assertIsInstance(eventLoop, MockEventLoop)
+        self.assertIs(eventBuilder, eventLoop.eventBuilder)
+        self.assertIs(dataset1, eventLoop.dataset)
+        self.assertIsInstance(eventLoop.reader, MockReader)
+        self.assertEqual(10, eventLoop.start)
+        self.assertEqual(10, eventLoop.nEvents)
+
+        self.assertEqual(("dataset1", eventLoop.reader), collector.pairs[1])
+
+        eventLoop =  eventLoopRunner.eventLoops[2]
+        self.assertIsInstance(eventLoop, MockEventLoop)
+        self.assertIs(eventBuilder, eventLoop.eventBuilder)
+        self.assertIs(dataset1, eventLoop.dataset)
+        self.assertIsInstance(eventLoop.reader, MockReader)
+        self.assertEqual(20, eventLoop.start)
+        self.assertEqual(10, eventLoop.nEvents)
+
+        self.assertEqual(("dataset1", eventLoop.reader), collector.pairs[2])
+
+        # end
+        self.assertFalse(eventLoopRunner.ended)
+        self.assertFalse(collector.collected)
+        self.assertIs(collector.ret, obj.end())
+        self.assertTrue(eventLoopRunner.ended)
+        self.assertTrue(collector.collected)
+
+    def test_create_starting_event_list(self):
+        obj = EventReader(MockEventBuilder(), MockEventLoopRunner(), MockReader(), MockCollector())
+        self.assertEqual([0, 10, 20, 30], obj._create_starting_event_list(40, 10))
+        self.assertEqual([0, 10, 20, 30, 40], obj._create_starting_event_list(41, 10))
+        self.assertEqual([0], obj._create_starting_event_list(40, 40))
+        self.assertEqual([0], obj._create_starting_event_list(40, 50))
 
 ##__________________________________________________________________||
