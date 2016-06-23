019dfeffe3979da96811b8e5f49fe4b9025aab29
Tai Sakuma <tai.sakuma@gmail.com>
2016-06-23 14:30:28 +0200

    clean up the code in ProgressPrint.py

--- AlphaTwirl/ProgressBar/ProgressPrint.py
+++ AlphaTwirl/ProgressBar/ProgressPrint.py
@@ -17,15 +17,12 @@ class ProgressPrint(object):
     def present(self, report):
         self.reports[report.taskid] = report
         if not self._need_to_update(report): return
-        self._delete_previous_lines()
         self._create_lines()
         self._print_lines()
         self._readTime()
 
-    def _delete_previous_lines(self):
-        self.lines = [ ]
-
     def _create_lines(self):
+        self.lines = [ ]
         for taskid, report in self.reports.items():
             line = self.createLine(report)
             if report.done >= report.total:
@@ -36,17 +33,16 @@ class ProgressPrint(object):
 
     def _print_lines(self):
         sys.stdout.write("\n")
-        print time.asctime(time.localtime(time.time()))
+        sys.stdout.write(time.asctime(time.localtime(time.time())))
+        sys.stdout.write("\n")
         if len(self.last) > 0: sys.stdout.write("\n".join(self.last) + "\n")
         sys.stdout.write("\n".join(self.lines) + "\n")
         sys.stdout.flush()
 
     def createLine(self, report):
-        nameFieldLength = 32
         percent = float(report.done)/report.total if report.total > 0 else 1
         bar = (':' * int(percent * 40)).ljust(40, " ")
         percent = round(percent * 100, 2)
-        name = report.name[0:nameFieldLength]
         return " {1:8d} / {2:8d} ({0:6.2f}%) {3} ".format(percent, report.done, report.total, report.name)
 
     def _need_to_update(self, report):
