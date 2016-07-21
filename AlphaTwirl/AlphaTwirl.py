16ddb278c10a8c0f23fb3a12e18b0741c3ec8728
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-21 15:27:57 +0200

    extract a new class ArgumentReader from AlphaTwirl

--- AlphaTwirl/AlphaTwirl.py
+++ AlphaTwirl/AlphaTwirl.py
@@ -115,6 +115,20 @@ def createTreeReader(analyzerName, fileName, treeName, reader, collector, nevent
     return eventReader
 
 ##__________________________________________________________________||
+class ArgumentReader(object):
+    def __init__(self, owner, *args, **kwargs):
+        self.parser = ArgumentParserWrapper(owner, *args, **kwargs)
+        self.parser.add_argument('-i', '--heppydir', default = '/Users/sakuma/work/cms/c150130_RA1_data/74X/MC/20150713_MC/20150713_SingleMu', action = 'store', help = "Heppy results dir")
+        self.parser.add_argument("-p", "--processes", action = "store", default = None, type = int, help = "number of processes to run in parallel")
+        self.parser.add_argument("-q", "--quiet", action = "store_true", default = False, help = "quiet mode")
+        self.parser.add_argument('-o', '--outDir', default = 'tbl/out', action = 'store')
+        self.parser.add_argument("-n", "--nevents", action = "store", default = -1, type = int, help = "maximum number of events to process for each component")
+        self.parser.add_argument("--max-events-per-process", action = "store", default = -1, type = int, help = "maximum number of events per process")
+        self.parser.add_argument("-c", "--components", default = None, nargs = '*', help = "the list of components")
+        self.parser.add_argument("--force", action = "store_true", default = False, dest="force", help = "recreate all output files")
+
+
+##__________________________________________________________________||
 class AlphaTwirl(object):
 
     def __init__(self):
@@ -123,20 +137,8 @@ class AlphaTwirl(object):
         self.treeReaderConfigs = [ ]
 
     def ArgumentParser(self, *args, **kwargs):
-        parser = ArgumentParserWrapper(self, *args, **kwargs)
-        parser = self._add_arguments(parser)
-        return parser
-
-    def _add_arguments(self, parser):
-        parser.add_argument('-i', '--heppydir', default = '/Users/sakuma/work/cms/c150130_RA1_data/74X/MC/20150713_MC/20150713_SingleMu', action = 'store', help = "Heppy results dir")
-        parser.add_argument("-p", "--processes", action = "store", default = None, type = int, help = "number of processes to run in parallel")
-        parser.add_argument("-q", "--quiet", action = "store_true", default = False, help = "quiet mode")
-        parser.add_argument('-o', '--outDir', default = 'tbl/out', action = 'store')
-        parser.add_argument("-n", "--nevents", action = "store", default = -1, type = int, help = "maximum number of events to process for each component")
-        parser.add_argument("--max-events-per-process", action = "store", default = -1, type = int, help = "maximum number of events per process")
-        parser.add_argument("-c", "--components", default = None, nargs = '*', help = "the list of components")
-        parser.add_argument("--force", action = "store_true", default = False, dest="force", help = "recreate all output files")
-        return parser
+        argumentReader = ArgumentReader(self, *args, **kwargs)
+        return argumentReader.parser
 
     def addComponentReader(self, reader):
         self.componentReaders.add(reader)
