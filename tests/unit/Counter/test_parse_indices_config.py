615e65901a60895862366161a3818dec0f10a357
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-28 19:54:23 +0200

    update Test_parse_indices_config

--- tests/unit/Counter/test_parse_indices_config.py
+++ tests/unit/Counter/test_parse_indices_config.py
@@ -5,6 +5,8 @@ import unittest
 class Test_parse_indices_config(unittest.TestCase):
 
     def test_1(self):
-        Counter.parse_indices_config((None, None, '(*)', '(*)', '\\1', '\\2'))
+        actual = Counter.parse_indices_config((None, None, '(*)', '(*)', '\\1', '\\2'))
+        expected = ([None, None, None, None, 2, 3], [0, 0, '*', '*', '\\1', '\\2'])
+        self.assertEqual(expected, actual)
 
 ##__________________________________________________________________||
