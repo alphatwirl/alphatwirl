131f132f0a5ffbdf99da159cc95a8c37c053fcf9
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-31 16:50:38 +0200

    add a new file test_BackrefMultipleArrayReader

---
+++ tests/unit/Counter/test_BackrefMultipleArrayReader.py
@@ -0,0 +1,9 @@
+import unittest
+import AlphaTwirl.Counter as Counter
+
+##__________________________________________________________________||
+class BackrefMultipleArrayReader(unittest.TestCase):
+
+    def test_one(self):
+        pass
+##__________________________________________________________________||
