90d2f5b141f8fcc1e92b2ba520a86d4b1283d0e9
Tai Sakuma <tai.sakuma@gmail.com>
2016-07-31 17:29:50 +0200

    clean up the code in KeyValueComposer

--- AlphaTwirl/Counter/KeyValueComposer.py
+++ AlphaTwirl/Counter/KeyValueComposer.py
@@ -40,14 +40,11 @@ class KeyValueComposer(object):
     def begin(self, event):
         attr_names = self.keyAttrNames + self.valAttrNames
         idxs_conf = self.keyIndices + self.valIndices
-        self.backref_idxs, idxs_conf = parse_indices_config(idxs_conf)
-        arrays = self._collect_arrays(event,  attr_names) 
+        backref_idxs, idxs_conf = parse_indices_config(idxs_conf)
+        arrays = self._collect_arrays(event,  attr_names)
 
         self._active = True if arrays else False
-
-        self._use_backref = any([e is not None for e in self.backref_idxs])
-
-        self._zipped = self._zip_arrays(arrays, idxs_conf, self.backref_idxs) if arrays is not None else None
+        self._array_reader = BackrefMultipleArrayReader(arrays, idxs_conf, backref_idxs)
 
     def _collect_arrays(self, event, attr_names):
         ret = [ ]
@@ -61,24 +58,15 @@ class KeyValueComposer(object):
             ret.append(attr)
         return ret
 
-    def _zip_arrays(self, arrays, idxs_conf, backref_idxs):
-        self._array_reader = BackrefMultipleArrayReader(arrays, idxs_conf, backref_idxs)
-        array_idxs = range(len(arrays))
-        return zip(array_idxs, arrays, idxs_conf, backref_idxs)
-
     def __call__(self, event):
         if not self._active: return None, None
-
         varis = self._array_reader.read()
-
-        # if self._zipped is None: return ()
-        # varis = self._read_zipped(self._zipped, self.backref_idxs)
-        key, val =  self._seprate_into_keys_and_vals_2(varis)
+        key, val =  self._seprate_into_keys_and_vals(varis)
         key = self._apply_binnings(self.binnings, key)
         key, val = self._remove_None(key, val)
         return key, val
 
-    def _seprate_into_keys_and_vals_2(self, varis):
+    def _seprate_into_keys_and_vals(self, varis):
         key = [v[:len(self.keyAttrNames)] for v in varis] if self.keyAttrNames else None
         val = [v[len(self.keyAttrNames):] for v in varis] if self.valAttrNames else None
         return key, val
@@ -88,25 +76,25 @@ class KeyValueComposer(object):
         return tuple(tuple(b(k) for b, k in zip(binnings, kk)) for kk in keys)
 
     def _remove_None(self, key, val):
-        if key is None:
-            if val is None:
-                return key, val
-            else:
-                idxs = tuple(i for i, e in enumerate(val) if None not in e)
-                val = tuple(val[i] for i in idxs)
-                return key, val
-        else:
-            if val is None:
-                idxs = tuple(i for i, e in enumerate(key) if None not in e)
-                key = tuple(key[i] for i in idxs)
-                return key, val
-            else:
-                idxs_key = set(i for i, e in enumerate(key) if None not in e)
-                idxs_val = set(i for i, e in enumerate(val) if None not in e)
-                idxs = idxs_key & idxs_val # intersection
-                idxs = sorted(list(idxs))
-                key = tuple(key[i] for i in idxs)
-                val = tuple(val[i] for i in idxs)
-                return key, val
+        if key is None and val is None:
+            return key, val
+
+        if key is None and val is not None:
+            idxs = tuple(i for i, e in enumerate(val) if None not in e)
+            val = tuple(val[i] for i in idxs)
+            return key, val
+
+        if key is not None and val is None:
+            idxs = tuple(i for i, e in enumerate(key) if None not in e)
+            key = tuple(key[i] for i in idxs)
+            return key, val
+
+        idxs_key = set(i for i, e in enumerate(key) if None not in e)
+        idxs_val = set(i for i, e in enumerate(val) if None not in e)
+        idxs = idxs_key & idxs_val # intersection
+        idxs = sorted(list(idxs))
+        key = tuple(key[i] for i in idxs)
+        val = tuple(val[i] for i in idxs)
+        return key, val
 
 ##__________________________________________________________________||
