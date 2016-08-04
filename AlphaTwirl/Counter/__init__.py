9a5e3a7c54b04d56d34c7848762bc3ab803ff49b
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-04 15:01:29 +0200

    import BackrefMultipleArrayReader to  AlphaTwirl.Counter

--- AlphaTwirl/Counter/__init__.py
+++ AlphaTwirl/Counter/__init__.py
@@ -6,3 +6,4 @@ from KeyValueComposer import KeyValueComposer
 from parse_indices_config import parse_indices_config
 from NextKeyComposer import NextKeyComposer
 from Counts import Counts
+from BackrefMultipleArrayReader import BackrefMultipleArrayReader
