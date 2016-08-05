ff009996b1fcf3a2a346d62c191d7573af4f3718
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-05 14:09:00 +0200

    clean up the code in BackrefMultipleArrayReader

--- AlphaTwirl/Counter/BackrefMultipleArrayReader.py
+++ AlphaTwirl/Counter/BackrefMultipleArrayReader.py
@@ -1,5 +1,6 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 import itertools
+import numbers
 
 ##__________________________________________________________________||
 class BackrefMultipleArrayReader(object):
@@ -12,171 +13,203 @@ class BackrefMultipleArrayReader(object):
                 )
             )
 
-        self.backref_idxs = backref_idxs
-        self._use_backref = any([e is not None for e in backref_idxs])
-        self._zipped = zip(range(len(arrays)), arrays, idxs_conf, backref_idxs)
-
         self.arrays = arrays
+        self.wildcard_conf = tuple(c == '*' for c in idxs_conf)
+        self.idxs_conf = tuple(c if isinstance(c, numbers.Number) else None for c in idxs_conf)
+        self.backref_idxs = backref_idxs
+
+        use_backref = any([e is not None for e in backref_idxs])
+        if not use_backref:
+            self._read = self._fast_read_without_backref
+            self._zipped = zip(self.arrays, self.idxs_conf, self.wildcard_conf)
+            return
+
+        # use backref
+        self._read = self._read_with_backref
+
+        # e.g.
+        # self.arrays = [[], [], [], [], [], [], [], []]
+        # self.idxs_conf = (0, None, None, None, None, None, None, None)
+        # self.wildcard_conf = (False, True, False, True, False, False, False, False)
+        # self.backref_idxs = [None, None, 1, None, 3, 1, 1, 3]
+
+        self.uniq_idxs = [ ]
+        self.ref_idxs = [ ]
+        for i in self.backref_idxs:
+            if i is None:
+                l = [ ]
+                self.uniq_idxs.append(l)
+                self.ref_idxs.append(l)
+            else:
+                self.ref_idxs.append(self.ref_idxs[i])
+
+        # e.g.,
+        # self.uniq_idxs = [[], [], []]
+        # [id(o) for o in self.uniq_idxs] = [4738625048, 4738625480, 4738625120]
+        # self.ref_idxs = [[], [], [], [], [], [], [], []]
+        # [id(o) for o in self.ref_idxs] = [4738625048, 4738625480, 4738625480, 4738625120, 4738625120, 4738625480, 4738625480, 4738625120]
+
+        self._zipped_1 = zip(self.arrays, self.idxs_conf, self.wildcard_conf, self.ref_idxs)
+        self._zipped_2 = zip(self.arrays, self.ref_idxs)
+
 
     def read(self):
-        return self._read_zipped(self._zipped, self.backref_idxs)
+        return self._read()
 
-    def _read_zipped(self, zipped, backref_idxs):
+    def _fast_read_without_backref(self):
 
-        varis = self._unzip_and_read_arrays(zipped)
         # e.g.,
-        # varis = [
-        #     [1001],
-        #     [15.3, 12.9, 9.2, 10.5],
-        #     [-1.2, 5.2, 2.2, 0.5],
-        #     [20.2, 11.9, 13.3, 5.2],
-        #     [2.2, 1.2, -1.5, -0.5],
-        #     [0.1, 0.6, 1.2, None],
-        #     [16.2, 13.1, 10.1, 11.8],
-        #     [22.1, 15.2, 16.3, None]
+        # self.idxs_conf = (1, None, None, 2, None)
+        # self.wildcard_conf = (False, True, True, False, True)
+        # self.arrays = [
+        #     [55, 66, 77],
+        #     [12, 13, 14],
+        #     [104, 105],
+        #     [222, 333, 444, 555],
+        #     [1001, 1002, 1003]
         # ]
 
-        if not self._use_backref:
-            # fast path without backref
-            return tuple(itertools.product(*varis))
-
+        # read arrays
+        vals = [ ]
+        for array, idx, wild in self._zipped:
+            if wild:
+                vals.append(list(array))
+                continue
+            if idx < len(array):
+                vals.append([array[idx]])
+                continue
+            # idx is out of the range
+            vals.append([ ])
         # e.g.,
-        # backref_idxs = [None, None, 1, None, 3, 1, 1, 3]
-
-        uniq_idxs, ref_key_idxs = self._build_uniq_ref_idxs(varis, backref_idxs)
-        # e.g.,
-        # uniq_idxs = [
-        #     [0],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3]
-        # ]
-        #
-        # ref_key_idxs = [
-        #     [0],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        #     [0, 1, 2, 3],
-        # ]
-        #
-        # these 5 lists are the same object:
-        #   uniq_idxs[1]
-        #   ref_key_idxs[1]
-        #   ref_key_idxs[2]
-        #   ref_key_idxs[5]
-        #   ref_key_idxs[]
-        #
-        # so are these 3 lists:
-        #   uniq_idxs[2]
-        #   ref_key_idxs[3]
-        #   ref_key_idxs[4]
-        #   ref_key_idxs[7]
-
-        self._expand_idxs_with_all_combinations(uniq_idxs)
-        # e.g.,
-        # uniq_idxs = [
-        #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #     [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
-        #     [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
-        # ]
-        #
-        # e.g.,
-        # ref_key_idxs = [
-        #     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        #     [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
-        #     [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
-        #     [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
-        #     [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3],
-        #     [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
-        #     [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
-        #     [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
+        # vals = [
+        #     [66],
+        #     [12, 13, 14],
+        #     [104, 105],
+        #     [444],
+        #     [1001, 1002, 1003]
         # ]
 
-        varis = self._build_ret(varis, ref_key_idxs)
+        # expand with all combinations
+        ret =  tuple(itertools.product(*vals))
         # e.g.,
-        # varis = (
-        #     (1001, 15.3, -1.2, 20.2, 2.2, 0.1, 16.2, 22.1),
-        #     (1001, 15.3, -1.2, 11.9, 1.2, 0.1, 16.2, 15.2),
-        #     (1001, 15.3, -1.2, 13.3, -1.5, 0.1, 16.2, 16.3),
-        #     (1001, 15.3, -1.2, 5.2, -0.5, 0.1, 16.2, None),
-        #     (1001, 12.9, 5.2, 20.2, 2.2, 0.6, 13.1, 22.1),
-        #     (1001, 12.9, 5.2, 11.9, 1.2, 0.6, 13.1, 15.2),
-        #     (1001, 12.9, 5.2, 13.3, -1.5, 0.6, 13.1, 16.3),
-        #     (1001, 12.9, 5.2, 5.2, -0.5, 0.6, 13.1, None),
-        #     (1001, 9.2, 2.2, 20.2, 2.2, 1.2, 10.1, 22.1),
-        #     (1001, 9.2, 2.2, 11.9, 1.2, 1.2, 10.1, 15.2),
-        #     (1001, 9.2, 2.2, 13.3, -1.5, 1.2, 10.1, 16.3),
-        #     (1001, 9.2, 2.2, 5.2, -0.5, 1.2, 10.1, None),
-        #     (1001, 10.5, 0.5, 20.2, 2.2, None, 11.8, 22.1),
-        #     (1001, 10.5, 0.5, 11.9, 1.2, None, 11.8, 15.2),
-        #     (1001, 10.5, 0.5, 13.3, -1.5, None, 11.8, 16.3),
-        #     (1001, 10.5, 0.5, 5.2, -0.5, None, 11.8, None)
+        # ret = (
+        #     (66, 12, 104, 444, 1001),
+        #     (66, 12, 104, 444, 1002),
+        #     (66, 12, 104, 444, 1003),
+        #     (66, 12, 105, 444, 1001),
+        #     (66, 12, 105, 444, 1002),
+        #     (66, 12, 105, 444, 1003),
+        #     (66, 13, 104, 444, 1001),
+        #     (66, 13, 104, 444, 1002),
+        #     (66, 13, 104, 444, 1003),
+        #     (66, 13, 105, 444, 1001),
+        #     (66, 13, 105, 444, 1002),
+        #     (66, 13, 105, 444, 1003),
+        #     (66, 14, 104, 444, 1001),
+        #     (66, 14, 104, 444, 1002),
+        #     (66, 14, 104, 444, 1003),
+        #     (66, 14, 105, 444, 1001),
+        #     (66, 14, 105, 444, 1002),
+        #     (66, 14, 105, 444, 1003)
         # )
 
-        varis = self._remove_None(varis)
-        # e.g.,
-        # varis = (
-        #     (1001, 15.3, -1.2, 20.2, 2.2, 0.1, 16.2, 22.1),
-        #     (1001, 15.3, -1.2, 11.9, 1.2, 0.1, 16.2, 15.2),
-        #     (1001, 15.3, -1.2, 13.3, -1.5, 0.1, 16.2, 16.3),
-        #     (1001, 12.9, 5.2, 20.2, 2.2, 0.6, 13.1, 22.1),
-        #     (1001, 12.9, 5.2, 11.9, 1.2, 0.6, 13.1, 15.2),
-        #     (1001, 12.9, 5.2, 13.3, -1.5, 0.6, 13.1, 16.3),
-        #     (1001, 9.2, 2.2, 20.2, 2.2, 1.2, 10.1, 22.1),
-        #     (1001, 9.2, 2.2, 11.9, 1.2, 1.2, 10.1, 15.2),
-        #     (1001, 9.2, 2.2, 13.3, -1.5, 1.2, 10.1, 16.3)
-        # )
-
-        return varis
-
-    def _unzip_and_read_arrays(self, zipped):
-        backref_map = { }
-        varis = [ ]
-        for var_idx, attr, conf_attr_idx, backref_idx in zipped:
-            attr_idxs = self._determine_array_indices_to_read(attr, conf_attr_idx, var_idx, backref_idx, backref_map)
-            attr_vals = [(attr[i] if i < len(attr) else None) for i in attr_idxs]
-            varis.append(attr_vals)
-        return varis
-
-    def _determine_array_indices_to_read(self, attr, conf_attr_idx, var_idx, backref_idx, backref_map):
-        if backref_idx is None:
-            if conf_attr_idx == '*': ret = range(len(attr))
-            elif conf_attr_idx < len(attr): ret = [conf_attr_idx]
-            else: ret = [ ] # conf_attr_idx is out of the range
-        else:
-            ret = backref_map[backref_idx]
-        backref_map[var_idx] = ret
         return ret
 
-    def _build_uniq_ref_idxs(self, keys, backref_idxs):
-        uniq_idxs = [ ]
-        ref_idxs = [ ]
-        for keys, backrefIdx in zip(keys, backref_idxs):
-            if backrefIdx is None:
-                idxs = range(len(keys))
-                uniq_idxs.append(idxs)
-                ref_idxs.append(idxs)
-            else:
-                ref_idxs.append(ref_idxs[backrefIdx])
-        return uniq_idxs, ref_idxs
+    def _read_with_backref(self):
 
-    def _expand_idxs_with_all_combinations(self, idxs):
-        prod = tuple(itertools.product(*idxs))
-        for i in range(len(idxs)):
-            idxs[i][:] = [p[i] for p in prod]
+        # e.g.,
+        # self.arrays = [
+        #     [1001],
+        #     [12, 13, 14, 15],
+        #     [104, 105, 106, 107],
+        #     [51, 52],
+        #     [84, 85, 86],
+        #     [403, 404, 405],
+        #     [207, 208, 209, 210],
+        #     [91, 92, 93]
+        # ]
 
-    def _build_ret(self, varis, idxs):
-        if not idxs: return tuple()
-        ret = [ ]
-        for i in range(len(idxs[0])):
-            ret.append(tuple([b[subidxs[i]] for b, subidxs in zip(varis, idxs)]))
-        val = [ ]
-        return tuple(ret)
+        # initialize self.uniq_idxs
+        for i in self.uniq_idxs:
+            i[:] = [ ]
+        # e.g.,
+        # self.uniq_idxs = [[], [], []]
+        # self.ref_idxs = [[], [], [], [], [], [], [], []]
 
-    def _remove_None(self, varis):
-        return tuple(v for v in varis if None not in v)
+
+        # fill self.ref_idxs
+        for array, idx, wild, ref in self._zipped_1:
+            larray = len(array)
+            if idx is not None:
+                ref[:] = [idx] if idx < larray else [ ]
+                continue
+            if wild:
+                ref[:] = range(larray)
+                continue
+            # backref
+            ref[:] = [j for j in ref if j < larray]
+        # e.g.,
+        # self.uniq_idxs = [
+        #     [0],
+        #     [0, 1, 2],
+        #     [0, 1]
+        # ]
+        # self.ref_idxs = [
+        #     [0],
+        #     [0, 1, 2],
+        #     [0, 1, 2],
+        #     [0, 1],
+        #     [0, 1],
+        #     [0, 1, 2],
+        #     [0, 1, 2],
+        #     [0, 1]
+        # ]
+
+        # expand self.uniq_idxs with all combinations
+        prod = tuple(itertools.product(*self.uniq_idxs))
+        for i in range(len(self.uniq_idxs)):
+            self.uniq_idxs[i][:] = [p[i] for p in prod]
+        # e.g.,
+        # self.uniq_idxs = [
+        #     [0, 0, 0, 0, 0, 0],
+        #     [0, 0, 1, 1, 2, 2],
+        #     [0, 1, 0, 1, 0, 1]
+        # ]
+        # self.ref_idxs = [
+        #     [0, 0, 0, 0, 0, 0],
+        #     [0, 0, 1, 1, 2, 2],
+        #     [0, 0, 1, 1, 2, 2],
+        #     [0, 1, 0, 1, 0, 1],
+        #     [0, 1, 0, 1, 0, 1],
+        #     [0, 0, 1, 1, 2, 2],
+        #     [0, 0, 1, 1, 2, 2],
+        #     [0, 1, 0, 1, 0, 1]
+        # ]
+
+        ret = [[a[j] for j in i] for a, i in self._zipped_2]
+        # e.g.,
+        # ret = [
+        #     [1001, 1001, 1001, 1001, 1001, 1001],
+        #     [12, 12, 13, 13, 14, 14],
+        #     [104, 104, 105, 105, 106, 106],
+        #     [51, 52, 51, 52, 51, 52],
+        #     [84, 85, 84, 85, 84, 85],
+        #     [403, 403, 404, 404, 405, 405],
+        #     [207, 207, 208, 208, 209, 209],
+        #     [91, 92, 91, 92, 91, 92]
+        # ]
+
+        # take transpose
+        ret = tuple(map(tuple, zip(*ret)))
+        # e.g.,
+        # ret = (
+        #     (1001, 12, 104, 51, 84, 403, 207, 91),
+        #     (1001, 12, 104, 52, 85, 403, 207, 92),
+        #     (1001, 13, 105, 51, 84, 404, 208, 91),
+        #     (1001, 13, 105, 52, 85, 404, 208, 92),
+        #     (1001, 14, 106, 51, 84, 405, 209, 91),
+        #     (1001, 14, 106, 52, 85, 405, 209, 92)
+        # )
+        return ret
 
 ##__________________________________________________________________||
