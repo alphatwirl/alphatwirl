37492475686092ece6158660043eeeb138fe9faf
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-06 23:14:57 +0200

    update Combine for Count

--- AlphaTwirl/Combine.py
+++ AlphaTwirl/Combine.py
@@ -7,16 +7,12 @@ class Combine(object):
         for datasetName, reader in datasetReaderPairs:
             counts = reader.results()
             if not counts: continue
-            counts = dict([((datasetName, )+ k, v.copy()) for k, v in counts.iteritems()])
+            counts = dict([((datasetName, ) + k, v.copy()) for k, v in counts.iteritems()])
             for k, v in counts.iteritems():
                 if k not in combined:
                     combined[k] = v
                 else:
-                    for kk in combined[k].iterkeys():
-                        if kk in v:
-                            combined[k][kk] += v[kk]
-                        else:
-                            combined[k][kk] = v[kk]
+                    combined[k] += v
         return combined
 
 ##__________________________________________________________________||
