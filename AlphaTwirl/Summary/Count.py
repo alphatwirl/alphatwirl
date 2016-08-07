60b4bcb9413105b0b0547c94bafdc5fc130060d9
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-07 13:51:04 +0200

    add __add__, __iadd__, __radd__ to Count

--- AlphaTwirl/Summary/Count.py
+++ AlphaTwirl/Summary/Count.py
@@ -26,4 +26,28 @@ class Count(object):
     def results(self):
         return self._results
 
+    def __add__(self, other):
+        ret = Count()
+        results = {k: v.copy() for k, v in self._results.iteritems()}
+        if not other == 0: # other is 0 when e.g. sum([obj1, obj2])
+            self._add_results_inplace(results, other._results)
+        ret._results.clear()
+        ret._results.update(results)
+        return ret
+
+    def __iadd__(self, other):
+        self._add_results_inplace(self._results, other._results)
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def _add_results_inplace(self, res1, res2):
+        # res1 += res2, modify res1
+        for k, v in res2.iteritems():
+            if k not in self._results:
+                res1[k] = v.copy()
+            else:
+                res1[k] = self._results[k] + v
+
 ##__________________________________________________________________||
