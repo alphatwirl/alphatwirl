632ec51b0cce881bb8fdd4d466a4ff6387cd2efe
Tai Sakuma <tai.sakuma@gmail.com>
2016-08-07 10:08:15 +0200

    update CombineIntoList for Count

--- tests/unit/test_CombineIntoList.py
+++ tests/unit/test_CombineIntoList.py
@@ -1,6 +1,6 @@
 from AlphaTwirl import countsToList, CombineIntoList
-import collections
 import unittest
+import numpy as np
 
 ##__________________________________________________________________||
 class MockReader(object):
@@ -19,55 +19,37 @@ class TestCountsToList(unittest.TestCase):
     def test_call(self):
 
         counts  = {
-            (1, ): {'n': 4.0, 'nvar': 6.0},
-            (2, ): {'n': 3.0, 'nvar': 9.0},
-            (3, ): {'n': 2.0, 'nvar': 3.0},
+            (1, ): np.array((4, 6)),
+            (2, ): np.array((3, 9)),
+            (3, ): np.array((2, 3)),
         }
 
         expected = [
-            ('v1', 'nvar', 'n'),
-            (1, 6.0, 4.0),
-            (2, 9.0, 3.0),
-            (3, 3.0, 2.0),
-        ]
-
-        columns = ("v1", )
-        self.assertEqual(expected, countsToList(counts, columns))
-
-    def test_call_ordered_values(self):
-
-        counts  = {
-            (1, ): collections.OrderedDict((('n', 4.0), ('nvar', 6.0))),
-            (2, ): collections.OrderedDict((('n', 3.0), ('nvar', 9.0))),
-            (3, ): collections.OrderedDict((('n', 2.0), ('nvar', 3.0))),
-            }
-
-        expected = [
             ('v1', 'n', 'nvar'),
-            (1, 4.0, 6.0),
-            (2, 3.0, 9.0),
-            (3, 2.0, 3.0),
+            (1, 4, 6),
+            (2, 3, 9),
+            (3, 2, 3),
         ]
 
-        columns = ("v1", )
+        columns = ('v1', 'n', 'nvar')
         self.assertEqual(expected, countsToList(counts, columns))
 
     def test_call_threeValues(self):
 
         counts  = {
-            (1, ): {'n': 4.0, 'nvar': 6.0, 'skewness': 2.3 },
-            (2, ): {'n': 3.0, 'nvar': 9.0, 'skewness': 5.4 },
-            (3, ): {'n': 2.0, 'nvar': 3.0, 'skewness': 3.6 },
+            (1, ): np.array((4, 6, 2.3)),
+            (2, ): np.array((3, 9, 5.4)),
+            (3, ): np.array((2, 3, 3.6)),
             }
 
         expected = [
-            ('v1', 'nvar', 'skewness', 'n'),
-            (1, 6.0, 2.3, 4.0),
-            (2, 9.0, 5.4, 3.0),
-            (3, 3.0, 3.6, 2.0)
+            ('v1', 'n', 'nvar', 'skewness'),
+            (1, 4.0, 6.0, 2.3),
+            (2, 3.0, 9.0, 5.4),
+            (3, 2.0, 3.0, 3.6)
         ]
 
-        columns = ("v1", )
+        columns = ('v1', 'n', 'nvar', 'skewness')
         self.assertEqual(expected, countsToList(counts, columns))
 
 ##__________________________________________________________________||
@@ -76,37 +58,37 @@ class TestCombineIntoList(unittest.TestCase):
     def test_combine_oneReader(self):
 
         counts  = {
-            (1, ): {'n': 4.0, 'nvar': 6.0},
-            (2, ): {'n': 3.0, 'nvar': 9.0},
-            (3, ): {'n': 2.0, 'nvar': 3.0},
+            (1, ): np.array((4, 6)),
+            (2, ): np.array((3, 9)),
+            (3, ): np.array((2, 3)),
             }
 
         reader = MockReader(counts)
         datasetReaderPairs = [('data1', reader), ]
 
         expected = [
-            ('component', 'v1', 'nvar', 'n'),
-            ('data1', 1, 6.0, 4.0),
-            ('data1', 2, 9.0, 3.0),
-            ('data1', 3, 3.0, 2.0),
+            ('component', 'v1', 'n', 'nvar'),
+            ('data1', 1, 4, 6),
+            ('data1', 2, 3, 9),
+            ('data1', 3, 2, 3),
         ]
 
-        combine = CombineIntoList(keyNames = ('v1', ))
+        combine = CombineIntoList(keyNames = ('v1', ), valNames = ('n', 'nvar'))
         self.assertEqual(expected, combine.combine(datasetReaderPairs))
 
     def test_combine_twoReaders(self):
 
         counts1  = {
-            (1, ): {'n': 4.0, 'nvar': 6.0},
-            (2, ): {'n': 3.0, 'nvar': 9.0},
-            (3, ): {'n': 2.0, 'nvar': 3.0},
+            (1, ): np.array((4, 6)),
+            (2, ): np.array((3, 9)),
+            (3, ): np.array((2, 3)),
             }
 
         reader1 = MockReader(counts1)
 
         counts2  = {
-            (2, ): {'n': 3.0, 'nvar': 6.0},
-            (4, ): {'n': 2.0, 'nvar': 2.0},
+            (2, ): np.array((3, 6)),
+            (4, ): np.array((2, 2)),
             }
 
         reader2 = MockReader(counts2)
@@ -114,23 +96,23 @@ class TestCombineIntoList(unittest.TestCase):
         datasetReaderPairs = [('data1', reader1), ('data2', reader2)]
 
         expected = [
-            ('component', 'v1', 'nvar', 'n'),
-            ('data1', 1, 6.0, 4.0),
-            ('data1', 2, 9.0, 3.0),
-            ('data1', 3, 3.0, 2.0),
-            ('data2', 2, 6.0, 3.0),
-            ('data2', 4, 2.0, 2.0),
+            ('component', 'v1', 'n', 'nvar'),
+            ('data1', 1, 4, 6),
+            ('data1', 2, 3, 9),
+            ('data1', 3, 2, 3),
+            ('data2', 2, 3, 6),
+            ('data2', 4, 2, 2),
         ]
 
-        combine = CombineIntoList(keyNames = ('v1', ))
+        combine = CombineIntoList(keyNames = ('v1', ), valNames = ('n', 'nvar'))
         self.assertEqual(expected, combine.combine(datasetReaderPairs))
 
     def test_combine_with_empty_counts(self):
 
         counts1  = {
-            (1, ): {'n': 4.0, 'nvar': 6.0},
-            (2, ): {'n': 3.0, 'nvar': 9.0},
-            (3, ): {'n': 2.0, 'nvar': 3.0},
+            (1, ): np.array((4, 6)),
+            (2, ): np.array((3, 9)),
+            (3, ): np.array((2, 3)),
             }
 
         reader1 = MockReader(counts1)
@@ -142,13 +124,13 @@ class TestCombineIntoList(unittest.TestCase):
         datasetReaderPairs = [('data1', reader1), ('data2', reader2)]
 
         expected = [
-            ('component', 'v1', 'nvar', 'n'),
-            ('data1', 1, 6.0, 4.0),
-            ('data1', 2, 9.0, 3.0),
-            ('data1', 3, 3.0, 2.0),
+            ('component', 'v1', 'n', 'nvar'),
+            ('data1', 1, 4, 6),
+            ('data1', 2, 3, 9),
+            ('data1', 3, 2, 3),
         ]
 
-        combine = CombineIntoList(keyNames = ('v1', ))
+        combine = CombineIntoList(keyNames = ('v1', ), valNames = ('n', 'nvar'))
         self.assertEqual(expected, combine.combine(datasetReaderPairs))
 
     def test_combine_all_empty_counts(self):
@@ -167,14 +149,14 @@ class TestCombineIntoList(unittest.TestCase):
             ('component', 'v1', 'n', 'nvar'),
         ]
 
-        combine = CombineIntoList(keyNames = ('v1', ))
+        combine = CombineIntoList(keyNames = ('v1', ), valNames = ('n', 'nvar'))
         self.assertEqual(expected, combine.combine(datasetReaderPairs))
 
     def test_combine_empty_pairs(self):
 
         datasetReaderPairs = [ ]
 
-        combine = CombineIntoList(keyNames = ('v1', ))
+        combine = CombineIntoList(keyNames = ('v1', ), valNames = ('n', 'nvar'))
         self.assertEqual(None, combine.combine(datasetReaderPairs))
 
 ##__________________________________________________________________||
