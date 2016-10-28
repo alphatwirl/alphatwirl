2c85638b4e74bc2ab67c90e495ba9bcaf8062f44
Tai Sakuma <tai.sakuma@gmail.com>
2016-10-28 18:31:59 +0200

    comment out sphinx_bootstrap_theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 import shlex
-import sphinx_bootstrap_theme
+# import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -135,7 +135,7 @@ html_theme = 'bizstyle'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
