f8fcae7dd7579b51c3c204337dfa70c702fdbf38
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-22 14:54:05 +0100

    add new namedtuple Chunk

---
+++ AlphaTwirl/HeppyResult/Chunk.py
@@ -0,0 +1,9 @@
+# Tai Sakuma <tai.sakuma@cern.ch>
+
+##__________________________________________________________________||
+import collections
+
+##__________________________________________________________________||
+Chunk = collections.namedtuple('Chunk', 'inputPath treeName maxEvents start component name')
+
+##__________________________________________________________________||
