6d671cd0e0e326c8592722d334dc7646850b2bf8
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-22 18:11:19 +0100

    receive a chunk of a data set

--- tests/unit/Loop/test_EventLoop.py
+++ tests/unit/Loop/test_EventLoop.py
@@ -9,14 +9,12 @@ class MockEventBuilder(object):
     def __init__(self, events):
         self.events = events
 
-    def build(self, dataset, start, nEvents):
-        self.dataset = dataset
-        self.start = start
-        self.nEvents = nEvents
+    def build(self, chunk):
+        self.chunk = chunk
         return self.events
 
 ##__________________________________________________________________||
-class MockDataset(object): pass
+class MockChunk(object): pass
 
 ##__________________________________________________________________||
 class MockReader(object):
@@ -42,11 +40,11 @@ class TestEventLoop(unittest.TestCase):
         events = [MockEvent(), MockEvent(), MockEvent()]
         eventBuilder = MockEventBuilder(events)
 
-        dataset = MockDataset()
+        chunk = MockChunk()
 
         reader = MockReader()
 
-        obj = EventLoop(eventBuilder, dataset, reader, start = 100, nEvents = 10000)
+        obj = EventLoop(eventBuilder, chunk, reader)
 
         self.assertIsNone(reader.beginCalled)
         self.assertFalse(reader.endCalled)
@@ -54,35 +52,7 @@ class TestEventLoop(unittest.TestCase):
 
         self.assertEqual(reader, obj())
 
-        self.assertEqual(dataset, eventBuilder.dataset)
-        self.assertEqual(100, eventBuilder.start)
-        self.assertEqual(10000, eventBuilder.nEvents)
-
-        self.assertIs(events, reader.beginCalled)
-        self.assertIsNot(events, reader.events)
-        self.assertEqual(events, reader.events)
-        self.assertTrue(reader.endCalled)
-
-    def test_call_default_options(self):
-
-        events = [MockEvent(), MockEvent(), MockEvent()]
-        eventBuilder = MockEventBuilder(events)
-
-        dataset = MockDataset()
-
-        reader = MockReader()
-
-        obj = EventLoop(eventBuilder, dataset, reader) # don't give start or nEvents
-
-        self.assertIsNone(reader.beginCalled)
-        self.assertFalse(reader.endCalled)
-        self.assertEqual([ ], reader.events)
-
-        self.assertEqual(reader, obj())
-
-        self.assertEqual(dataset, eventBuilder.dataset)
-        self.assertEqual(0, eventBuilder.start) # the default value
-        self.assertEqual(-1, eventBuilder.nEvents) # the default value
+        self.assertEqual(chunk, eventBuilder.chunk)
 
         self.assertIs(events, reader.beginCalled)
         self.assertIsNot(events, reader.events)
