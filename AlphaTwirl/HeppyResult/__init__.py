480b711aca6a11521deccd95d9d596ff5610fc0e
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-22 18:12:00 +0100

    update HeppyResult.__init__

--- AlphaTwirl/HeppyResult/__init__.py
+++ AlphaTwirl/HeppyResult/__init__.py
@@ -3,6 +3,7 @@ from ComponentReaderComposite import ComponentReaderComposite
 from HeppyResult import HeppyResult, defaultExcludeList, componentHasTheseFiles
 from Component import Component
 from ReadComponentConfig import ReadComponentConfig
+from Chunk import Chunk
 from ReadVersionInfo import ReadVersionInfo
 from Analyzer import Analyzer
 from ReadCounter import ReadCounter
@@ -11,10 +12,16 @@ from TblCounterLong import TblCounterLong
 from TblComponentConfig import TblComponentConfig
 from TblBrilCalc import TblBrilCalc
 
+hasROOT = False
 try:
+    import ROOT
+    hasROOT = True
+except ImportError:
+    pass
+
+if hasROOT:
+    from ComponentSplitter import ComponentSplitter
     from EventBuilder import EventBuilder
     from BEventBuilder import BEventBuilder
     from TblBranch import TblBranch
     from TblTree import TblTree
-except ImportError:
-    pass
