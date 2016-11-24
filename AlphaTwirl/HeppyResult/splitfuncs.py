8bc9ed5d980e7d94b3f6a45d4b3aa1512c8d8651
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-24 19:04:38 +0100

    add functions to splitfuncs.py

--- AlphaTwirl/HeppyResult/splitfuncs.py
+++ AlphaTwirl/HeppyResult/splitfuncs.py
@@ -1,8 +1,35 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 
 ##__________________________________________________________________||
+def create_file_start_length_list(file_nevents_list, max_per_run = -1, max_total = -1):
+
+    file_nevents_list = _apply_max_total(file_nevents_list, max_total)
+
+    ret = [ ]
+    for file, nevents in file_nevents_list:
+        start_length_pairs = start_length_pairs_for_split_lists(nevents, max_per_run)
+        for start, length in start_length_pairs:
+            ret.append((file, start, length))
+    return ret
+
+##__________________________________________________________________||
+def _apply_max_total(file_nevents_list, max_total = -1):
+
+    if max_total < 0: return file_nevents_list
+
+    ret = [ ]
+    for file, nevents in file_nevents_list:
+        if max_total == 0: break
+        nevents = min(max_total, nevents)
+        ret.append((file, nevents))
+        max_total -= nevents
+    return ret
+
+##__________________________________________________________________||
 def start_length_pairs_for_split_lists(ntotal, max_per_list):
     # e.g., ntotal =  35, max_per_list = 10
+
+    if max_per_list < 0: return [(0, ntotal)]
 
     nlists = ntotal/max_per_list
     # nlists = 3
