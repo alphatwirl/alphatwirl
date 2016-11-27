521cb93bf539a050cc64437b5ac5e85721332814
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-27 18:32:11 +0100

    remove Sum, the same as Summarizer, and rename SumImp Sum

--- AlphaTwirl/Summary/Sum.py
+++ AlphaTwirl/Summary/Sum.py
@@ -2,10 +2,9 @@
 
 ##__________________________________________________________________||
 import numpy as np
-import copy
 
 ##__________________________________________________________________||
-class SumImp(object):
+class Sum(object):
 
     def __init__(self, val = None, weight = 1, contents = None):
 
@@ -13,60 +12,13 @@ class SumImp(object):
             self.contents = np.array(contents)
             return
 
-        self.contents = np.array(val*weight)
+        self.contents = np.array(val)*weight
 
     def __add__(self, other):
         contents = self.contents + other.contents
         return self.__class__(contents = contents)
 
-##__________________________________________________________________||
-class Sum(object):
-    def __init__(self, initial_val = (0, )):
-        self._results = { }
-        self.Summary = SumImp
-        self.initial_contents = initial_val
-
-    def add(self, key, val, weight = 1):
-        self.add_key(key)
-        self._results[key] = self._results[key] + self.Summary(val, weight)
-
-    def add_key(self, key):
-        if key not in self._results:
-            self._results[key] = self.Summary(contents = self.initial_contents)
-
-    def keys(self):
-        return self._results.keys()
-
-    def copy_from(self, src):
-        self._results.clear()
-        self._results.update(src._results)
-
-    def results(self):
-        ## return self._results
-        return {k: v.contents for k, v in self._results.iteritems()}
-
-    def __add__(self, other):
-        ret = self.__class__()
-        results = {k: copy.deepcopy(v) for k, v in self._results.iteritems()}
-        if not other == 0: # other is 0 when e.g. sum([obj1, obj2])
-            self._add_results_inplace(results, other._results)
-        ret._results.clear()
-        ret._results.update(results)
-        return ret
-
-    def __iadd__(self, other):
-        self._add_results_inplace(self._results, other._results)
-        return self
-
-    def __radd__(self, other):
-        return self.__add__(other)
-
-    def _add_results_inplace(self, res1, res2):
-        # res1 += res2, modify res1
-        for k, v in res2.iteritems():
-            if k not in self._results:
-                res1[k] = copy.deepcopy(v)
-            else:
-                res1[k] = self._results[k] + v
+    def __repr__(self):
+        return '{}(contents = {})'.format(self.__class__, self.contents)
 
 ##__________________________________________________________________||
