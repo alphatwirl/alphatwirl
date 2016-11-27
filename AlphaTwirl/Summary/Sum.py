d34f631dbf9f2a0f171f1f92c76a9310eb814e21
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-27 12:31:41 +0100

    make Sum similar to Count

--- AlphaTwirl/Summary/Sum.py
+++ AlphaTwirl/Summary/Sum.py
@@ -3,19 +3,34 @@
 ##__________________________________________________________________||
 import numpy as np
 
+class SumImp(object):
+
+    def __init__(self, val = None, weight = 1, contents = None):
+
+        if contents is not None:
+            self.contents = np.array(contents)
+            return
+
+        self.contents = np.array(val*weight)
+
+    def __add__(self, other):
+        contents = self.contents + other.contents
+        return self.__class__(contents = contents)
+
 ##__________________________________________________________________||
 class Sum(object):
     def __init__(self, initial_val = (0, )):
         self._results = { }
-        self.initial_val = initial_val
+        self.Summary = SumImp
+        self.initial_contents = initial_val
 
     def add(self, key, val, weight = 1):
         self.add_key(key)
-        self._results[key] = self._results[key] + np.array(val*weight)
+        self._results[key] = self._results[key] + self.Summary(val, weight)
 
     def add_key(self, key):
         if key not in self._results:
-            self._results[key] = np.array(self.initial_val)
+            self._results[key] = self.Summary(contents = self.initial_contents)
 
     def keys(self):
         return self._results.keys()
@@ -25,11 +40,12 @@ class Sum(object):
         self._results.update(src._results)
 
     def results(self):
-        return self._results
+        ## return self._results
+        return {k: v.contents for k, v in self._results.iteritems()}
 
     def __add__(self, other):
-        ret = Sum()
-        results = {k: v.copy() for k, v in self._results.iteritems()}
+        ret = self.__class__()
+        results = {k: copy.deepcopy(v) for k, v in self._results.iteritems()}
         if not other == 0: # other is 0 when e.g. sum([obj1, obj2])
             self._add_results_inplace(results, other._results)
         ret._results.clear()
@@ -47,7 +63,7 @@ class Sum(object):
         # res1 += res2, modify res1
         for k, v in res2.iteritems():
             if k not in self._results:
-                res1[k] = v.copy()
+                res1[k] = copy.deepcopy(v)
             else:
                 res1[k] = self._results[k] + v
 
