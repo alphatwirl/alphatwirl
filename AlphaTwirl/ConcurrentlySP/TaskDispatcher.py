05c9c6363ff8df6a09e0a8f7fac3550537e9bcc3
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-03 13:14:04 +0100

    add an option pipe

--- AlphaTwirl/ConcurrentlySP/TaskDispatcher.py
+++ AlphaTwirl/ConcurrentlySP/TaskDispatcher.py
@@ -5,19 +5,26 @@ import collections
 
 ##__________________________________________________________________||
 class TaskDispatcher(object):
-    def __init__(self):
+    def __init__(self, pipe = False):
         self.running_procs = collections.deque()
+        self.pipe = pipe
 
     def run(self, taskdir, package_path):
         run_script = os.path.join(taskdir, 'run.py')
         args = [run_script, package_path]
-        proc = subprocess.Popen(args)
+        proc = subprocess.Popen(
+            args,
+            stdout = subprocess.PIPE if self.pipe else None,
+            stderr = subprocess.PIPE if self.pipe else None,
+        )
         self.running_procs.append(proc)
 
     def wait(self):
+        ret = [ ] # [(stdout, stderr), ] a list of pairs of stdout and stderr
         while self.running_procs:
             proc = self.running_procs.popleft()
-            proc.communicate()
+            ret.append(proc.communicate())
+        return ret
 
     def terminate(self):
         while self.running_procs:
