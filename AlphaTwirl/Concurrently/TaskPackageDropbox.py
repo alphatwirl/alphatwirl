46683c17aa1292a1a439daec5a2a50dc999a97dc
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-04 13:25:09 +0100

    tar AlphaTwirl

--- AlphaTwirl/Concurrently/TaskPackageDropbox.py
+++ AlphaTwirl/Concurrently/TaskPackageDropbox.py
@@ -4,15 +4,19 @@ import shutil
 import pickle
 import datetime
 import tempfile
+import imp
+import tarfile
 
 ##__________________________________________________________________||
 class TaskPackageDropbox(object):
-    def __init__(self, dispatcher, path):
+    def __init__(self, dispatcher, path, tar_alphatwirl = True):
         self.dispatcher = dispatcher
         self.path = path
+        self.tar_alphatwirl = tar_alphatwirl
 
     def open(self):
         self.workdir = self._prepare_workdir(self.path)
+        if self.tar_alphatwirl: self._copy_alphatwirl(self.workdir)
         self.taskindices = [ ]
         self.last_taskindex = -1 # so it starts from 0
 
@@ -46,6 +50,20 @@ class TaskPackageDropbox(object):
         shutil.copy(src, workdir)
 
         return workdir
+
+    def _copy_alphatwirl(self, workdir):
+
+        def tar_filter(tarinfo):
+            exclude_extensions = ('.pyc', )
+            exclude_names = ('.git', )
+            if os.path.splitext(tarinfo.name)[1] in exclude_extensions: return None
+            if os.path.basename(tarinfo.name) in exclude_names: return None
+            return tarinfo
+
+        alphatwirl_path = imp.find_module('AlphaTwirl')[1]
+        tar = tarfile.open(os.path.join(workdir, 'AlphaTwirl.tar.gz'), 'w:gz')
+        tar.add(alphatwirl_path, arcname = 'AlphaTwirl', filter = tar_filter)
+        tar.close()
 
     def _save_package_in_workdir(self, task_idx, package, workdir):
         package_path = 'task_{:05d}.p'.format(task_idx)
