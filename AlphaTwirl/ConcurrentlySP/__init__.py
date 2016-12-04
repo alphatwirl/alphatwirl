3d3277961cdb67b65a093d79ec38ffc95a500631
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-04 11:22:28 +0100

    remove CommunicationChannel, TaskPackage from ConcurrentlySP

--- AlphaTwirl/ConcurrentlySP/__init__.py
+++ AlphaTwirl/ConcurrentlySP/__init__.py
@@ -1,4 +1,2 @@
-from CommunicationChannel import CommunicationChannel
-from TaskPackage import TaskPackage
 from TaskPackageDropbox import TaskPackageDropbox
 from SubprocessRunner import SubprocessRunner
