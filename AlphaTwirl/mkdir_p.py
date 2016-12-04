1086b16a9064fc2fc80afe853334135d7ab0d26e
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-04 19:15:44 +0100

    use logging

--- AlphaTwirl/mkdir_p.py
+++ AlphaTwirl/mkdir_p.py
@@ -1,14 +1,21 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
 
 import os, errno
+import logging
 
 ##__________________________________________________________________||
 def mkdir_p(path):
+
+    logger = logging.getLogger(__name__)
+
     # http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
     try:
         os.makedirs(path)
+        logger.info('created a directory, {}'.format(path))
+
     except OSError as exc: # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
+            logger.debug('tried to create a directory, {}. probably already existed'.format(path))
             pass
         else: raise
 
