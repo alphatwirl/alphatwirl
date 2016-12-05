3cb9e4bbe63ab974bfe1afa4bc3bdb44be7d49f2
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-05 12:26:25 +0100

    extract a new class Storage from TaskPackageDropbox

--- AlphaTwirl/Concurrently/TaskPackageDropbox.py
+++ AlphaTwirl/Concurrently/TaskPackageDropbox.py
@@ -8,58 +8,37 @@ import imp
 import tarfile
 
 ##__________________________________________________________________||
-class TaskPackageDropbox(object):
-    def __init__(self, dispatcher, path, put_alphatwirl = True, user_modules = ()):
-        self.dispatcher = dispatcher
-        self.path = path
-        self.put_alphatwirl = put_alphatwirl
-        self.user_modules = user_modules
+class Storage(object):
+    """
+        Args:
+        dir (str): a path to a directory in which a new directory will be created
 
-    def open(self):
-        self.workdir = self._prepare_workdir(self.path)
-        self._copy_python_modules(self.workdir, self.put_alphatwirl, self.user_modules)
-        self.taskindices = [ ]
+    """
+
+    def __init__(self, dir):
+        self.dirpath = self._prepare_dir(dir)
         self.last_taskindex = -1 # so it starts from 0
 
-    def put(self, package):
-        self.last_taskindex += 1
-        taskindex = self.last_taskindex
-        package_path = self._save_package_in_workdir(taskindex, package, self.workdir)
-        self.taskindices.append(taskindex)
-        self.dispatcher.run(self.workdir, package_path)
-
-    def receive(self):
-        self.dispatcher.wait()
-        results = [self._collect_result(i, self.workdir) for i in self.taskindices]
-        self.taskindices[:] = [ ]
-        return results
-
-    def close(self):
-        self.dispatcher.terminate()
-
-    def _prepare_workdir(self, path):
+    def _prepare_dir(self, path):
 
         prefix = 'tpd_{:%Y%m%d_%H%M%S}_'.format(datetime.datetime.now())
         # e.g., 'tpd_20161129_122841_'
 
-        workdir = tempfile.mkdtemp(prefix = prefix, dir = path)
+        dirpath = tempfile.mkdtemp(prefix = prefix, dir = path)
         # e.g., '{path}/tpd_20161129_122841_HnpcmF'
 
         # copy run.py to the task dir
         thisdir = os.path.dirname(__file__)
         src = os.path.join(thisdir, 'run.py')
-        shutil.copy(src, workdir)
+        shutil.copy(src, dirpath)
 
-        return workdir
+        return dirpath
 
-    def _copy_python_modules(self, workdir, put_alphatwirl, user_modules):
-
-        modules = list(user_modules)
-        if put_alphatwirl: modules.append('AlphaTwirl')
+    def store_python_modules(self, modules):
 
         if not modules: return
 
-        tar = tarfile.open(os.path.join(workdir, 'python_modules.tar.gz'), 'w:gz')
+        tar = tarfile.open(os.path.join(self.dirpath, 'python_modules.tar.gz'), 'w:gz')
 
         def tar_filter(tarinfo):
             exclude_extensions = ('.pyc', )
@@ -75,24 +54,28 @@ class TaskPackageDropbox(object):
             tar.add(path, arcname = arcname, filter = tar_filter)
         tar.close()
 
-    def _save_package_in_workdir(self, task_idx, package, workdir):
-        package_path = 'task_{:05d}.p'.format(task_idx)
-        # relative to workdir, e.g., 'task_00009.p'
+    def store_package(self, package):
 
-        package_fullpath = os.path.join(workdir, package_path)
+        self.last_taskindex += 1
+        task_idx = self.last_taskindex
+
+        package_path = 'task_{:05d}.p'.format(task_idx)
+        # relative to dirpath, e.g., 'task_00009.p'
+
+        package_fullpath = os.path.join(self.dirpath, package_path)
         # e.g., '{path}/tpd_20161129_122841_HnpcmF/task_00009.p'
 
         f = open(package_fullpath, 'wb')
         pickle.dump(package, f)
 
-        return package_path
+        return task_idx, package_path
 
-    def _collect_result(self, task_idx, workdir):
+    def collect_result(self, task_idx):
 
         dirname = 'task_{:05d}'.format(task_idx)
         # e.g., 'task_00009'
 
-        result_path = os.path.join(workdir, 'results', dirname, 'result.p')
+        result_path = os.path.join(self.dirpath, 'results', dirname, 'result.p')
         # e.g., '{path}/tpd_20161129_122841_HnpcmF/results/task_00009/result.p'
 
         f = open(result_path, 'rb')
@@ -100,5 +83,32 @@ class TaskPackageDropbox(object):
 
         return result
 
+
+##__________________________________________________________________||
+class TaskPackageDropbox(object):
+    def __init__(self, dispatcher, path, put_alphatwirl = True, user_modules = ()):
+        self.dispatcher = dispatcher
+        self.path = path
+        self.python_modules = list(user_modules)
+        if put_alphatwirl: self.python_modules.append('AlphaTwirl')
+
+    def open(self):
+        self.storage = Storage(self.path)
+        self.storage.store_python_modules(self.python_modules)
+        self.taskindices = [ ]
+
+    def put(self, package):
+        taskindex, package_path = self.storage.store_package(package)
+        self.taskindices.append(taskindex)
+        self.dispatcher.run(self.storage.dirpath, package_path)
+
+    def receive(self):
+        self.dispatcher.wait()
+        results = [self.storage.collect_result(i) for i in self.taskindices]
+        self.taskindices[:] = [ ]
+        return results
+
+    def close(self):
+        self.dispatcher.terminate()
 
 ##__________________________________________________________________||
