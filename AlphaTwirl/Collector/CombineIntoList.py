3a73ac540992782d0d7152c28732d8fa693d005f
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-11 14:25:17 +0100

    clean up the code

--- AlphaTwirl/Collector/CombineIntoList.py
+++ AlphaTwirl/Collector/CombineIntoList.py
@@ -4,14 +4,22 @@ import collections
 from functions import *
 
 ##__________________________________________________________________||
-def combinedToList(combined, columns, sort = True):
-    d = [ ]
-    for datasetName, summarizer in combined:
+def combinedToList(dataset_summarizer_pairs, columns, sort = True):
+
+    dataset_key_vals_dict_pairs = [ ]
+    for datasetName, summarizer in dataset_summarizer_pairs:
         key_vals_dict = collections.OrderedDict([(k, v.contents) for k, v in summarizer.results().iteritems()])
+        dataset_key_vals_dict_pairs.append((datasetName, key_vals_dict))
+
+    d = [ ]
+    for datasetName, key_vals_dict in dataset_key_vals_dict_pairs:
         l = convert_key_vals_dict_to_tuple_list(key_vals_dict, fill = 0, sort = sort)
         d.extend([(datasetName, ) + e for e in l])
+
     if sort: d.sort()
+
     d.insert(0, columns)
+
     return d
 
 ##__________________________________________________________________||
@@ -25,7 +33,11 @@ class CombineIntoList(object):
     def combine(self, datasetReaderPairs):
         if len(datasetReaderPairs) == 0: return None
         dataset_summarizer_pairs = [(d, r.results()) for d, r in datasetReaderPairs]
-        combined = add_summarizers_for_the_same_dataset(dataset_summarizer_pairs)
-        return combinedToList(combined, (self.datasetColumnName, ) + self.keyNames + self.valNames, self.sort)
+        dataset_summarizer_pairs = add_summarizers_for_the_same_dataset(dataset_summarizer_pairs)
+        return combinedToList(
+            dataset_summarizer_pairs,
+            (self.datasetColumnName, ) + self.keyNames + self.valNames,
+            self.sort
+        )
 
 ##__________________________________________________________________||
