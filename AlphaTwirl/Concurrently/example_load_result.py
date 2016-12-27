254e8359cabdada005f5be67671c5a1489524131
Tai Sakuma <tai.sakuma@gmail.com>
2016-12-27 18:15:37 +0100

    add a new file example_load_result.py

---
+++ AlphaTwirl/Concurrently/example_load_result.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Tai Sakuma <sakuma@fnal.gov>
+import os, sys
+import argparse
+import pickle
+import tarfile
+
+##__________________________________________________________________||
+parser = argparse.ArgumentParser()
+parser.add_argument('pickle', help = 'path to a pickle file')
+args = parser.parse_args()
+
+
+##__________________________________________________________________||
+pickle_path = os.path.abspath(args.pickle)
+
+##__________________________________________________________________||
+os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(pickle_path))))
+
+##__________________________________________________________________||
+dirname = 'python_modules'
+tarname = dirname + '.tar.gz'
+if os.path.exists(tarname) and not os.path.exists(dirname):
+    tar = tarfile.open(tarname)
+    tar.extractall()
+    tar.close()
+sys.path.insert(0, dirname)
+
+##__________________________________________________________________||
+f = open(pickle_path, 'rb')
+package = pickle.load(f)
+
+print package
+
+##__________________________________________________________________||
