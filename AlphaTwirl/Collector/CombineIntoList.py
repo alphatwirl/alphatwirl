55e85742a2fd2b1c77a3ad1163c529af290b3601
Tai Sakuma <tai.sakuma@gmail.com>
2017-02-02 10:40:55 +0100

    make summarizer_to_tuple_list an argument to CombineIntoList

--- AlphaTwirl/Collector/CombineIntoList.py
+++ AlphaTwirl/Collector/CombineIntoList.py
@@ -4,11 +4,50 @@ import collections
 from functions import *
 
 ##__________________________________________________________________||
+def summarizer_to_key_vals_dict(summarizer):
+
+    ret = collections.OrderedDict([(k, v.contents) for k, v in summarizer.results().iteritems()])
+    # e.g.,
+    # OrderedDict([
+    #     ((200, 2), [array([120, 240])]),
+    #     ((300, 2), [array([490, 980])]),
+    #     ((300, 3), [array([210, 420])])
+    # ])
+
+    return ret
+
+##__________________________________________________________________||
+def summarizer_to_tuple_list(summarizer, sort):
+
+    key_vals_dict = summarizer_to_key_vals_dict(summarizer)
+    # e.g.,
+    # OrderedDict([
+    #     ((200, 2), [array([120, 240])]),
+    #     ((300, 2), [array([490, 980])]),
+    #     ((300, 3), [array([210, 420])])
+    # ])
+
+    tuple_list = convert_key_vals_dict_to_tuple_list(key_vals_dict, fill = 0, sort = sort)
+    # e.g.,
+    # [
+    #     (200, 2, 120, 240),
+    #     (300, 2, 490, 980),
+    #     (300, 3, 210, 420)
+    # ]
+
+    return tuple_list
+
+##__________________________________________________________________||
 class CombineIntoList(object):
-    def __init__(self, summaryColumnNames, sort = True, datasetColumnName = 'component'):
+    def __init__(self, summaryColumnNames,
+                 sort = True,
+                 datasetColumnName = 'component',
+                 summarizer_to_tuple_list = summarizer_to_tuple_list):
+
         self.summaryColumnNames = summaryColumnNames
         self.sort = sort
         self.datasetColumnName = datasetColumnName
+        self.summarizer_to_tuple_list = summarizer_to_tuple_list
 
     def __repr__(self):
         return '{}(summaryColumnNames = {!r}, sort = {!r}, datasetColumnName = {!r})'.format(
@@ -51,7 +90,7 @@ class CombineIntoList(object):
 
         dataset_tuple_list_pairs = [ ]
         for dataset, summarizer in dataset_summarizer_pairs:
-            tuple_list = summarizer_to_tuple_list(summarizer, sort = self.sort)
+            tuple_list = self.summarizer_to_tuple_list(summarizer, sort = self.sort)
             dataset_tuple_list_pairs.append((dataset, tuple_list))
         # e.g.,
         # dataset_tuple_list_pairs = [
@@ -93,39 +132,5 @@ class CombineIntoList(object):
         # ]
 
         return ret
-
-##__________________________________________________________________||
-def summarizer_to_key_vals_dict(summarizer):
-
-    ret = collections.OrderedDict([(k, v.contents) for k, v in summarizer.results().iteritems()])
-    # e.g.,
-    # OrderedDict([
-    #     ((200, 2), [array([120, 240])]),
-    #     ((300, 2), [array([490, 980])]),
-    #     ((300, 3), [array([210, 420])])
-    # ])
-
-    return ret
-
-##__________________________________________________________________||
-def summarizer_to_tuple_list(summarizer, sort):
-
-    key_vals_dict = summarizer_to_key_vals_dict(summarizer)
-    # e.g.,
-    # OrderedDict([
-    #     ((200, 2), [array([120, 240])]),
-    #     ((300, 2), [array([490, 980])]),
-    #     ((300, 3), [array([210, 420])])
-    # ])
-
-    tuple_list = convert_key_vals_dict_to_tuple_list(key_vals_dict, fill = 0, sort = sort)
-    # e.g.,
-    # [
-    #     (200, 2, 120, 240),
-    #     (300, 2, 490, 980),
-    #     (300, 3, 210, 420)
-    # ]
-
-    return tuple_list
 
 ##__________________________________________________________________||
