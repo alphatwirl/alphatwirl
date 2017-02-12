45f49e4bd4ecb32ae6d58a92ad9365926828dab1
Sean Kalafut <stkalafut@gmail.com>
2017-02-12 19:07:19 +0100

    description of Combine binning class

--- AlphaTwirl/Binning/Combine.py
+++ AlphaTwirl/Binning/Combine.py
@@ -8,12 +8,22 @@ def plusOne(x): return x + 1
 
 ##__________________________________________________________________||
 class Combine(object):
-    """A combine two binnings.
+    """The Combine class allows one variable defined with two binnings to be
+	combined into one list.
 
-    This class uses *low* for the values below *at* and otherwise uses
-    *high*.
+	For example, consider studying jet pT with a linear scale for
+	jet pT below 300, and a log scale for jet pT above 300. Only one entry in
+	the main table defined in the AlphaTwirl executable file would be needed, and
+	the binning in the same executable would be defined like this:
 
-    This class requires bin labels of both binnings to be values in
+	lowpt = Round(50.0, 100)   for 70.0 GeV wide bins
+	highpt = Round(0.1, 100)   for variable bins which have a fixed 0.1 width in log scale
+	ptbinning = Combine(low = lowpt, high = highpt, at = 300)
+
+	where the last argument, at = 300, defines the point where the bins switch
+	from linear to log scale.
+
+	This class requires bin labels of both binnings to be values in
     the bins.
 
     """
