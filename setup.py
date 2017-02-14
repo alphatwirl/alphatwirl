5d7f0a4d336255164a7311ed93af4588a97e9014
Tai Sakuma <tai.sakuma@gmail.com>
2017-02-14 19:13:42 +0100

    add a new file setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'AlphaTwirl',
+    version= '0.9',
+    description = 'A Python library for summarizing event data',
+    author = 'Tai Sakuma',
+    author_email = 'tai.sakuma@gmail.com',
+    url = 'https://github.com/TaiSakuma/AlphaTwirl',
+)
