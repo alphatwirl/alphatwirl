236f480c9a0796148e88a807120dcf488baadd57
Sean Kalafut <kalaf004@umn.edu>
2017-03-06 16:09:31 -0600

    added documentation to Round and RoundLog binning classes

--- AlphaTwirl/Binning/Round.py
+++ AlphaTwirl/Binning/Round.py
@@ -8,6 +8,37 @@ def returnTrue(x): return True
 
 ##__________________________________________________________________||
 class Round(object):
+    """The Round class allows the user to define a set of fixed linear
+	width bins used to summarize a variable.  This can also be done with the
+	Binning class, but using the Round class will yield more
+	intelligently chosen bins (in terms of their lower and upper edges)
+	in less time.
+
+	An instance of the Round class is defined with two arguments.  The
+	first specifies the width of every bin in the units used to make
+	the TTree branch.  The second argument identifies one particular
+	bin boundary - the upper edge of one bin, and the lower edge of
+	the next bin.  The Round class studies the input TTree branch and
+	chooses the lower edge value of the lowest bin so that the user
+	defined bin boundary and bin width are respected, there are no
+	underflow events, and the number of initial bins (starting with
+	bin number 1) with zero entries is minimized.  The Round class
+	will automatically create more bins until there are no overflow events.
+
+	If an input TTree contains a branch with the leading jet pT in
+	GeV, then the following binning would yield non-overlapping bins
+	which are 20 GeV wide, and the lower edge of one bin would be
+	100 GeV::
+		
+		Round = AlphaTwirl.Binning.Round
+		jetptbin = Round(20, 100)
+
+	One bin would cover 100 to 120 GeV, the next bin would cover
+	120 to 140 GeV, the bin after that would cover 140 to 160 GeV,
+	and so on.
+	
+	
+    """
     def __init__(self, width = 1, aBoundary = None,
                  min = None, underflow_bin = None,
                  max = None, overflow_bin = None,
--- AlphaTwirl/Binning/RoundLog.py
+++ AlphaTwirl/Binning/RoundLog.py
@@ -7,6 +7,35 @@ def returnTrue(x): return True
 
 ##__________________________________________________________________||
 class RoundLog(object):
+    """Similar to the Round class, the RoundLog class allows the user
+	to define a set of bins used to summarize a variable that have a
+	fixed width in log base 10.
+
+	An instance of the RoundLog class is defined with two arguments.  The
+	first specifies the width of every bin in log base 10 of the units used
+	to make the TTree branch.  The second argument identifies one particular
+	bin boundary - the upper edge of one bin, and the lower edge of
+	the next bin.  The RoundLog class studies the input TTree branch and
+	chooses the lower edge value of the lowest bin so that the user
+	defined bin boundary and bin width are respected, there are no
+	underflow events, and the number of initial bins (starting with
+	bin number 1) with zero entries is minimized.  The RoundLog class
+	will automatically create more bins until there are no overflow events.
+
+	If an input TTree contains a branch with the leading jet pT in
+	GeV, then the following binning would yield non-overlapping bins
+	which are 0.1 units wide in log(GeV), and the lower edge of one
+	bin would be 100 GeV::
+		
+		RoundLog = AlphaTwirl.Binning.RoundLog
+		jetptlogbin = RoundLog(0.1, 100)
+
+	One bin would cover 2.0 (100 GeV in log base 10) to 2.1 (125 GeV
+	in log base 10), the next bin would cover 2.1 to 2.2 (158 GeV in
+	log base 10), and so on.
+	
+	
+    """
     def __init__(self, width = 0.1, aBoundary = 1,
                  min = None, underflow_bin = None,
                  max = None, overflow_bin = None,
