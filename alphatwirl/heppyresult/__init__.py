a6fca6dbc5b4fee772af4c21dbcbf2daeeefc6c4
Tai Sakuma <tai.sakuma@gmail.com>
2017-03-18 08:52:34 +0100

    reorder imports in __init__.py

--- alphatwirl/heppyresult/__init__.py
+++ alphatwirl/heppyresult/__init__.py
@@ -1,15 +1,15 @@
+from Analyzer import Analyzer
+from Component import Component
 from ComponentLoop import ComponentLoop
 from ComponentReaderComposite import ComponentReaderComposite
-from HeppyResult import HeppyResult
-from Component import Component
-from ReadComponentConfig import ReadComponentConfig
 from EventBuilderConfig import EventBuilderConfig
-from ReadVersionInfo import ReadVersionInfo
-from Analyzer import Analyzer
+from HeppyResult import HeppyResult
+from ReadComponentConfig import ReadComponentConfig
 from ReadCounter import ReadCounter
+from ReadVersionInfo import ReadVersionInfo
 from TblCounter import TblCounter
-from TblCounterLong import TblCounterLong
 from TblComponentConfig import TblComponentConfig
+from TblCounterLong import TblCounterLong
 from TblBrilCalc import TblBrilCalc
 
 hasROOT = False
@@ -20,8 +20,8 @@ except ImportError:
     pass
 
 if hasROOT:
-    from EventBuilderConfigMaker import EventBuilderConfigMaker
-    from EventBuilder import EventBuilder
     from BEventBuilder import BEventBuilder
+    from EventBuilder import EventBuilder
+    from EventBuilderConfigMaker import EventBuilderConfigMaker
     from TblBranch import TblBranch
     from TblTree import TblTree
--- alphatwirl/progressbar/__init__.py
+++ alphatwirl/progressbar/__init__.py
@@ -1,8 +1,8 @@
-from ProgressBar import ProgressBar
-from ProgressPrint import ProgressPrint
-from ProgressMonitor import ProgressMonitor, Queue
-from ProgressReporter import ProgressReporter
-from ProgressReport import ProgressReport
-from NullProgressMonitor import NullProgressMonitor
 from BProgressMonitor import BProgressMonitor
+from NullProgressMonitor import NullProgressMonitor
+from ProgressBar import ProgressBar
+from ProgressMonitor import ProgressMonitor, Queue
+from ProgressPrint import ProgressPrint
 from ProgressReportPickup import ProgressReportPickup
+from ProgressReport import ProgressReport
+from ProgressReporter import ProgressReporter
