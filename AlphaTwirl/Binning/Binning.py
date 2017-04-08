509ffb9b1d3930a38c1088ed0eac8665ce3cfea2
Sean Kalafut <stkalafut@gmail.com>
2017-04-08 18:05:57 +0200

    added comments to methods of Binning class

--- AlphaTwirl/Binning/Binning.py
+++ AlphaTwirl/Binning/Binning.py
@@ -5,75 +5,93 @@ def returnTrue(x): return True
 
 ##__________________________________________________________________||
 class Binning(object):
-    """This is an abstract class which can run in any environment.  It
-	doesn't know about ROOT TTrees or other ROOT things.  .
-	
-	The Binning class allows the set of bins used to summarize the
-	information from one TTree branch to be defined with specific lower
-	and upper bin edge values set by the user.
+    """
+	The Binning class allows the set of bins used to summarize input
+	information to be configured with user defined lower and upper bin
+	edge values.
 	
 	An instance of the Binning class can be defined with a single argument
 	``boundaries``, which is set equal to a list of the bin boundaries
-	which start at the lowest value, and increase for every bin
+	that begin at the lowest value, and increase for every bin
 	added, like::
 		
 		metbin = Binning(boundaries = (0, 100, 200, 400, 700, 1100))
 	
-	Alternatively, an instance of the Binning class can be defined with
+	Alternatively, an instance of the Binning class can be instantiated with
 	two arguments ``lows`` and ``ups``.  These two arguments are set equal
 	to two lists of the same length that specify the lower and upper edge
 	of every bin, like::
 		
 		jetPtBin = Binning(lows = (0, 20, 40, 60), ups = (20, 40, 60, 200))
 	
-	In general, if the results should be summarized in non-overlapping
-	bins which completely cover a well defined domain of a variable, the
-	single argument ``boundaries`` definition of the Binning class should
-	be used.
 
-	Two powerful features of the two argument definition of a Binning
-	class instance are that a subset of variable values can be blinded
-	from the summary created by AlphaTwirl, and overlapping bins can
-	be defined.  If an input TTree passed to AlphaTwirl contains a branch
-	with the dilepton mass using the two leading leptons in each event,
-	the following binning would summarize events in 4 different bins
-	that are all centered at 90 GeV::
+	The main function of this class is __call__.  __call__ requires one 
+	input argument, which is a value of a variable being summarized.
+	If the input value is not valid, like a floating point number is given
+	for a variable with integer bins, then None is returned.  Users must
+	check that the return value of __call__ is not None.  Otherwise, if
+	a valid value is passed as input, __call__ returns the bin to which
+	the value belongs.  If an instance of the Binning class has already
+	been created and is named obj, then __call__ is executed like this:
 
-		massOverlapBins = Binning(lows = (60, 70, 80, 85), ups = (120, 110, 100, 95))
+	obj.__call__(1.2)
 
-	REMOVE OVERLAPPING BIN DISCUSSION.  It is not possible
+	this is equivalent to executing:
 
-	Using the same input TTree and dilepton mass branch, events with 
-	dilepton mass between 85 and 95 would be ignored in the AlphaTwirl
-	summary if the following binning is used::
+	obj(1.2)
 
-		massIgnoreBins = Binning(lows = (70, 80, 95, 95), ups = (85, 85, 100, 110))
+	A functor example of __call__ is the following:
 
-	HOW TO INSTANTIATE
+	def test_call(self):
+		#here the bins are assigned integer labels 1 through 4. this is not
+		#a required input parameter, but can be used when retvalue is set
+		#to 'number'
+        bins = (1, 2, 3, 4)
+        lows = (10.0, 20.0, 30.0, 40.0)
+        ups = (20.0, 30.0, 40.0, 50.0)
+        obj = Binning(bins = bins, lows = lows, ups = ups, retvalue = 'number')
+        self.assertEqual(1, obj(15))
+        self.assertEqual(2, obj(21))
+        self.assertEqual(2, obj(20)) # on the low edge
+        self.assertEqual(0, obj(5)) # underflow
+        self.assertEqual(5, obj(55)) # overflow
 
-	EXECUTE __call__ AND DESCRIBE WHAT IS RETURNED
 
-	EXECUTE __next__ AND DESCRIBE WHAT IS RETURNED
+	The function next requires a bin as an input parameter, and
+	returns the next bin.  If the input parameter is the underflow
+	bin, then the first bin is returned.  If the input parameter is the overflow
+	bin, then the overflow bin is returned.  If an instance of the Binning
+	class has already been created and is named obj, then next is
+	used like this:
+
+	obj.next(11)
 	
-	SHOW FUNCTOR EXAMPLE see tests/unit/Binning/test_Round.py. Line 17
-	is for call, and the object is defined on line 18.
-	
+	A functor example of next is the following:
 
-	
+	def test_next(self):
+		bounds = (10, 20, 30, 40)
+		obj = Binning(boundaries = bounds, retvalue = 'lowedge')
+		self.assertEqual(10, obj.next( float('-inf') ) )
+		self.assertEqual(20, obj.next(10))
+		self.assertEqual(30, obj.next(20))
+		self.assertEqual(40, obj.next(30))
+		self.assertEqual(40, obj.next(40))
+
+
     """
     def __init__(self, boundaries = None, lows = None, ups = None,
                  retvalue = 'lowedge', bins = None, underflow_bin = None, overflow_bin = None,
                  valid = returnTrue):
-		"""retvalue is automatically set to lowedge
+		"""this creates an instance of the Binning class.
 
-		validity of added value is, by default 'returnTrue', always true
-		if users want to check that the added value is actually valid, then they should
-		change this default in __init__
+		By default:
+			retvalue is set to lowedge.  It can also be set to number.
+			every value added has the parameter valid equal to True.  Thus, __call__
+			will never return None.
+			specific values for underflow_bin and overflow_bin are not set.
 
-		users can set specific values for the underflow_bin and overflow_bin, by
-		default they are none.  an example for underflow_bin could be -999
-
-		an example overflow_bin could be the upper edge of the last bin
+		For example, underflow_bin could be -999, and overflow_bin could be the
+		upper edge of the last bin.
 		
 		"""
 
@@ -126,23 +144,18 @@ class Binning(object):
         return ret + "\n".join('{:>5} {:>10} {:>10}'.format(b, l, u) for b, l, u in zip(self.bins, self.lows, self.ups))
 
     def __call__(self, val):
-		"""this is the main function of this class
+		"""main function of this class. returns the bin to which val belongs.
 		
-		after class is instantiated, this class simply
-
-		if val is not valid, null is returned.  By default, true is always returned if
-		__init__ is modified such that the validity of the added value is changed from returnTrue
+		if val is not valid, None is returned.  If the default value of valid set in __init__
+		is not changed from returnTrue, then __call__ will never return None.  If this default
+		value of valid set in __init__ is changed from returnTrue, then users must check that
+		the return value of __call__ is not None.
 		
-		USERS OF THIS BINNING CLASS must check that return value of __call__ is not None
-		For example, if all values must be integers and a floating point number is
-		given, then None is returned
+		the upper bin boundary defined with __init__ belongs to the next bin, not the current
+		bin.  If val passed to __call__ is the upper edge of a bin, then the value belongs
+		to the next bin.
 
-		the upper boundary defined with __init__ belongs to the next bin, not the current
-		bin.  Thus, the line which decides if a value belongs to overflow_bin uses greater
-		than or equal to, not just greater than.
-
-		in the return function, if the value 'val' is equal to the lower edge, then 'val'
-		belongs to the current bin.
+		if val is equal to the lower edge of bin J, then val belongs to bin J.
 		
 		"""
         if not self._valid(val): return None
@@ -151,11 +164,8 @@ class Binning(object):
         return [b for b, l, u in zip(self.bins, self.lows, self.ups) if l <= val < u][0]
 
     def next(self, bin):
-		"""if you give one bin, this method returns the next bin
+		"""given one bin, this method returns the next bin.
 		
-		in original implementation, the bins were just numbered, and
-		the lower edge of each bin was tracked separately
-
 		__call__ is called first to determine the correct current bin (this is tricky
 		for bins whose edges are floating point values)
 		
