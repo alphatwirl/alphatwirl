1afef874a9e1ba791ba02385fa467abf59493f75
Tai Sakuma <tai.sakuma@gmail.com>
2017-04-18 12:19:52 +0100

    add poll()

--- alphatwirl/concurrently/SubprocessRunner.py
+++ alphatwirl/concurrently/SubprocessRunner.py
@@ -26,6 +26,22 @@ class SubprocessRunner(object):
         self.running_procs.append(proc)
         return proc.pid
 
+    def poll(self):
+        """check if the jobs are running and return a list of pids for
+        finished jobs
+
+        """
+        finished_procs = [p for p in self.running_procs if p.poll() is not None]
+        self.running_procs = [p for p in self.running_procs if p not in finished_procs]
+
+        for proc in finished_procs:
+            stdout, stderr = proc.communicate()
+            ## proc.communicate() returns (stdout, stderr) when
+            ## self.pipe = True. Otherwise they are (None, None)
+
+        finished_pids = [p.pid for p in  finished_procs]
+        return finished_pids
+
     def wait(self):
         """wait until all jobs finish and return a list of pids
         """
