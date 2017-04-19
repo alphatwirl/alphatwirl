41636c28bc76f80aa6e423e6791d6a62a9d9ab8a
Tai Sakuma <tai.sakuma@gmail.com>
2017-04-19 11:19:14 +0100

    copy BEventBuilder from heppyresult to roottree

---
+++ alphatwirl/roottree/BEventBuilder.py
@@ -0,0 +1,24 @@
+# Tai Sakuma <tai.sakuma@cern.ch>
+import ROOT
+from .BEvents import BEvents
+
+##__________________________________________________________________||
+class BEventBuilder(object):
+    def __init__(self, config):
+        self.config = config
+
+    def __repr__(self):
+        return '{}({!r})'.format(
+            self.__class__.__name__,
+            self.config
+        )
+
+    def __call__(self):
+        file = ROOT.TFile.Open(self.config.inputPath)
+        tree = file.Get(self.config.treeName)
+        events = BEvents(tree, self.config.maxEvents, self.config.start)
+        events.config = self.config
+        events.component = self.config.component
+        return events
+
+##__________________________________________________________________||
---
+++ tests/unit/roottree/test_BEventBuilder.py
@@ -0,0 +1,94 @@
+import unittest
+import sys
+
+from alphatwirl.heppyresult import EventBuilderConfig
+
+##__________________________________________________________________||
+hasROOT = False
+try:
+    import ROOT
+    hasROOT = True
+except ImportError:
+    pass
+
+if hasROOT:
+    from alphatwirl.roottree.BEventBuilder import BEventBuilder
+
+##__________________________________________________________________||
+class MockAnalyzer(object):
+    def __init__(self):
+        self.path = '/heppyresult/dir/TTJets/treeProducerSusyAlphaT'
+
+##__________________________________________________________________||
+class MockComponent(object):
+    def __init__(self):
+        self.treeProducerSusyAlphaT = MockAnalyzer()
+
+##__________________________________________________________________||
+class MockTObject(object):
+    def __init__(self, name):
+        self.name = name
+
+    def GetEntries(self):
+        return 5500
+
+##__________________________________________________________________||
+class MockTFile(object):
+    def Open(self, path):
+        self.path = path
+        return self
+    def Get(self, name):
+        return MockTObject(name)
+
+##__________________________________________________________________||
+class MockROOT(object):
+    def __init__(self): self.TFile = MockTFile()
+
+##__________________________________________________________________||
+class MockEvents(object):
+    def __init__(self, tree, maxEvents, start = 0):
+        self.tree = tree
+        self.maxEvents = maxEvents
+        self.start = start
+
+##__________________________________________________________________||
+@unittest.skipUnless(hasROOT, "has no ROOT")
+class TestBEventBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.moduleBEventBuilder = sys.modules['alphatwirl.roottree.BEventBuilder']
+        self.orgROOT = self.moduleBEventBuilder.ROOT
+        self.moduleBEventBuilder.ROOT = MockROOT()
+
+        self.orgEvents = self.moduleBEventBuilder.BEvents
+        self.moduleBEventBuilder.BEvents = MockEvents
+
+    def tearDown(self):
+        self.moduleBEventBuilder.ROOT = self.orgROOT
+        self.moduleBEventBuilder.BEvents = self.orgEvents
+
+    def test_build(self):
+
+        component = MockComponent()
+
+        config = EventBuilderConfig(
+            inputPath = '/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root',
+            treeName = 'tree',
+            maxEvents = 123,
+            start = 11,
+            component = component,
+            name = 'TTJets'
+        )
+
+        obj = BEventBuilder(config)
+
+        events = obj()
+
+        self.assertEqual('/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root', self.moduleBEventBuilder.ROOT.TFile.path)
+        self.assertIsInstance(events, MockEvents)
+        self.assertEqual('tree', events.tree.name)
+        self.assertEqual(11, events.start)
+        self.assertEqual(123, events.maxEvents)
+        self.assertIs(component, events.component)
+
+##__________________________________________________________________||
