af6e573cd1105e56a3df6a38749cb082b87faf67
Tai Sakuma <tai.sakuma@gmail.com>
2017-05-02 13:50:18 +0100

    set ROOT.PyConfig.IgnoreCommandLineOptions True sooner

--- alphatwirl/__init__.py
+++ alphatwirl/__init__.py
@@ -1,3 +1,14 @@
+
+##__________________________________________________________________||
+try:
+    ## https://root-forum.cern.ch/t/pyroot-hijacks-help/15207
+    import ROOT
+    ROOT.PyConfig.IgnoreCommandLineOptions = True
+except ImportError:
+    pass
+
+##__________________________________________________________________||
+
 import aggregate
 import binning
 import collector
@@ -22,13 +33,5 @@ log_handler = logging.StreamHandler()
 log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log_handler.setFormatter(log_formatter)
 logger.addHandler(log_handler)
-
-##__________________________________________________________________||
-try:
-    ## https://root-forum.cern.ch/t/pyroot-hijacks-help/15207
-    import ROOT
-    ROOT.PyConfig.IgnoreCommandLineOptions = True
-except ImportError:
-    pass
 
 ##__________________________________________________________________||
