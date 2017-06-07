e4ba6a24fd84640fa5ee684b810d144f68eb40dc
Tai Sakuma <tai.sakuma@gmail.com>
2017-06-07 10:55:41 +0100

    update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'alphatwirl',
-    version= '0.9',
+    version= '0.9.5',
     description = 'A Python library for summarizing event data',
     author = 'Tai Sakuma',
     author_email = 'tai.sakuma@gmail.com',
