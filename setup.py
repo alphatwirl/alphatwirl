003c3a6539aa2d27f2ba79ea18392304a0ce9c43
Tai Sakuma <tai.sakuma@gmail.com>
2017-08-24 16:24:19 +0100

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup(
     author_email = 'tai.sakuma@gmail.com',
     url = 'https://github.com/alphatwirl/alphatwirl',
     packages = find_packages(exclude=['docs', 'images', 'tests']),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+    ]
 )
