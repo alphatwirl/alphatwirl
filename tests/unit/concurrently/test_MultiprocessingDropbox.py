8938ad06408dcfc2aabbe56775b7d7d42dd6d7ca
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-07 12:16:18 +0000

    replace unittest with pytest

--- tests/unit/concurrently/test_MultiprocessingDropbox.py
+++ tests/unit/concurrently/test_MultiprocessingDropbox.py
@@ -1,7 +1,13 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
-import unittest
 import time
 import os
+
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
 
 from alphatwirl.concurrently import MultiprocessingDropbox
 from alphatwirl.concurrently import TaskPackage
@@ -22,152 +28,149 @@ class MockResult(object):
         self.data = data
 
 ##__________________________________________________________________||
-class TestMultiprocessingDropbox(unittest.TestCase):
+@pytest.fixture()
+def obj():
+    return MultiprocessingDropbox()
 
-    def test_repr(self):
-        obj = MultiprocessingDropbox()
-        repr(obj)
+##__________________________________________________________________||
+def test_repr(obj):
+    repr(obj)
 
-    def test_init_raise(self):
-        self.assertRaises(ValueError, MultiprocessingDropbox, nprocesses = 0)
+def test_init_raise():
+    with pytest.raises(ValueError):
+        MultiprocessingDropbox(nprocesses = 0)
 
-    def test_open_close(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
-        obj.close()
+def test_open_close(obj):
+    obj.open()
+    obj.close()
 
-    def test_open_open_close(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
-        obj.open() # don't create workers again
-        obj.close()
+def test_open_open_close(obj):
+    obj.open()
+    obj.open() # don't create workers again
+    obj.close()
 
-    def test_put(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
+def test_put(obj):
+    obj.open()
 
-        result1 = MockResult('task1')
-        task1 = MockTask(result1, 0.003)
-        package1 = TaskPackage(task = task1, args = (), kwargs = {})
-        obj.put(package1)
+    result1 = MockResult('task1')
+    task1 = MockTask(result1, 0.003)
+    package1 = TaskPackage(task = task1, args = (), kwargs = {})
+    obj.put(package1)
 
-        result2 = MockResult('task2')
-        task2 = MockTask(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (), kwargs = {})
-        obj.put(package2)
+    result2 = MockResult('task2')
+    task2 = MockTask(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (), kwargs = {})
+    obj.put(package2)
 
-        obj.close()
+    obj.close()
 
-    def test_put_receive(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
+def test_put_receive(obj):
 
-        result1 = MockResult('task1')
-        task1 = MockTask(result1, 0.003)
-        package1 = TaskPackage(task = task1, args = (), kwargs = {})
-        obj.put(package1)
+    obj.open()
 
-        result2 = MockResult('task2')
-        task2 = MockTask(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (), kwargs = {})
-        obj.put(package2)
+    result1 = MockResult('task1')
+    task1 = MockTask(result1, 0.003)
+    package1 = TaskPackage(task = task1, args = (), kwargs = {})
+    obj.put(package1)
 
-        actual = [r.data for r in obj.receive()]
-        self.assertEqual(set(['task1', 'task2']), set(actual))
+    result2 = MockResult('task2')
+    task2 = MockTask(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (), kwargs = {})
+    obj.put(package2)
 
-        obj.close()
+    actual = [r.data for r in obj.receive()]
+    assert set(['task1', 'task2']) == set(actual)
 
-    def test_receive_order(self):
-        # results of tasks are sorted in the order in which the tasks
-        # are put.
+    obj.close()
 
-        obj = MultiprocessingDropbox()
-        obj.open()
+def test_receive_order(obj):
+    # results of tasks are sorted in the order in which the tasks
+    # are put.
 
-        result1 = MockResult('task1')
-        task1 = MockTask(result1, 0.010)
-        package1 = TaskPackage(task = task1, args = (), kwargs = {})
-        obj.put(package1)
+    obj.open()
 
-        result2 = MockResult('task2')
-        task2 = MockTask(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (), kwargs = {})
-        obj.put(package2)
+    result1 = MockResult('task1')
+    task1 = MockTask(result1, 0.010)
+    package1 = TaskPackage(task = task1, args = (), kwargs = {})
+    obj.put(package1)
 
-        result3 = MockResult('task3')
-        task3 = MockTask(result3, 0.005)
-        package3 = TaskPackage(task = task3, args = (), kwargs = {})
-        obj.put(package3)
+    result2 = MockResult('task2')
+    task2 = MockTask(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (), kwargs = {})
+    obj.put(package2)
 
-        actual = [r.data for r in obj.receive()]
-        self.assertEqual(['task1', 'task2', 'task3'], actual)
+    result3 = MockResult('task3')
+    task3 = MockTask(result3, 0.005)
+    package3 = TaskPackage(task = task3, args = (), kwargs = {})
+    obj.put(package3)
 
-        obj.close()
+    actual = [r.data for r in obj.receive()]
+    assert ['task1', 'task2', 'task3'] == actual
 
-    def test_put_receive_repeat(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
+    obj.close()
 
-        result1 = MockResult('task1')
-        task1 = MockTask(result1, 0.003)
-        package1 = TaskPackage(task = task1, args = (), kwargs = {})
-        obj.put(package1)
+def test_put_receive_repeat(obj):
 
-        result2 = MockResult('task2')
-        task2 = MockTask(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (), kwargs = {})
-        obj.put(package2)
+    obj.open()
 
-        actual = [r.data for r in obj.receive()]
-        self.assertEqual(set(['task1', 'task2']), set(actual))
+    result1 = MockResult('task1')
+    task1 = MockTask(result1, 0.003)
+    package1 = TaskPackage(task = task1, args = (), kwargs = {})
+    obj.put(package1)
 
-        result3 = MockResult('task3')
-        task3 = MockTask(result3, 0.002)
-        package3 = TaskPackage(task = task3, args = (), kwargs = {})
-        obj.put(package3)
+    result2 = MockResult('task2')
+    task2 = MockTask(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (), kwargs = {})
+    obj.put(package2)
 
-        result4 = MockResult('task4')
-        task4 = MockTask(result4, 0.002)
-        package4 = TaskPackage(task = task4, args = (), kwargs = {})
-        obj.put(package4)
+    actual = [r.data for r in obj.receive()]
+    assert set(['task1', 'task2']) == set(actual)
 
-        actual = [r.data for r in obj.receive()]
-        self.assertEqual(set(['task3', 'task4']), set(actual))
+    result3 = MockResult('task3')
+    task3 = MockTask(result3, 0.002)
+    package3 = TaskPackage(task = task3, args = (), kwargs = {})
+    obj.put(package3)
 
-        obj.close()
+    result4 = MockResult('task4')
+    task4 = MockTask(result4, 0.002)
+    package4 = TaskPackage(task = task4, args = (), kwargs = {})
+    obj.put(package4)
 
-    def test_begin_put_recive_end_repeat(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
+    actual = [r.data for r in obj.receive()]
+    assert set(['task3', 'task4']) == set(actual)
 
-        result = MockResult('task1')
-        task = MockTask(result, 0.003)
-        package = TaskPackage(task = task, args = (), kwargs = {})
-        obj.put(package)
+    obj.close()
 
-        obj.receive()
+def test_begin_put_recive_end_repeat(obj):
 
-        obj.close()
+    obj.open()
 
-        obj.open()
+    result = MockResult('task1')
+    task = MockTask(result, 0.003)
+    package = TaskPackage(task = task, args = (), kwargs = {})
+    obj.put(package)
 
-        result = MockResult('task2')
-        task = MockTask(result, 0.003)
-        package = TaskPackage(task = task, args = (), kwargs = {})
-        obj.put(package)
+    obj.receive()
 
-        obj.receive()
+    obj.close()
 
-        obj.close()
+    obj.open()
 
+    result = MockResult('task2')
+    task = MockTask(result, 0.003)
+    package = TaskPackage(task = task, args = (), kwargs = {})
+    obj.put(package)
 
-    def test_receive_without_put(self):
-        obj = MultiprocessingDropbox()
-        obj.open()
+    obj.receive()
 
-        self.assertEqual([ ], obj.receive())
+    obj.close()
 
-        obj.close()
+def test_receive_without_put(obj):
+    obj.open()
+
+    assert [ ] == obj.receive()
+
+    obj.close()
 
 ##__________________________________________________________________||
 class MockTaskWithProgressReporter(object):
@@ -186,38 +189,36 @@ class MockProgressReporter(object):
 
 ##__________________________________________________________________||
 class MockProgressMonitor(object):
-    def createReporter(self): return MockProgressReporter()
-    def monitor(self): pass
-    def last(self): pass
+    def createReporter(obj): return MockProgressReporter()
+    def monitor(obj): pass
+    def last(obj): pass
 
 ##__________________________________________________________________||
-class TestMultiprocessingDropbox_ProgressMonitor(unittest.TestCase):
+def test_ProgressMonitor():
+    progressMonitor = MockProgressMonitor()
+    obj = MultiprocessingDropbox(nprocesses = 3, progressMonitor = progressMonitor)
+    obj.open()
 
-    def test_ProgressMonitor(self):
-        progressMonitor = MockProgressMonitor()
-        obj = MultiprocessingDropbox(nprocesses = 3, progressMonitor = progressMonitor)
-        obj.open()
+    result1 = MockResult('task1')
+    task1 = MockTaskWithProgressReporter(result1, 0.003)
+    package1 = TaskPackage(task = task1, args = (), kwargs = {})
+    obj.put(package1)
 
-        result1 = MockResult('task1')
-        task1 = MockTaskWithProgressReporter(result1, 0.003)
-        package1 = TaskPackage(task = task1, args = (), kwargs = {})
-        obj.put(package1)
+    result2 = MockResult('task2')
+    task2 = MockTaskWithProgressReporter(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (), kwargs = {})
+    obj.put(package2)
 
-        result2 = MockResult('task2')
-        task2 = MockTaskWithProgressReporter(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (), kwargs = {})
-        obj.put(package2)
+    # the results in the main process don't have a ProgressReporter
+    assert not hasattr(result1, "progressReporter")
+    assert not hasattr(result2, "progressReporter")
 
-        # the results in the main process don't have a ProgressReporter
-        self.assertFalse(hasattr(result1, "progressReporter"))
-        self.assertFalse(hasattr(result2, "progressReporter"))
+    # the results returned from other processes do.
+    returnedResults = obj.receive()
+    assert isinstance(returnedResults[0].progressReporter, MockProgressReporter)
+    assert isinstance(returnedResults[1].progressReporter, MockProgressReporter)
 
-        # the results returned from other processes do.
-        returnedResults = obj.receive()
-        self.assertIsInstance(returnedResults[0].progressReporter, MockProgressReporter)
-        self.assertIsInstance(returnedResults[1].progressReporter, MockProgressReporter)
-
-        obj.close()
+    obj.close()
 
 ##__________________________________________________________________||
 class MockTaskWithArguments(object):
@@ -247,60 +248,58 @@ class MockTaskWithArgumentsAndProgressReporter(object):
         return self.result
 
 ##__________________________________________________________________||
-class TestMultiprocessingDropbox_task_arguments(unittest.TestCase):
+def test_task_without_ProgressReporterno(obj):
+    progressMonitor = MockProgressMonitor()
+    obj = MultiprocessingDropbox(nprocesses = 3, progressMonitor = progressMonitor)
+    obj.open()
 
-    def test_task_without_ProgressReporterno(self):
-        progressMonitor = MockProgressMonitor()
-        obj = MultiprocessingDropbox(nprocesses = 3, progressMonitor = progressMonitor)
-        obj.open()
+    result1 = MockResult('task1')
+    task1 = MockTaskWithArguments(result1, 0.003)
+    package1 = TaskPackage(task = task1, args = (111, 222, 333), kwargs = {})
+    obj.put(package1)
 
-        result1 = MockResult('task1')
-        task1 = MockTaskWithArguments(result1, 0.003)
-        package1 = TaskPackage(task = task1, args = (111, 222, 333), kwargs = {})
-        obj.put(package1)
+    result2 = MockResult('task2')
+    task2 = MockTaskWithArguments(result2, 0.001)
+    package2 = TaskPackage(task = task2, args = (444, 555), kwargs = dict(C = 666))
+    obj.put(package2)
 
-        result2 = MockResult('task2')
-        task2 = MockTaskWithArguments(result2, 0.001)
-        package2 = TaskPackage(task = task2, args = (444, 555), kwargs = dict(C = 666))
-        obj.put(package2)
+    result3 = MockResult('task3')
+    task3 = MockTaskWithArgumentsAndProgressReporter(result3, 0.001)
+    package3 = TaskPackage(task = task3, args = (777, 888), kwargs = dict(C = 999))
+    obj.put(package3)
 
-        result3 = MockResult('task3')
-        task3 = MockTaskWithArgumentsAndProgressReporter(result3, 0.001)
-        package3 = TaskPackage(task = task3, args = (777, 888), kwargs = dict(C = 999))
-        obj.put(package3)
+    # the results in the main process don't have the attributes
+    assert not hasattr(result1, 'A')
+    assert not hasattr(result1, 'B')
+    assert not hasattr(result1, 'C')
 
-        # the results in the main process don't have the attributes
-        self.assertFalse(hasattr(result1, 'A'))
-        self.assertFalse(hasattr(result1, 'B'))
-        self.assertFalse(hasattr(result1, 'C'))
+    assert not hasattr(result2, 'A')
+    assert not hasattr(result2, 'B')
+    assert not hasattr(result2, 'C')
 
-        self.assertFalse(hasattr(result2, 'A'))
-        self.assertFalse(hasattr(result2, 'B'))
-        self.assertFalse(hasattr(result2, 'C'))
+    assert not hasattr(result3, 'A')
+    assert not hasattr(result3, 'B')
+    assert not hasattr(result3, 'C')
+    assert not hasattr(result3, 'progressReporter')
 
-        self.assertFalse(hasattr(result3, 'A'))
-        self.assertFalse(hasattr(result3, 'B'))
-        self.assertFalse(hasattr(result3, 'C'))
-        self.assertFalse(hasattr(result3, 'progressReporter'))
+    # the results returned from other processes do.
+    returnedResults = obj.receive()
+    assert 'task1' == returnedResults[0].data
+    assert 111 == returnedResults[0].A
+    assert 222 == returnedResults[0].B
+    assert 333 == returnedResults[0].C
 
-        # the results returned from other processes do.
-        returnedResults = obj.receive()
-        self.assertEqual('task1', returnedResults[0].data)
-        self.assertEqual(111, returnedResults[0].A)
-        self.assertEqual(222, returnedResults[0].B)
-        self.assertEqual(333, returnedResults[0].C)
+    assert 'task2' == returnedResults[1].data
+    assert 444 == returnedResults[1].A
+    assert 555 == returnedResults[1].B
+    assert 666 == returnedResults[1].C
 
-        self.assertEqual('task2', returnedResults[1].data)
-        self.assertEqual(444, returnedResults[1].A)
-        self.assertEqual(555, returnedResults[1].B)
-        self.assertEqual(666, returnedResults[1].C)
+    assert 'task3' == returnedResults[2].data
+    assert 777 == returnedResults[2].A
+    assert 888 == returnedResults[2].B
+    assert 999 == returnedResults[2].C
+    assert isinstance(returnedResults[2].progressReporter, MockProgressReporter)
 
-        self.assertEqual('task3', returnedResults[2].data)
-        self.assertEqual(777, returnedResults[2].A)
-        self.assertEqual(888, returnedResults[2].B)
-        self.assertEqual(999, returnedResults[2].C)
-        self.assertIsInstance(returnedResults[2].progressReporter, MockProgressReporter)
-
-        obj.close()
+    obj.close()
 
 ##__________________________________________________________________||
