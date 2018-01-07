eb739431a56fd7609a09ed145fe6c01cd8fcc4b3
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-07 15:05:30 +0000

    replace unittest with pytest

--- tests/unit/progressbar/test_ProgressReporter.py
+++ tests/unit/progressbar/test_ProgressReporter.py
@@ -1,5 +1,6 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+
 from alphatwirl.progressbar import ProgressReporter, ProgressReport
-import unittest
 
 ##__________________________________________________________________||
 class MockTime(object):
@@ -14,61 +15,59 @@ class MockQueue(object):
     def empty(self): return len(self.queue) == 0
 
 ##__________________________________________________________________||
-class TestProgressReporter(unittest.TestCase):
+def test_repr():
+    queue = MockQueue()
+    obj = ProgressReporter(queue)
+    repr(obj)
 
-    def test_repr(self):
-        queue = MockQueue()
-        obj = ProgressReporter(queue)
-        repr(obj)
+def test_report():
+    queue = MockQueue()
+    reporter = ProgressReporter(queue)
 
-    def test_report(self):
-        queue = MockQueue()
-        reporter = ProgressReporter(queue)
+    mocktime = MockTime(1000.0)
+    reporter._time = mocktime
 
-        mocktime = MockTime(1000.0)
-        reporter._time = mocktime
+    reporter._readTime()
+    assert 1000.0 == reporter.lastTime
 
-        reporter._readTime()
-        self.assertEqual(1000.0, reporter.lastTime)
+    mocktime.time = 1000.2
+    reporter._report(ProgressReport(name = "dataset1", done = 124, total = 1552))
 
-        mocktime.time = 1000.2
-        reporter._report(ProgressReport(name = "dataset1", done = 124, total = 1552))
+    report = queue.get()
+    assert "dataset1" == report.name
+    assert 124 == report.done
+    assert 1552 == report.total
 
-        report = queue.get()
-        self.assertEqual("dataset1", report.name)
-        self.assertEqual(124, report.done)
-        self.assertEqual(1552, report.total)
+    assert 1000.2 == reporter.lastTime
 
-        self.assertEqual(1000.2, reporter.lastTime)
+def test_needToReport():
+    queue = MockQueue()
+    reporter = ProgressReporter(queue)
 
-    def test_needToReport(self):
-        queue = MockQueue()
-        reporter = ProgressReporter(queue)
+    interval = reporter.interval
+    assert 0.1 == interval
 
-        interval = reporter.interval
-        self.assertEqual(0.1, interval)
+    mocktime = MockTime(1000.0)
+    reporter._time = mocktime
 
-        mocktime = MockTime(1000.0)
-        reporter._time = mocktime
+    reporter._readTime()
+    assert 1000.0 == reporter.lastTime
 
-        reporter._readTime()
-        self.assertEqual(1000.0, reporter.lastTime)
+    # before the interval passes
+    mocktime.time += 0.1*interval
+    report = ProgressReport(name = "dataset1", done = 124, total = 1552)
+    assert not reporter._needToReport(report)
+    assert 1000.0 == reporter.lastTime
 
-        # before the interval passes
-        mocktime.time += 0.1*interval
-        report = ProgressReport(name = "dataset1", done = 124, total = 1552)
-        self.assertFalse(reporter._needToReport(report))
-        self.assertEqual(1000.0, reporter.lastTime)
+    # the last report before the interval passes
+    report = ProgressReport(name = "dataset1", done = 1552, total = 1552)
+    assert reporter._needToReport(report)
+    assert 1000.0 == reporter.lastTime
 
-        # the last report before the interval passes
-        report = ProgressReport(name = "dataset1", done = 1552, total = 1552)
-        self.assertTrue(reporter._needToReport(report))
-        self.assertEqual(1000.0, reporter.lastTime)
-
-        # after the interval passes
-        mocktime.time += 1.2*interval
-        report = ProgressReport(name = "dataset2", done = 1022, total = 4000)
-        self.assertTrue(reporter._needToReport(report))
-        self.assertEqual(1000.0, reporter.lastTime)
+    # after the interval passes
+    mocktime.time += 1.2*interval
+    report = ProgressReport(name = "dataset2", done = 1022, total = 4000)
+    assert reporter._needToReport(report)
+    assert 1000.0 == reporter.lastTime
 
 ##__________________________________________________________________||
