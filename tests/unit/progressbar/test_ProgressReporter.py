f5b0b8ba53b549e6bfc51aa7136095179ed2dd38
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-07 15:13:56 +0000

    replace MockTime with MagicMock

--- tests/unit/progressbar/test_ProgressReporter.py
+++ tests/unit/progressbar/test_ProgressReporter.py
@@ -1,11 +1,12 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
 
 from alphatwirl.progressbar import ProgressReporter, ProgressReport
-
-##__________________________________________________________________||
-class MockTime(object):
-    def __init__(self, time): self.time = time
-    def __call__(self): return self.time
 
 ##__________________________________________________________________||
 class MockQueue(object):
@@ -24,13 +25,13 @@ def test_report():
     queue = MockQueue()
     reporter = ProgressReporter(queue)
 
-    mocktime = MockTime(1000.0)
+    mocktime = mock.MagicMock(return_value = 1000.0)
     reporter._time = mocktime
 
     reporter._readTime()
     assert 1000.0 == reporter.lastTime
 
-    mocktime.time = 1000.2
+    mocktime.return_value = 1000.2
     reporter._report(ProgressReport(name = "dataset1", done = 124, total = 1552))
 
     report = queue.get()
@@ -47,14 +48,14 @@ def test_needToReport():
     interval = reporter.interval
     assert 0.1 == interval
 
-    mocktime = MockTime(1000.0)
+    mocktime = mock.MagicMock(return_value = 1000.0)
     reporter._time = mocktime
 
     reporter._readTime()
     assert 1000.0 == reporter.lastTime
 
     # before the interval passes
-    mocktime.time += 0.1*interval
+    mocktime.return_value += 0.1*interval
     report = ProgressReport(name = "dataset1", done = 124, total = 1552)
     assert not reporter._needToReport(report)
     assert 1000.0 == reporter.lastTime
@@ -65,7 +66,7 @@ def test_needToReport():
     assert 1000.0 == reporter.lastTime
 
     # after the interval passes
-    mocktime.time += 1.2*interval
+    mocktime.return_value += 1.2*interval
     report = ProgressReport(name = "dataset2", done = 1022, total = 4000)
     assert reporter._needToReport(report)
     assert 1000.0 == reporter.lastTime
