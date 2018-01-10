3453ddb44d8d81735d387fc2ce9a18463fe3d8ca
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-10 08:49:58 +0000

    update test_createReporter()

--- tests/unit/progressbar/test_ProgressMonitor.py
+++ tests/unit/progressbar/test_ProgressMonitor.py
@@ -1,11 +1,12 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+import sys
 
 try:
     import unittest.mock as mock
 except ImportError:
     import mock
 
-from alphatwirl.progressbar import ProgressReporter, Queue, ProgressMonitor
+from alphatwirl.progressbar import Queue, ProgressMonitor
 
 ##__________________________________________________________________||
 def test_queue_put():
@@ -22,9 +23,16 @@ def test_begin_end():
     monitor.begin()
     monitor.end()
 
-def test_createReporter():
+def test_createReporter(monkeypatch):
+    module = sys.modules['alphatwirl.progressbar.ProgressMonitor']
+    mock_reporter = mock.MagicMock()
+    MockReporter = mock.MagicMock()
+    MockReporter.return_value = mock_reporter
+    monkeypatch.setattr(module, 'ProgressReporter', MockReporter)
+
     presentation = mock.MagicMock()
     monitor = ProgressMonitor(presentation)
-    assert isinstance(monitor.createReporter(), ProgressReporter)
+    assert monitor.createReporter() is mock_reporter
+    MockReporter.assert_called_once_with(monitor.queue)
 
 ##__________________________________________________________________||
