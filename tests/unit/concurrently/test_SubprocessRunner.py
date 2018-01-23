26a94c367cd9948a66a4a6e3ecd772380ee06576
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-23 10:50:00 +0000

    replace unittest with pytest in test_SubprocessRunner.py

--- tests/unit/concurrently/test_SubprocessRunner.py
+++ tests/unit/concurrently/test_SubprocessRunner.py
@@ -1,9 +1,12 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
 import os
 import stat
-import time
-import tempfile
-import shutil
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
 
 from alphatwirl.concurrently import SubprocessRunner
 
@@ -23,21 +26,24 @@ run_py = run_py.lstrip()
 
 ##__________________________________________________________________||
 class MockWorkingArea(object):
+    def __init__(self, path):
+        self.path = path
+
     def open(self):
-        self.path = self._prepare_dir()
+        self._prepare_dir(path=self.path)
         self._setup_packages()
 
     def close(self):
-        shutil.rmtree(self.path)
-        self.path = None
+        pass
 
-    def _prepare_dir(self):
-        path = tempfile.mkdtemp()
+    def package_path(self, package_index):
+        return self.package_path_dict[package_index]
+
+    def _prepare_dir(self, path):
         path_run_py = os.path.join(path, 'run.py')
         with open(path_run_py, 'w') as f:
             f.write(run_py)
         os.chmod(path_run_py, os.stat(path_run_py).st_mode | stat.S_IXUSR)
-        return path
 
     def _setup_packages(self):
         os.makedirs(os.path.join(self.path, 'aaa'))
@@ -52,65 +58,60 @@ class MockWorkingArea(object):
 
         self.package_path_dict = {0:'aaa', 1:'bbb', 2:'ccc'}
 
-    def package_path(self, package_index):
-        return self.package_path_dict[package_index]
+##__________________________________________________________________||
+@pytest.fixture()
+def workingarea(tmpdir_factory):
+    path = tmpdir_factory.mktemp('')
+    path = str(path)
+    ret = MockWorkingArea(path=path)
+    ret.open()
+    yield ret
+    ret.close()
+
+def test_MockWorkingArea(workingarea):
+    assert 'aaa' == workingarea.package_path(0)
+    assert 'bbb' == workingarea.package_path(1)
+    assert 'ccc' == workingarea.package_path(2)
 
 ##__________________________________________________________________||
-class TestSubprocessRunner(unittest.TestCase):
+@pytest.fixture()
+def obj():
+    return SubprocessRunner()
 
-    def setUp(self):
-        self.workingArea = MockWorkingArea()
-        self.workingArea.open()
+def test_repr(obj):
+    repr(obj)
 
-    def tearDown(self):
-        self.workingArea.close()
+def test_run_wait_terminate(obj, workingarea):
 
-    def test_run_wait_terminate(self):
-        obj = SubprocessRunner(pipe = True)
+    pid1 = obj.run(workingarea, 0)
+    pid2 = obj.run(workingarea, 1)
+    pid3 = obj.run(workingarea, 2)
 
-        pid1 = obj.run(self.workingArea, 0)
-        pid2 = obj.run(self.workingArea, 1)
-        pid3 = obj.run(self.workingArea, 2)
+    assert {pid1, pid2, pid3} == set(obj.wait())
+    # obj.wait() returns a list of finished pids, unsorted
 
-        self.assertEqual({pid1, pid2, pid3}, set(obj.wait()))
-        # obj.wait() returns a list of finished pids, unsorted
+    assert '{} aaa 0.20'.format(pid1) == open(os.path.join(workingarea.path, 'aaa', 'result.txt')).read()
+    assert '{} bbb 0.02'.format(pid2) == open(os.path.join(workingarea.path, 'bbb', 'result.txt')).read()
+    assert '{} ccc 0.15'.format(pid3) == open(os.path.join(workingarea.path, 'ccc', 'result.txt')).read()
 
-        self.assertEqual(
-            '{} aaa 0.20'.format(pid1),
-            open(os.path.join(self.workingArea.path, 'aaa', 'result.txt')).read()
-        )
-        self.assertEqual(
-            '{} bbb 0.02'.format(pid2),
-            open(os.path.join(self.workingArea.path, 'bbb', 'result.txt')).read()
-        )
-        self.assertEqual(
-            '{} ccc 0.15'.format(pid3),
-            open(os.path.join(self.workingArea.path, 'ccc', 'result.txt')).read()
-        )
+    obj.failed_runids([])
 
-        obj.failed_runids([])
+    obj.terminate()
 
-        obj.terminate()
+def test_run_poll_terminate():
+    # don't explicitly test poll() because the finished jobs are
+    # not deterministic. poll() is used by wait() and is
+    # indirectly tested through wait().
+    pass
 
-    def test_run_poll_terminate(self):
-        # don't explicitly test poll() because the finished jobs are
-        # not deterministic. poll() is used by wait() and is
-        # indirectly tested through wait().
-        pass
+def test_run_terminate(obj, workingarea):
+    pid1 = obj.run(workingarea, 0)
+    pid2 = obj.run(workingarea, 1)
+    pid3 = obj.run(workingarea, 2)
+    obj.terminate()
 
-    def test_run_terminate(self):
-        obj = SubprocessRunner(pipe = True)
-        pid1 = obj.run(self.workingArea, 0)
-        pid2 = obj.run(self.workingArea, 1)
-        pid3 = obj.run(self.workingArea, 2)
-        obj.terminate()
-
-    def test_wait_terminate(self):
-        obj = SubprocessRunner(pipe = True)
-        expected = [ ]
-        actual = obj.wait()
-        self.assertEqual(expected, actual)
-        obj.terminate()
+def test_wait_terminate(obj):
+    assert [ ] == obj.wait()
+    obj.terminate()
 
 ##__________________________________________________________________||
-
