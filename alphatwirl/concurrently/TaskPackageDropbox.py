e290b96ed5e0a30033c6b2663aea6c6bc643e6ab
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-23 14:49:30 +0000

    stop catching KeyboardInterrupt

--- alphatwirl/concurrently/TaskPackageDropbox.py
+++ alphatwirl/concurrently/TaskPackageDropbox.py
@@ -39,47 +39,41 @@ class TaskPackageDropbox(object):
 
     def receive(self):
         pkgidx_result_pairs = [ ] # a list of (package_index, _result)
-        try:
-            while self.runid_package_index_map:
+        while self.runid_package_index_map:
 
-                finished_runid = self.dispatcher.poll()
-                # e.g., [1001, 1003]
+            finished_runid = self.dispatcher.poll()
+            # e.g., [1001, 1003]
 
-                runid_pkgidx = [(i, self.runid_package_index_map.pop(i)) for i in finished_runid]
-                # e.g., [(1001, 0), (1003, 2)]
+            runid_pkgidx = [(i, self.runid_package_index_map.pop(i)) for i in finished_runid]
+            # e.g., [(1001, 0), (1003, 2)]
 
-                runid_pkgidx_result = [(ri, pi, self.workingArea.collect_result(pi)) for ri, pi in runid_pkgidx]
-                # e.g., [(1001, 0, result0), (1003, 2, None)] # None indicates the job failed
+            runid_pkgidx_result = [(ri, pi, self.workingArea.collect_result(pi)) for ri, pi in runid_pkgidx]
+            # e.g., [(1001, 0, result0), (1003, 2, None)] # None indicates the job failed
 
-                failed = [e for e in runid_pkgidx_result if e[2] is None]
-                # e.g., [(1003, 2, None)]
+            failed = [e for e in runid_pkgidx_result if e[2] is None]
+            # e.g., [(1003, 2, None)]
 
-                succeeded = [e for e in runid_pkgidx_result if e not in failed]
-                # e.g., [(1001, 0, result0)]
+            succeeded = [e for e in runid_pkgidx_result if e not in failed]
+            # e.g., [(1001, 0, result0)]
 
-                # let the dispatcher know the failed runid
-                failed_runid = [e[0] for e in failed]
-                self.dispatcher.failed_runids(failed_runid)
+            # let the dispatcher know the failed runid
+            failed_runid = [e[0] for e in failed]
+            self.dispatcher.failed_runids(failed_runid)
 
-                # rerun failed jobs
-                for _, pkgidx, _ in failed:
-                    logger = logging.getLogger(__name__)
-                    logger.warning('resubmitting {}'.format(self.workingArea.package_path(pkgidx)))
+            # rerun failed jobs
+            for _, pkgidx, _ in failed:
+                logger = logging.getLogger(__name__)
+                logger.warning('resubmitting {}'.format(self.workingArea.package_path(pkgidx)))
 
-                    runid = self.dispatcher.run(self.workingArea, pkgidx)
-                    self.runid_package_index_map[runid] = pkgidx
+                runid = self.dispatcher.run(self.workingArea, pkgidx)
+                self.runid_package_index_map[runid] = pkgidx
 
-                pairs = [(pkgidx, result) for runid, pkgidx, result in succeeded]
-                # e.g., [(0, result0)] # only successful ones
+            pairs = [(pkgidx, result) for runid, pkgidx, result in succeeded]
+            # e.g., [(0, result0)] # only successful ones
 
-                pkgidx_result_pairs.extend(pairs)
+            pkgidx_result_pairs.extend(pairs)
 
-                time.sleep(self.sleep)
-
-        except KeyboardInterrupt:
-            logger = logging.getLogger(__name__)
-            logger.warning('received KeyboardInterrupt')
-            self.dispatcher.terminate()
+            time.sleep(self.sleep)
 
         # sort in the order of package_index
         pkgidx_result_pairs = sorted(pkgidx_result_pairs, key = itemgetter(0))
