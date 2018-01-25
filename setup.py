638fa0672dd160b60897eb5c55f076a3f7a4dd41
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-25 09:41:15 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup, find_packages
-
+import os
 import versioneer
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
 setup(
-    name = 'alphatwirl',
-    version = versioneer.get_version(),
-    cmdclass = versioneer.get_cmdclass(),
-    description = 'A Python library for summarizing event data',
-    author = 'Tai Sakuma',
-    author_email = 'tai.sakuma@gmail.com',
-    url = 'https://github.com/alphatwirl/alphatwirl',
-    packages = find_packages(exclude=['docs', 'images', 'tests']),
-    classifiers = [
+    name='alphatwirl',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='A Python library for summarizing event data',
+    long_description=long_description,
+    description_content_type='text/markdown',
+    author='Tai Sakuma',
+    author_email='tai.sakuma@gmail.com',
+    url='https://github.com/alphatwirl/alphatwirl',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
-    ]
+    ],
+    packages=find_packages(exclude=['docs', 'images', 'tests']),
 )
