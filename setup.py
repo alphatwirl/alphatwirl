b0f8ca4cbb83f765fc4f4e0d30ac782afbfef6f3
Tai Sakuma <tai.sakuma@gmail.com>
2018-01-28 20:36:26 +0000

    update setup.py - convert README.md to RST with pypandoc

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import os
 import versioneer
+import pypandoc
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md')) as f:
-    long_description = f.read()
+long_description = pypandoc.convert('README.md', 'rst')
 
 setup(
     name='alphatwirl',
@@ -13,7 +13,6 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description='A Python library for summarizing event data',
     long_description=long_description,
-    description_content_type='text/markdown',
     author='Tai Sakuma',
     author_email='tai.sakuma@gmail.com',
     url='https://github.com/alphatwirl/alphatwirl',
