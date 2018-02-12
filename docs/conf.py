e935ce23238b28ae174357d665d2a3680a8ccc62
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-12 08:51:53 +0000

    add ROOT to autodoc_mock_imports

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,7 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
+autodoc_mock_imports = ['ROOT']
 
 # -- Options for HTML output ----------------------------------------------
 
