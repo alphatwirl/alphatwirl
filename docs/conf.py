a2375c28cd1b81841353bcef7f6c887a66fd7ef1
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-12 15:33:53 +0000

    add style.css, change the logo size

--- docs/conf.py
+++ docs/conf.py
@@ -112,5 +112,6 @@ def setup(app):
             'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
+    app.add_stylesheet('style.css')
 
 ##__________________________________________________________________||
