1b7a1c91719c4cef47dad27371dc4ce4624e4360
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-16 17:22:32 +0000

    add __repr__ to ComponentLoop

--- alphatwirl/heppyresult/ComponentLoop.py
+++ alphatwirl/heppyresult/ComponentLoop.py
@@ -6,6 +6,17 @@ class ComponentLoop(object):
     def __init__(self, heppyResult, reader):
         self.reader = reader
         self.heppyResult = heppyResult
+
+    def __repr__(self):
+        name_value_pairs = (
+            ('reader',      self.reader),
+            ('heppyResult', self.heppyResult),
+        )
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join(['{}={!r}'.format(n, v) for n, v in name_value_pairs]),
+        )
+
     def __call__(self):
         self.reader.begin()
         for component in self.heppyResult.components():
