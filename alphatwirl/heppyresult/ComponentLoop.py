9098a5f9385060417bbdd2fe14a6666b36b0a107
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-16 17:59:30 +0000

    clean up the code in ComponentLoop

--- alphatwirl/heppyresult/ComponentLoop.py
+++ alphatwirl/heppyresult/ComponentLoop.py
@@ -6,19 +6,19 @@ from alphatwirl.datasetloop import DatasetLoop
 class ComponentLoop(object):
 
     def __init__(self, heppyResult, reader):
-        self.reader = reader
-        self.heppyResult = heppyResult
-        self.components = self.heppyResult.components()
-        self.datasetloop = DatasetLoop(datasets=self.components, reader=self.reader)
+        self.datasetloop = DatasetLoop(
+            datasets=heppyResult.components(),
+            reader=reader
+        )
+        self.name_value_pairs = (
+            ('reader',      reader),
+            ('heppyResult', heppyResult),
+        )
 
     def __repr__(self):
-        name_value_pairs = (
-            ('reader',      self.reader),
-            ('heppyResult', self.heppyResult),
-        )
         return '{}({})'.format(
             self.__class__.__name__,
-            ', '.join(['{}={!r}'.format(n, v) for n, v in name_value_pairs]),
+            ', '.join(['{}={!r}'.format(n, v) for n, v in self.name_value_pairs]),
         )
 
     def __call__(self):
