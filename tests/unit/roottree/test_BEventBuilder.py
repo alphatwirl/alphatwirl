7b6fc2def0e77ea8e5f7f680536eb44ffd631f05
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-16 15:02:26 +0000

    use pytest in test_BEventBuilder.py

--- tests/unit/roottree/test_BEventBuilder.py
+++ tests/unit/roottree/test_BEventBuilder.py
@@ -1,74 +1,61 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
 import sys
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+has_no_ROOT = False
+try:
+    import ROOT
+except ImportError:
+    has_no_ROOT = True
 
 from alphatwirl.roottree import EventBuilderConfig
 
-##__________________________________________________________________||
-hasROOT = False
-try:
-    import ROOT
-    hasROOT = True
-except ImportError:
-    pass
-
-if hasROOT:
+if not has_no_ROOT:
     from alphatwirl.roottree.BEventBuilder import BEventBuilder
 
 ##__________________________________________________________________||
-class MockTChain(object):
-    def __init__(self, name):
-        self.treeName = name
-        self.paths = [ ]
-
-    def Add(self, name):
-        self.paths.append(name)
+pytestmark = pytest.mark.skipif(has_no_ROOT, reason="has no ROOT")
 
 ##__________________________________________________________________||
-class MockROOT(object):
-    def __init__(self):
-        self.TChain = MockTChain
+@pytest.fixture()
+def mockroot(monkeypatch):
+    ret = mock.Mock()
+    module = sys.modules['alphatwirl.roottree.BEventBuilder']
+    monkeypatch.setattr(module, 'ROOT', ret)
+    return ret
+
+@pytest.fixture()
+def mockevents(monkeypatch):
+    ret = mock.Mock()
+    module = sys.modules['alphatwirl.roottree.BEventBuilder']
+    monkeypatch.setattr(module, 'BEvents', ret)
+    return ret
+
+@pytest.fixture()
+def obj(mockroot, mockevents):
+    config = EventBuilderConfig(
+        inputPaths=['/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root'],
+        treeName='tree',
+        maxEvents=123,
+        start=11,
+        name='TTJets'
+    )
+    return BEventBuilder(config)
 
 ##__________________________________________________________________||
-class MockEvents(object):
-    def __init__(self, tree, maxEvents, start = 0):
-        self.tree = tree
-        self.maxEvents = maxEvents
-        self.start = start
+def test_repr(mockroot, mockevents):
+    pass
 
-##__________________________________________________________________||
-@unittest.skipUnless(hasROOT, "has no ROOT")
-class TestBEventBuilder(unittest.TestCase):
-
-    def setUp(self):
-        self.module = sys.modules['alphatwirl.roottree.BEventBuilder']
-        self.orgROOT = self.module.ROOT
-        self.module.ROOT = MockROOT()
-
-        self.orgEvents = self.module.BEvents
-        self.module.BEvents = MockEvents
-
-    def tearDown(self):
-        self.module.ROOT = self.orgROOT
-        self.module.BEvents = self.orgEvents
-
-    def test_build(self):
-
-        config = EventBuilderConfig(
-            inputPaths = ['/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root'],
-            treeName = 'tree',
-            maxEvents = 123,
-            start = 11,
-            name = 'TTJets'
-        )
-
-        obj = BEventBuilder(config)
-
-        events = obj()
-
-        self.assertEqual(['/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root'], events.tree.paths)
-        self.assertIsInstance(events, MockEvents)
-        self.assertEqual('tree', events.tree.treeName)
-        self.assertEqual(11, events.start)
-        self.assertEqual(123, events.maxEvents)
+def test_build(obj, mockroot, mockevents):
+    events = obj()
+    assert [mock.call('tree')] == mockroot.TChain.call_args_list
+    chain = mockroot.TChain()
+    assert [mock.call('/heppyresult/dir/TTJets/treeProducerSusyAlphaT/tree.root')] == chain.Add.call_args_list
+    assert [mock.call(chain, 123, 11)] == mockevents.call_args_list
 
 ##__________________________________________________________________||
