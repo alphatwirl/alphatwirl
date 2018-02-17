9db61f47b962f00a66b56c06ffad5c4185ea3c99
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-17 12:01:31 +0000

    use pytest in test_run_py.py

--- tests/unit/concurrently/test_run_py.py
+++ tests/unit/concurrently/test_run_py.py
@@ -1,97 +1,99 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
 import os
-import tempfile
-import shutil
 import gzip
-import subprocess
+import shutil
 
 try:
    import cPickle as pickle
 except:
    import pickle
 
+import pytest
+
 import alphatwirl
 import alphatwirl.concurrently
 from alphatwirl.concurrently import TaskPackage
 
 ##__________________________________________________________________||
+@pytest.fixture()
+def env():
+   # add path to alphatwirl to PYTHONPATH in env for subprocess
+   env = os.environ.copy()
+   alphatwirl_path = os.path.dirname(os.path.dirname(os.path.abspath(alphatwirl.__file__)))
+   if 'PYTHONPATH' in env:
+      env['PYTHONPATH'] = '{}:{}'.format(alphatwirl_path, env['PYTHONPATH'])
+   else:
+      env['PYTHONPATH'] = alphatwirl_path
+   return env
+
+@pytest.fixture()
+def workingarea_path(tmpdir_factory):
+   ret = str(tmpdir_factory.mktemp(''))
+   src = os.path.join(os.path.dirname(alphatwirl.concurrently.__file__), 'run.py')
+   dest = os.path.join(ret, 'run.py')
+   shutil.copy(src, dest)
+   return ret
+
+##__________________________________________________________________||
 def task_null(*args, **kwargs):
-    return
+   return
+
+@pytest.fixture()
+def package_rel_path_task_null(workingarea_path):
+
+   ret = 'task_00009.p.gz'
+
+   package = TaskPackage(
+      task = task_null,
+      args = [ ],
+      kwargs =   { }
+   )
+
+   path = os.path.join(workingarea_path, ret)
+   with gzip.open(path, 'wb') as f:
+      pickle.dump(package, f, protocol = pickle.HIGHEST_PROTOCOL)
+
+   return ret
+
+@pytest.mark.script_launch_mode('subprocess')
+def test_run_task_null(script_runner, workingarea_path, package_rel_path_task_null, env):
+
+   script_path = os.path.join('.', 'run.py')
+   args = [package_rel_path_task_null]
+   ret = script_runner.run(script_path, *args, cwd=workingarea_path, env=env)
+   assert ret.success
+   assert '' == ret.stderr
+   assert '' == ret.stdout
 
 ##__________________________________________________________________||
 def task_raise(*args, **kwargs):
     raise Exception('test error from task_raise()')
 
-##__________________________________________________________________||
-class TestRunPy(unittest.TestCase):
+@pytest.fixture()
+def package_rel_path_task_raise(workingarea_path):
 
-    def setUp(self):
-        self.tempdir_path = tempfile.mkdtemp()
-        self.run_py_path = self._copy_run_py_to_tempdir()
-        self.env = self._build_env_for_subprocess()
+   ret = 'task_00009.p.gz'
 
-    def _copy_run_py_to_tempdir(self):
-        src = os.path.join(os.path.dirname(alphatwirl.concurrently.__file__), 'run.py')
-        dest = os.path.join(self.tempdir_path, 'run.py')
-        shutil.copy(src, dest)
-        return dest
+   package = TaskPackage(
+      task = task_raise,
+      args = [ ],
+      kwargs =   { }
+   )
 
-    def _build_env_for_subprocess(self):
-        # add path to alphatwirl to PYTHONPATH in env for subprocess
-        env = os.environ.copy()
-        alphatwirl_path = os.path.dirname(os.path.dirname(os.path.abspath(alphatwirl.__file__)))
-        if 'PYTHONPATH' in env:
-            env['PYTHONPATH'] = '{}:{}'.format(alphatwirl_path, env['PYTHONPATH'])
-        else:
-            env['PYTHONPATH'] = alphatwirl_path
-        return env
+   path = os.path.join(workingarea_path, ret)
+   with gzip.open(path, 'wb') as f:
+      pickle.dump(package, f, protocol = pickle.HIGHEST_PROTOCOL)
 
-    def tearDown(self):
-        shutil.rmtree(self.tempdir_path)
-        pass
+   return ret
 
-    def test_run_task_null(self):
-        package = TaskPackage(
-            task = task_null,
-            args = [ ],
-            kwargs =   { }
+@pytest.mark.script_launch_mode('subprocess')
+def test_run_task_null(script_runner, workingarea_path, package_rel_path_task_raise, env):
 
-        )
-        package_path = os.path.join(self.tempdir_path, 'task_00009.p.gz')
-        with gzip.open(package_path, 'wb') as f:
-            pickle.dump(package, f, protocol = pickle.HIGHEST_PROTOCOL)
-            f.close()
-
-        proc = subprocess.Popen(
-            [os.path.join('.', 'run.py'), 'task_00009.p.gz'],
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-            cwd = self.tempdir_path,
-            env = self.env
-        )
-        stdout, stderr = proc.communicate()
-
-    def test_run_task_raise(self):
-        package = TaskPackage(
-            task = task_raise,
-            args = [ ],
-            kwargs =   { }
-
-        )
-        package_path = os.path.join(self.tempdir_path, 'task_00009.p.gz')
-        with gzip.open(package_path, 'wb') as f:
-            pickle.dump(package, f, protocol = pickle.HIGHEST_PROTOCOL)
-            f.close()
-
-        proc = subprocess.Popen(
-            [os.path.join('.', 'run.py'), 'task_00009.p.gz'],
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-            cwd = self.tempdir_path,
-            env = self.env
-        )
-        stdout, stderr = proc.communicate()
-        self.assertIn('Exception:'.encode(), stderr)
+   script_path = os.path.join('.', 'run.py')
+   args = [package_rel_path_task_raise]
+   ret = script_runner.run(script_path, *args, cwd=workingarea_path, env=env)
+   assert ret.success == False
+   assert 'Exception:' in ret.stderr
+   assert '' == ret.stdout
 
 ##__________________________________________________________________||
-
