84bae20ff2de25581867895a84fe3ee1967e29fa
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-22 21:14:58 +0000

    rewrite test_Events.py with pytest

--- tests/unit/roottree/test_Events.py
+++ tests/unit/roottree/test_Events.py
@@ -1,4 +1,5 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 
 from alphatwirl.roottree import Events
 
@@ -8,7 +9,7 @@ class MockFile(object):
 
 ##__________________________________________________________________||
 class MockTree(object):
-    def __init__(self, entries = 100):
+    def __init__(self, entries=100):
         self.entries = entries
         self.ievent = -1
         self.branch1 = 1111
@@ -26,202 +27,210 @@ class MockTree(object):
             self.ievent = -1
         return nbytes
 
-##__________________________________________________________________||
-class TestMockTree(unittest.TestCase):
+def test_mocktree():
 
-    def test_mocktree(self):
-        tree = MockTree(entries = 3)
-        self.assertIsInstance(tree.GetDirectory(), MockFile)
-        self.assertEqual(3, tree.GetEntries())
+    tree = MockTree(entries=3)
+    assert isinstance(tree.GetDirectory(), MockFile)
+    assert 3 == tree.GetEntries()
 
-        self.assertEqual(-1, tree.ievent)
+    assert -1 == tree.ievent
 
-        nbytes = 10
-        self.assertEqual(nbytes, tree.GetEntry(0))
-        self.assertEqual(0, tree.ievent)
-        self.assertEqual(nbytes, tree.GetEntry(1))
-        self.assertEqual(1, tree.ievent)
-        self.assertEqual(nbytes, tree.GetEntry(2))
-        self.assertEqual(2, tree.ievent)
-        self.assertEqual(0, tree.GetEntry(3))
-        self.assertEqual(-1, tree.ievent)
+    nbytes = 10
+    assert nbytes == tree.GetEntry(0)
+    assert 0 == tree.ievent
+    assert nbytes == tree.GetEntry(1)
+    assert 1 == tree.ievent
+    assert nbytes == tree.GetEntry(2)
+    assert 2 == tree.ievent
+    assert 0 == tree.GetEntry(3)
+    assert -1 == tree.ievent
 
 ##__________________________________________________________________||
-class TestEvents(unittest.TestCase):
+def test_init():
+    tree = MockTree()
+    events = Events(tree)
+    events = Events(tree, 100)
 
-    def setUp(self):
-        self.tree = MockTree()
-        self.events = Events(self.tree)
+    assert tree is events.tree
 
-    def test_init(self):
-        tree = MockTree()
-        events = Events(tree)
-        events = Events(tree, 100)
+def test_repr():
+    tree = MockTree()
+    events = Events(tree)
+    repr(events)
 
-        self.assertIs(tree, events.tree)
+def test_nEvents():
+    tree = MockTree(entries=100)
+    events = Events(tree)
+    assert 100 == events.nEvents # default the same as entries
 
-    def test_repr(self):
-        repr(self.events)
+    events = Events(tree, -1)
+    assert 100 == events.nEvents # the same as entries
 
-    def test_nEvents(self):
-        tree = MockTree(entries = 100)
-        events = Events(tree)
-        self.assertEqual(100, events.nEvents) # default the same as entries
+    events = Events(tree, 50)
+    assert 50 == events.nEvents
 
-        events = Events(tree, -1)
-        self.assertEqual(100, events.nEvents) # the same as entries
+    events = Events(tree, 120)
+    assert 100 == events.nEvents
 
-        events = Events(tree, 50)
-        self.assertEqual(50, events.nEvents)
+    events = Events(tree, 100)
+    assert 100 == events.nEvents
 
-        events = Events(tree, 120)
-        self.assertEqual(100, events.nEvents)
 
-        events = Events(tree, 100)
-        self.assertEqual(100, events.nEvents)
+def test_nEvents_start():
 
-    def test_nEvents_start(self):
+    tree = MockTree(entries=100)
 
-        tree = MockTree(entries = 100)
+    events = Events(tree, maxEvents=-1, start=1)
+    assert 99 == events.nEvents
 
-        events = Events(tree, maxEvents = -1, start = 1)
-        self.assertEqual(99, events.nEvents)
+    events = Events(tree, maxEvents=10, start=1)
+    assert 10 == events.nEvents
 
-        events = Events(tree, maxEvents = 10, start = 1)
-        self.assertEqual(10, events.nEvents)
+    events = Events(tree, maxEvents=-1, start=99)
+    assert 1 == events.nEvents
 
-        events = Events(tree, maxEvents = -1, start = 99)
-        self.assertEqual(1, events.nEvents)
+    events = Events(tree, maxEvents=20, start=99)
+    assert 1 == events.nEvents
 
-        events = Events(tree, maxEvents = 20, start = 99)
-        self.assertEqual(1, events.nEvents)
+    events = Events(tree, maxEvents=-1, start=100)
+    assert 0 == events.nEvents
 
-        events = Events(tree, maxEvents = -1, start = 100)
-        self.assertEqual(0, events.nEvents)
+    events = Events(tree, maxEvents=-1, start=110)
+    assert 0 == events.nEvents
 
-        events = Events(tree, maxEvents = -1, start = 110)
-        self.assertEqual(0, events.nEvents)
+    events = Events(tree, maxEvents=10, start=110)
+    assert 0 == events.nEvents
 
-        events = Events(tree, maxEvents = 10, start = 110)
-        self.assertEqual(0, events.nEvents)
+    with pytest.raises(ValueError):
+        Events(tree, maxEvents=-1, start=-10)
 
-        self.assertRaises(ValueError, Events, tree, maxEvents = -1, start = -10)
 
-    def test_iter_iEvent(self):
-        tree = MockTree(entries = 4)
-        events = Events(tree)
-        self.assertEqual(-1, events.iEvent)
+def test_iter_iEvent():
 
-        it = iter(events)
-        event = next(it)
-        self.assertEqual(0, event.iEvent)
-        self.assertEqual(0, tree.ievent)
-        event = next(it)
-        self.assertEqual(1, event.iEvent)
-        self.assertEqual(1, tree.ievent)
-        event = next(it)
-        self.assertEqual(2, event.iEvent)
-        self.assertEqual(2, tree.ievent)
-        event = next(it)
-        self.assertEqual(3, event.iEvent)
-        self.assertEqual(3, tree.ievent)
-        self.assertRaises(StopIteration, next, it)
-        self.assertEqual(-1, event.iEvent)
+    tree = MockTree(entries=4)
+    events = Events(tree)
+    assert -1 == events.iEvent
 
-    def test_iter_maxEvents(self):
-        tree = MockTree(entries = 40)
-        events = Events(tree, maxEvents = 4)
-        self.assertEqual(-1, events.iEvent)
+    it = iter(events)
+    event = next(it)
+    assert 0 == event.iEvent
+    assert 0 == tree.ievent
+    event = next(it)
+    assert 1 == event.iEvent
+    assert 1 == tree.ievent
+    event = next(it)
+    assert 2 == event.iEvent
+    assert 2 == tree.ievent
+    event = next(it)
+    assert 3 == event.iEvent
+    assert 3 == tree.ievent
+    with pytest.raises(StopIteration):
+        next(it)
+    assert -1 == event.iEvent
 
-        it = iter(events)
-        event = next(it)
-        self.assertEqual(0, event.iEvent)
-        event = next(it)
-        self.assertEqual(1, event.iEvent)
-        event = next(it)
-        self.assertEqual(2, event.iEvent)
-        event = next(it)
-        self.assertEqual(3, event.iEvent)
-        self.assertRaises(StopIteration, next, it)
-        self.assertEqual(-1, event.iEvent)
 
-    def test_iter_iEvent_start(self):
-        tree = MockTree(entries = 4)
-        events = Events(tree, start = 2)
-        self.assertEqual(-1, events.iEvent)
+def test_iter_maxEvents():
+    tree = MockTree(entries=40)
+    events = Events(tree, maxEvents=4)
+    assert -1 == events.iEvent
 
-        it = iter(events)
-        event = next(it)
-        self.assertEqual(0, event.iEvent)
-        self.assertEqual(2, tree.ievent)
-        event = next(it)
-        self.assertEqual(1, event.iEvent)
-        self.assertEqual(3, tree.ievent)
-        self.assertRaises(StopIteration, next, it)
-        self.assertEqual(-1, event.iEvent)
+    it = iter(events)
+    event = next(it)
+    assert 0 == event.iEvent
+    event = next(it)
+    assert 1 == event.iEvent
+    event = next(it)
+    assert 2 == event.iEvent
+    event = next(it)
+    assert 3 == event.iEvent
+    with pytest.raises(StopIteration):
+        next(it)
+    assert -1 == event.iEvent
 
-    def test_iter_maxEvents_start(self):
-        tree = MockTree(entries = 40)
-        events = Events(tree, maxEvents = 4, start = 2)
-        self.assertEqual(-1, events.iEvent)
 
-        it = iter(events)
-        event = next(it)
-        self.assertEqual(0, event.iEvent)
-        self.assertEqual(2, tree.ievent)
-        event = next(it)
-        self.assertEqual(1, event.iEvent)
-        self.assertEqual(3, tree.ievent)
-        event = next(it)
-        self.assertEqual(2, event.iEvent)
-        self.assertEqual(4, tree.ievent)
-        event = next(it)
-        self.assertEqual(3, event.iEvent)
-        self.assertEqual(5, tree.ievent)
-        self.assertRaises(StopIteration, next, it)
-        self.assertEqual(-1, event.iEvent)
+def test_iter_iEvent_start():
+    tree = MockTree(entries=4)
+    events = Events(tree, start=2)
+    assert -1 == events.iEvent
 
-    def test_iter_getattr(self):
-        tree = MockTree(entries = 5)
-        events = Events(tree)
-        it = iter(events)
-        event = next(it)
-        self.assertEqual(1111, event.branch1)
-        tree.branch1 = 2222
-        self.assertEqual(2222, event.branch1)
+    it = iter(events)
+    event = next(it)
+    assert 0 == event.iEvent
+    assert 2 == tree.ievent
+    event = next(it)
+    assert 1 == event.iEvent
+    assert 3 == tree.ievent
+    with pytest.raises(StopIteration):
+        next(it)
+    assert -1 ==event.iEvent
 
-    def test_getitem(self):
-        tree = MockTree(entries = 4)
-        events = Events(tree)
-        self.assertEqual(-1, events.iEvent)
 
-        event = events[0]
-        self.assertEqual(0, event.iEvent)
-        self.assertEqual(0, tree.ievent)
-        event = events[1]
-        self.assertEqual(1, event.iEvent)
-        self.assertEqual(1, tree.ievent)
-        event = events[2]
-        self.assertEqual(2, event.iEvent)
-        self.assertEqual(2, tree.ievent)
-        event = events[3]
-        self.assertEqual(3, event.iEvent)
-        self.assertEqual(3, tree.ievent)
-        self.assertRaises(IndexError, events.__getitem__, 4)
-        self.assertEqual(-1, events.iEvent)
+def test_iter_maxEvents_start():
 
-    def test_getitem_start(self):
-        tree = MockTree(entries = 4)
-        events = Events(tree, start = 2)
-        self.assertEqual(-1, events.iEvent)
+    tree = MockTree(entries=40)
+    events = Events(tree, maxEvents=4, start=2)
+    assert -1 == events.iEvent
 
-        event = events[0]
-        self.assertEqual(0, event.iEvent)
-        self.assertEqual(2, tree.ievent)
-        event = events[1]
-        self.assertEqual(1, event.iEvent)
-        self.assertEqual(3, tree.ievent)
-        self.assertRaises(IndexError, events.__getitem__, 4)
-        self.assertEqual(-1, events.iEvent)
+    it = iter(events)
+    event = next(it)
+    assert 0 == event.iEvent
+    assert 2 == tree.ievent
+    event = next(it)
+    assert 1 == event.iEvent
+    assert 3 == tree.ievent
+    event = next(it)
+    assert 2 == event.iEvent
+    assert 4 == tree.ievent
+    event = next(it)
+    assert 3 == event.iEvent
+    assert 5 == tree.ievent
+    with pytest.raises(StopIteration):
+        next(it)
+    assert -1 == event.iEvent
+
+def test_iter_getattr():
+    tree = MockTree(entries=5)
+    events = Events(tree)
+    it = iter(events)
+    event = next(it)
+    assert 1111 == event.branch1
+    tree.branch1 = 2222
+    assert 2222 == event.branch1
+
+def test_getitem():
+    tree = MockTree(entries=4)
+    events = Events(tree)
+    assert -1 == events.iEvent
+
+    event = events[0]
+    assert 0 == event.iEvent
+    assert 0 == tree.ievent
+    event = events[1]
+    assert 1 == event.iEvent
+    assert 1 == tree.ievent
+    event = events[2]
+    assert 2 == event.iEvent
+    assert 2 == tree.ievent
+    event = events[3]
+    assert 3 == event.iEvent
+    assert 3 == tree.ievent
+    with pytest.raises(IndexError):
+        events[4]
+    assert -1 == events.iEvent
+
+def test_getitem_start():
+    tree = MockTree(entries=4)
+    events = Events(tree, start=2)
+    assert -1 == events.iEvent
+
+    event = events[0]
+    assert 0 == event.iEvent
+    assert 2 == tree.ievent
+    event = events[1]
+    assert 1 == event.iEvent
+    assert 3 == tree.ievent
+    with pytest.raises(IndexError):
+        events[4]
+    assert -1 == events.iEvent
 
 ##__________________________________________________________________||
