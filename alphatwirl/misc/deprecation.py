699f07eb0e6dd0c57a6e17d0b527bc85ab2ea22e
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-23 12:02:23 +0000

    define _decorate_class() and _decorate_func() inside atdeprecated()

--- alphatwirl/misc/deprecation.py
+++ alphatwirl/misc/deprecation.py
@@ -6,45 +6,46 @@ import logging
 
 ##__________________________________________________________________||
 def atdeprecated(msg=''):
-    def atdeprecated_imp(f):
+
+    def _decorate_class(c, msg):
+        module_name = c.__module__
+        logger = logging.getLogger(module_name)
+        class_name = c.__name__
+        name = '{}.{}'.format(module_name, class_name)
+        text = '{} is deprecated.'.format(name)
+        if msg:
+            text += ' ' + msg
+
+            original_init = c.__init__
+
+        def init(*args, **kwargs):
+            logger.warning(text)
+            return original_init(*args, **kwargs)
+
+        c.__init__ = init
+        return c
+
+    def _decorate_func(f, msg):
+        module_name = f.__module__
+        logger = logging.getLogger(module_name)
+        name = f.__name__
+        name = '{}.{}'.format(module_name, name)
+        text = '{}() is deprecated.'.format(name)
+        if msg:
+            text += ' ' + msg
+
+        @functools.wraps(f)
+        def g(*args, **kwargs):
+            logger.warning(text)
+            return f(*args, **kwargs)
+        return g
+
+    def _imp(f):
         if inspect.isclass(f):
             return _decorate_class(f, msg)
         return _decorate_func(f, msg)
-    return atdeprecated_imp
+    return _imp
 
 ##__________________________________________________________________||
-def _decorate_class(c, msg):
-    module_name = c.__module__
-    logger = logging.getLogger(module_name)
-    class_name = c.__name__
-    name = '{}.{}'.format(module_name, class_name)
-    text = '{} is deprecated.'.format(name)
-    if msg:
-        text += ' ' + msg
-
-    original_init = c.__init__
-
-    def init(*args, **kwargs):
-        logger.warning(text)
-        return original_init(*args, **kwargs)
-
-    c.__init__ = init
-    return c
-
-##__________________________________________________________________||
-def _decorate_func(f, msg):
-    module_name = f.__module__
-    logger = logging.getLogger(module_name)
-    name = f.__name__
-    name = '{}.{}'.format(module_name, name)
-    text = '{}() is deprecated.'.format(name)
-    if msg:
-        text += ' ' + msg
-
-    @functools.wraps(f)
-    def g(*args, **kwargs):
-        logger.warning(text)
-        return f(*args, **kwargs)
-    return g
 
 ##__________________________________________________________________||
