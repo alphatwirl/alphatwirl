0d053563cdf956d8877192d9fa4e664a229f82a7
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-27 17:34:03 +0000

    use build_parallel() instead of build_parallel_multiprocessing() in build_progressMonitor_communicationChannel()

--- alphatwirl/configure/build_progressMonitor_communicationChannel.py
+++ alphatwirl/configure/build_progressMonitor_communicationChannel.py
@@ -1,11 +1,14 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 from alphatwirl.misc.deprecation import atdeprecated
-from alphatwirl.parallel.build import build_parallel_multiprocessing
+from alphatwirl.parallel.build import build_parallel
 
 ##__________________________________________________________________||
-@atdeprecated(msg='use alphatwirl.parallel.build.build_parallel_multiprocessing() instead.')
+@atdeprecated(msg='use alphatwirl.parallel.build.build_parallel() instead.')
 def build_progressMonitor_communicationChannel(quiet, processes):
-    parallel = build_parallel_multiprocessing(quiet, processes)
+    parallel = build_parallel(
+        parallel_mode='multiprocessing',
+        quiet=quiet, processes=processes
+    )
     return parallel.progressMonitor, parallel.communicationChannel
 
 ##__________________________________________________________________||
