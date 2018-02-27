3342960317a18f751cc94980015f88370ffec6bf
Tai Sakuma <tai.sakuma@gmail.com>
2018-02-27 21:29:44 +0000

    clean up the code in CollectorComposite

--- alphatwirl/loop/CollectorComposite.py
+++ alphatwirl/loop/CollectorComposite.py
@@ -24,7 +24,6 @@ class CollectorComposite(object):
     def __init__(self, progressReporter=None):
 
         self.components = [ ]
-        self.progressReporter = progressReporter
 
     def __repr__(self):
         name_value_pairs = (
@@ -56,7 +55,7 @@ class CollectorComposite(object):
 
         ret = [ ]
         for i, collector in enumerate(self.components):
-            report = ProgressReport(name = 'collecting results', done = i + 1, total = len(self.components))
+            report = ProgressReport(name='collecting results', done=(i + 1), total=len(self.components))
             alphatwirl.progressbar.report_progress(report)
             ret.append(collector.collect([(dataset, tuple(r.readers[i] for r in readerComposites))
                                           for dataset, readerComposites in dataset_readers_list]))
