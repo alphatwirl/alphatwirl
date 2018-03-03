c778848fe7681662a352c33a14e5d194c14fb3b8
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-03 15:04:02 +0000

    follow pep 8

--- alphatwirl/configure/build_counter_collector_pair.py
+++ alphatwirl/configure/build_counter_collector_pair.py
@@ -7,22 +7,22 @@ from ..loop import Collector
 ##__________________________________________________________________||
 def build_counter_collector_pair(tblcfg):
     keyValComposer = KeyValueComposer(
-        keyAttrNames = tblcfg['keyAttrNames'],
-        binnings = tblcfg['binnings'],
-        keyIndices = tblcfg['keyIndices'],
-        valAttrNames = tblcfg['valAttrNames'],
-        valIndices = tblcfg['valIndices']
+        keyAttrNames=tblcfg['keyAttrNames'],
+        binnings=tblcfg['binnings'],
+        keyIndices=tblcfg['keyIndices'],
+        valAttrNames=tblcfg['valAttrNames'],
+        valIndices=tblcfg['valIndices']
     )
     nextKeyComposer = NextKeyComposer(tblcfg['binnings']) if tblcfg['binnings'] is not None else None
     summarizer = Summarizer(
-        Summary = tblcfg['summaryClass']
+        Summary=tblcfg['summaryClass']
     )
     reader = Reader(
-        keyValComposer = keyValComposer,
-        summarizer = summarizer,
-        nextKeyComposer = nextKeyComposer,
-        weightCalculator = tblcfg['weight'],
-        nevents = tblcfg['nevents']
+        keyValComposer=keyValComposer,
+        summarizer=summarizer,
+        nextKeyComposer=nextKeyComposer,
+        weightCalculator=tblcfg['weight'],
+        nevents=tblcfg['nevents']
     )
     resultsCombinationMethod = ToTupleListWithDatasetColumn(
         summaryColumnNames = tblcfg['keyOutColumnNames'] + tblcfg['valOutColumnNames']
