d675f3502192afe3e1e2254c4227d11b40b668c4
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-03 16:13:33 +0000

    copy EventsInDatasetReader to EventDatasetReader

--- alphatwirl/loop/__init__.py
+++ alphatwirl/loop/__init__.py
@@ -5,7 +5,9 @@ from .DatasetIntoEventBuildersSplitter import DatasetIntoEventBuildersSplitter
 from .EventLoop import EventLoop
 from .EventLoopProgressReportWriter import EventLoopProgressReportWriter
 from .EventLoopRunner import EventLoopRunner
-from .EventsInDatasetReader import EventsInDatasetReader
+from .EventDatasetReader import EventDatasetReader
 from .MPEventLoopRunner import MPEventLoopRunner
 from .NullCollector import NullCollector
 from .ReaderComposite import ReaderComposite
+
+from .EventsInDatasetReader import EventsInDatasetReader
