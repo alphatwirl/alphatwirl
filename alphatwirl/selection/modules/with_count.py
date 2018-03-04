985a7a22888eef541370f1558d9c925991474fde
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-04 16:50:14 +0000

    extract base class WithCountBase

--- alphatwirl/selection/modules/with_count.py
+++ alphatwirl/selection/modules/with_count.py
@@ -4,23 +4,11 @@ import copy
 from .Count import Count
 
 ##__________________________________________________________________||
-class AllwCount(object):
-    """select events that meet all conditions
+class WithCountBase(object):
 
-    """
-
-    def __init__(self, name=None):
-        self.name = name if name is not None else 'All'
+    def __init__(self):
         self.selections = [ ]
         self.count = Count()
-
-    def __repr__(self):
-        return '{}(name={!r}, selections={!r}), count={!r}'.format(
-            self.__class__.__name__,
-            self.name,
-            self.selections,
-            self.count
-        )
 
     def add(self, selection):
         self.selections.append(selection)
@@ -29,6 +17,51 @@ class AllwCount(object):
     def begin(self, event):
         for s in self.selections:
             if hasattr(s, 'begin'): s.begin(event)
+
+    def __call__(self, event):
+        return self.event(event)
+
+    def end(self):
+        for s in self.selections:
+            if hasattr(s, 'end'): s.end()
+
+    def merge(self, other):
+        self.count += other.count
+        for s, o in zip(self.selections, other.selections):
+            if not hasattr(s, 'merge'):
+                continue
+            s.merge(o)
+
+    def results(self, increment=False):
+
+        ret = self.count.copy()
+
+        # reversed enumerate
+        for i, s in zip(reversed(range(len(self.selections))), reversed(self.selections)):
+            if hasattr(s, 'results'):
+                ret.insert(i, s.results(increment=True))
+
+        if increment:
+            ret.increment_depth(by=1)
+
+        return ret
+##__________________________________________________________________||
+class AllwCount(WithCountBase):
+    """select events that meet all conditions
+
+    """
+
+    def __init__(self, name=None):
+        super(AllwCount, self).__init__()
+        self.name = name if name is not None else 'All'
+
+    def __repr__(self):
+        return '{}(name={!r}, selections={!r}), count={!r}'.format(
+            self.__class__.__name__,
+            self.name,
+            self.selections,
+            self.count
+        )
 
     def event(self, event):
         ret = True
@@ -41,44 +74,15 @@ class AllwCount(object):
         self.count.count(pass_)
         return ret
 
-    def __call__(self, event):
-        return self.event(event)
-
-    def end(self):
-        for s in self.selections:
-            if hasattr(s, 'end'): s.end()
-
-    def merge(self, other):
-        self.count += other.count
-        for s, o in zip(self.selections, other.selections):
-            if not hasattr(s, 'merge'):
-                continue
-            s.merge(o)
-
-    def results(self, increment=False):
-
-        ret = self.count.copy()
-
-        # reversed enumerate
-        for i, s in zip(reversed(range(len(self.selections))), reversed(self.selections)):
-            if hasattr(s, 'results'):
-                ret.insert(i, s.results(increment=True))
-
-        if increment:
-            ret.increment_depth(by=1)
-
-        return ret
-
 ##__________________________________________________________________||
-class AnywCount(object):
+class AnywCount(WithCountBase):
     """select events that meet any of the conditions
 
     """
 
     def __init__(self, name=None):
+        super(AnywCount, self).__init__()
         self.name = name if name is not None else 'Any'
-        self.selections = [ ]
-        self.count = Count()
 
     def __repr__(self):
         return '{}(name={!r}, selections={!r}), count={!r}'.format(
@@ -87,14 +91,6 @@ class AnywCount(object):
             self.selections,
             self.count
         )
-
-    def add(self, selection):
-        self.selections.append(selection)
-        self.count.add(selection)
-
-    def begin(self, event):
-        for s in self.selections:
-            if hasattr(s, 'begin'): s.begin(event)
 
     def event(self, event):
         ret = False
@@ -105,34 +101,6 @@ class AnywCount(object):
                 ret = True
                 break
         self.count.count(pass_)
-        return ret
-
-    def __call__(self, event):
-        return self.event(event)
-
-    def end(self):
-        for s in self.selections:
-            if hasattr(s, 'end'): s.end()
-
-    def merge(self, other):
-        self.count += other.count
-        for s, o in zip(self.selections, other.selections):
-            if not hasattr(s, 'merge'):
-                continue
-            s.merge(o)
-
-    def results(self, increment=False):
-
-        ret = self.count.copy()
-
-        # reversed enumerate
-        for i, s in zip(reversed(range(len(self.selections))), reversed(self.selections)):
-            if hasattr(s, 'results'):
-                ret.insert(i, s.results(increment=True))
-
-        if increment:
-            ret.increment_depth(by=1)
-
         return ret
 
 ##__________________________________________________________________||
