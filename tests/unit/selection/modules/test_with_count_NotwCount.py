e383b34d8caff87910dcb553efdfffb3ec74f270
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-04 16:29:11 +0000

    add test_nested() to test_with_count_NotwCount.py

--- tests/unit/selection/modules/test_with_count_NotwCount.py
+++ tests/unit/selection/modules/test_with_count_NotwCount.py
@@ -55,3 +55,38 @@ def test_standard(obj, sel1):
     ] == count._results
 
 ##__________________________________________________________________||
+def test_nested(sel1):
+
+    obj1 = NotwCount(selection=sel1)
+    obj = NotwCount(selection=obj1)
+
+    assert [ ] == sel1.begin.call_args_list
+    assert [ ] == sel1.end.call_args_list
+
+    event = mock.Mock()
+    obj.begin(event)
+    assert [mock.call(event)] == sel1.begin.call_args_list
+
+    event = mock.Mock()
+    sel1.return_value = False   # 0/1
+    assert not obj(event)
+
+    event = mock.Mock()
+    sel1.return_value = True   # 1/2
+    assert obj(event)
+
+    event = mock.Mock()
+    sel1.return_value = True   # 2/3
+    assert obj(event)
+
+    obj.end()
+    assert [mock.call()] == sel1.end.call_args_list
+
+    count = obj.results()
+    print count._results
+    assert [
+        [1, 'NotwCount', 'Not', 1, 3],
+        [2, 'MockEventSelection', 'sel1', 2, 3],
+    ] == count._results
+
+##__________________________________________________________________||
