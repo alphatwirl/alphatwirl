13c947d70d7f2d27996bd53c9d52964e5c01ef68
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-04 13:45:47 +0000

    rewrite tests for AllwCount with pytest

--- tests/unit/selection/modules/test_with_count_AllwCount.py
+++ tests/unit/selection/modules/test_with_count_AllwCount.py
@@ -1,166 +1,176 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
 from alphatwirl.selection.modules import AllwCount
-import unittest
 
 ##__________________________________________________________________||
-class MockEvent(object): pass
+@pytest.fixture()
+def obj():
+    return AllwCount()
 
-##__________________________________________________________________||
-class MockEventSelection(object):
-    def __init__(self, name = None):
-        self.name = name
-        self.is_begin_called = False
-        self.is_end_called = False
-        self.ret = True
+def test_repr(obj):
+    repr(obj)
 
-    def begin(self, event):
-        self.is_begin_called = True
+def test_empty(obj):
 
-    def __call__(self, event):
-        return self.ret
+    event = mock.Mock()
+    obj.begin(event)
 
-    def end(self):
-        self.is_end_called = True
+    event = mock.Mock()
+    assert obj(event)
 
-##__________________________________________________________________||
-class Test_AllwCount(unittest.TestCase):
+    count = obj.results()
+    assert [ ] == count._results
 
-    def test_empty(self):
-        obj = AllwCount()
+def test_standard(obj):
+    sel1 = mock.Mock()
+    sel1.name ='sel1'
+    sel1.__class__.__name__ = 'MockEventSelection'
+    del sel1.results
 
-        event = MockEvent()
-        obj.begin(event)
+    sel2 = mock.Mock()
+    sel2.name =''
+    sel2.__class__.__name__ = 'MockEventSelection'
+    del sel2.results
 
-        event = MockEvent()
-        self.assertTrue(obj(event))
+    obj.add(sel1)
+    obj.add(sel2)
 
-        count = obj.results()
-        self.assertEqual([ ], count._results)
+    assert [ ] == sel1.begin.call_args_list
+    assert [ ] == sel2.begin.call_args_list
 
-    def test_standard(self):
-        obj = AllwCount()
-        sel1 = MockEventSelection(name = 'sel1')
-        sel2 = MockEventSelection()
+    assert [ ] == sel1.end.call_args_list
+    assert [ ] == sel2.end.call_args_list
 
-        obj.add(sel1)
-        obj.add(sel2)
 
-        self.assertFalse(sel1.is_begin_called)
-        self.assertFalse(sel2.is_begin_called)
+    event = mock.Mock()
+    obj.begin(event)
+    assert [mock.call(event)] == sel1.begin.call_args_list
+    assert [mock.call(event)] == sel2.begin.call_args_list
 
-        self.assertFalse(sel1.is_end_called)
-        self.assertFalse(sel2.is_end_called)
+    event = mock.Mock()
+    sel1.return_value = True   # 1/1
+    sel2.return_value = True   # 1/1
+    assert obj(event)
 
-        event = MockEvent()
-        obj.begin(event)
-        self.assertTrue(sel1.is_begin_called)
-        self.assertTrue(sel2.is_begin_called)
+    event = mock.Mock()
+    sel1.return_value = True   # 2/2
+    sel2.return_value = False  # 1/2
+    assert not obj(event)
 
-        event = MockEvent()
-        sel1.ret = True   # 1/1
-        sel2.ret = True   # 1/1
-        self.assertTrue(obj(event))
+    event = mock.Mock()
+    sel1.return_value = False  # 2/3
+    sel2.return_value = True   # 1/2
+    assert not obj(event)
 
-        event = MockEvent()
-        sel1.ret = True   # 2/2
-        sel2.ret = False  # 1/2
-        self.assertFalse(obj(event))
+    event = mock.Mock()
+    sel1.ret = False  # 2/4
+    sel2.ret = False  # 1/2
+    assert not obj(event)
 
-        event = MockEvent()
-        sel1.ret = False  # 2/3
-        sel2.ret = True   # 1/2
-        self.assertFalse(obj.event(event))
+    obj.end()
+    assert [mock.call()] == sel1.end.call_args_list
+    assert [mock.call()] == sel2.end.call_args_list
 
-        event = MockEvent()
-        sel1.ret = False  # 2/4
-        sel2.ret = False  # 1/2
-        self.assertFalse(obj.event(event))
+    count = obj.results()
+    assert [
+            [1, 'MockEventSelection', 'sel1', 2, 4],
+            [1, 'MockEventSelection',     '', 1, 2],
+    ] == count._results
 
-        obj.end()
-        self.assertTrue(sel1.is_end_called)
-        self.assertTrue(sel2.is_end_called)
+def test_nested(obj):
+    #
+    # all (obj) --- all (obj1) --- sel (sel11)
+    #            |              +- sel (sel12)
+    #            +- all (obj2) --- sel (sel21)
+    #            |              +- sel (sel22)
+    #            +- sel (sel3)
+    #
+    obj1 = AllwCount('all1')
+    obj2 = AllwCount('all2')
 
-        count = obj.results()
-        self.assertEqual(
-            [
-                [1, 'MockEventSelection', 'sel1', 2, 4],
-                [1, 'MockEventSelection',     '', 1, 2],
-            ],
-            count._results
-        )
+    sel11 = mock.Mock()
+    sel11.name ='sel11'
+    sel11.__class__.__name__ = 'MockEventSelection'
+    del sel11.results
 
-    def test_nested(self):
-        #
-        # all (obj) --- all (obj1) --- sel (sel11)
-        #            |              +- sel (sel12)
-        #            +- all (obj2) --- sel (sel21)
-        #            |              +- sel (sel22)
-        #            +- sel (sel3)
-        #
+    sel12 = mock.Mock()
+    sel12.name ='sel12'
+    sel12.__class__.__name__ = 'MockEventSelection'
+    del sel12.results
 
-        obj = AllwCount()
-        obj1 = AllwCount('all1')
-        obj2 = AllwCount('all2')
-        sel3 = MockEventSelection('sel3')
-        sel11 = MockEventSelection('sel11')
-        sel12 = MockEventSelection('sel12')
-        sel21 = MockEventSelection('sel21')
-        sel22 = MockEventSelection('sel22')
-        obj.add(obj1)
-        obj.add(obj2)
-        obj.add(sel3)
-        obj1.add(sel11)
-        obj1.add(sel12)
-        obj2.add(sel21)
-        obj2.add(sel22)
+    sel21 = mock.Mock()
+    sel21.name ='sel21'
+    sel21.__class__.__name__ = 'MockEventSelection'
+    del sel21.results
 
-        self.assertFalse(sel11.is_begin_called)
-        self.assertFalse(sel12.is_begin_called)
-        self.assertFalse(sel21.is_begin_called)
-        self.assertFalse(sel22.is_begin_called)
-        self.assertFalse(sel3.is_begin_called)
+    sel22 = mock.Mock()
+    sel22.name ='sel22'
+    sel22.__class__.__name__ = 'MockEventSelection'
+    del sel22.results
 
-        self.assertFalse(sel11.is_end_called)
-        self.assertFalse(sel12.is_end_called)
-        self.assertFalse(sel21.is_end_called)
-        self.assertFalse(sel22.is_end_called)
-        self.assertFalse(sel3.is_end_called)
+    sel3 = mock.Mock()
+    sel3.name ='sel3'
+    sel3.__class__.__name__ = 'MockEventSelection'
+    del sel3.results
 
-        event = MockEvent()
-        obj.begin(event)
-        self.assertTrue(sel11.is_begin_called)
-        self.assertTrue(sel12.is_begin_called)
-        self.assertTrue(sel21.is_begin_called)
-        self.assertTrue(sel22.is_begin_called)
-        self.assertTrue(sel3.is_begin_called)
+    obj.add(obj1)
+    obj.add(obj2)
+    obj.add(sel3)
+    obj1.add(sel11)
+    obj1.add(sel12)
+    obj2.add(sel21)
+    obj2.add(sel22)
 
-        event = MockEvent()
-        sel11.ret = True   # 1/1
-        sel12.ret = True   # 1/1
-        sel21.ret = True   # 1/1
-        sel22.ret = True   # 1/1
-        sel3.ret = True    # 1/1
-        self.assertTrue(obj(event))
+    assert [ ] == sel11.begin.call_args_list
+    assert [ ] == sel12.begin.call_args_list
+    assert [ ] == sel21.begin.call_args_list
+    assert [ ] == sel22.begin.call_args_list
+    assert [ ] == sel3.begin.call_args_list
 
-        obj.end()
-        self.assertTrue(sel11.is_end_called)
-        self.assertTrue(sel12.is_end_called)
-        self.assertTrue(sel21.is_end_called)
-        self.assertTrue(sel22.is_end_called)
-        self.assertTrue(sel3.is_end_called)
+    assert [ ] == sel11.end.call_args_list
+    assert [ ] == sel12.end.call_args_list
+    assert [ ] == sel21.end.call_args_list
+    assert [ ] == sel22.end.call_args_list
+    assert [ ] == sel3.end.call_args_list
 
-        count = obj.results()
-        self.assertEqual(
-            [
-                [1, 'AllwCount', 'all1',  1, 1],
-                [2, 'MockEventSelection',     'sel11', 1, 1],
-                [2, 'MockEventSelection',     'sel12', 1, 1],
-                [1, 'AllwCount', 'all2',  1, 1],
-                [2, 'MockEventSelection',     'sel21', 1, 1],
-                [2, 'MockEventSelection',     'sel22', 1, 1],
-                [1, 'MockEventSelection',     'sel3',  1, 1],
-            ],
-            count._results
-        )
+    event = mock.Mock()
+    obj.begin(event)
+    assert [mock.call(event)] == sel11.begin.call_args_list
+    assert [mock.call(event)] == sel12.begin.call_args_list
+    assert [mock.call(event)] == sel21.begin.call_args_list
+    assert [mock.call(event)] == sel22.begin.call_args_list
+    assert [mock.call(event)] == sel3.begin.call_args_list
+
+    event = mock.Mock()
+    sel11.return_value = True   # 1/1
+    sel12.return_value = True   # 1/1
+    sel21.return_value = True   # 1/1
+    sel22.return_value = True   # 1/1
+    sel3.return_value = True    # 1/1
+    assert obj(event)
+
+    obj.end()
+    assert [mock.call()] == sel11.end.call_args_list
+    assert [mock.call()] == sel12.end.call_args_list
+    assert [mock.call()] == sel21.end.call_args_list
+    assert [mock.call()] == sel22.end.call_args_list
+    assert [mock.call()] == sel3.end.call_args_list
+
+    count = obj.results()
+    assert [
+        [1, 'AllwCount', 'all1',  1, 1],
+        [2, 'MockEventSelection',     'sel11', 1, 1],
+        [2, 'MockEventSelection',     'sel12', 1, 1],
+        [1, 'AllwCount', 'all2',  1, 1],
+        [2, 'MockEventSelection',     'sel21', 1, 1],
+        [2, 'MockEventSelection',     'sel22', 1, 1],
+        [1, 'MockEventSelection',     'sel3',  1, 1],
+    ] == count._results
 
 ##__________________________________________________________________||
