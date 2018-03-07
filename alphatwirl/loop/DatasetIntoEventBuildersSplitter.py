b1cda14dc4ae57ca168439b325bfe29df6c654b2
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-07 13:48:28 +0000

    remove the private methods moved to splitfuncs.py

--- alphatwirl/loop/DatasetIntoEventBuildersSplitter.py
+++ alphatwirl/loop/DatasetIntoEventBuildersSplitter.py
@@ -1,6 +1,6 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 
-from .splitfuncs import create_file_start_length_list
+from .splitfuncs import create_files_start_length_list
 
 ##__________________________________________________________________||
 class DatasetIntoEventBuildersSplitter(object):
@@ -16,7 +16,6 @@ class DatasetIntoEventBuildersSplitter(object):
         self.max_events_per_run = maxEventsPerRun
         self.max_files = maxFiles
         self.max_files_per_run = maxFilesPerRun
-        self.create_file_start_length_list = create_file_start_length_list
 
     def __repr__(self):
         return '{}(EventBuilder={!r}, eventBuilderConfigMaker={!r}, maxEvents={!r}, maxEventsPerRun={!r}, maxFiles={!r}, maxFilesPerRun={!r})'.format(
@@ -34,10 +33,10 @@ class DatasetIntoEventBuildersSplitter(object):
         files = self.eventBuilderConfigMaker.file_list_in(dataset, maxFiles=self.max_files)
         # e.g., ['A.root', 'B.root', 'C.root', 'D.root', 'E.root']
 
-        files_start_length_list = self._file_start_length_list(
+        files_start_length_list = create_files_start_length_list(
             files,
-            max_events=self.max_events,
             func_get_nevents_in_file=self.eventBuilderConfigMaker.nevents_in_file,
+            max_events=self.max_events,
             max_events_per_run=self.max_events_per_run,
             max_files_per_run=self.max_files_per_run
         )
@@ -54,59 +53,6 @@ class DatasetIntoEventBuildersSplitter(object):
         configs = self._create_configs(dataset, files_start_length_list)
         eventBuilders = [self.EventBuilder(c) for c in configs]
         return eventBuilders
-
-    def _file_start_length_list(self, files, func_get_nevents_in_file,
-                                max_events, max_events_per_run,
-                                max_files_per_run):
-
-        if not self._need_get_number_of_events_in_files(max_events, max_events_per_run):
-            return self._fast_path(files, max_files_per_run)
-
-        return self._full_path(files, func_get_nevents_in_file, max_events,
-                               max_events_per_run, max_files_per_run)
-
-    def _need_get_number_of_events_in_files(self, max_events, max_events_per_run):
-        return max_events >= 0 or max_events_per_run >= 0
-
-    def _fast_path(self, files, max_files_per_run):
-        if not files:
-            return [ ]
-        if max_files_per_run < 0:
-            return [(files, 0, -1)]
-        if max_files_per_run == 0:
-            return [ ]
-        return [(files[i:(i + max_files_per_run)], 0, -1) for i in range(0, len(files), max_files_per_run)]
-
-    def _full_path(self, files, func_get_nevents_in_file, max_events, max_events_per_run, max_files_per_run):
-
-        # this can be slow
-        file_nevents_list = self._file_nevents_list_(
-            files,
-            func_get_nevents_in_file=func_get_nevents_in_file,
-            max_events=max_events
-        )
-
-        file_start_length_list = self.create_file_start_length_list(
-            file_nevents_list=file_nevents_list,
-            max_events_per_run=max_events_per_run,
-            max_events_total=max_events,
-            max_files_per_run=max_files_per_run
-        )
-        return file_start_length_list
-
-    def _file_nevents_list_(self, files, func_get_nevents_in_file, max_events):
-        total_events = 0
-        ret = [ ]
-        for f in files:
-            if 0 <= max_events <= total_events:
-                break
-
-            # this can be slow
-            n = func_get_nevents_in_file(f)
-
-            ret.append((f, n))
-            total_events += n
-        return ret
 
     def _create_configs(self, dataset, file_start_length_list):
         configs = [ ]
