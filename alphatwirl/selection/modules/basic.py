06564202c5464e749020670422837d64bb898aa6
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-08 15:02:01 +0000

    extract Base from All and Any

--- alphatwirl/selection/modules/basic.py
+++ alphatwirl/selection/modules/basic.py
@@ -1,13 +1,13 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 
 ##__________________________________________________________________||
-class All(object):
-    """select events that meet all conditions
+class Base(object):
+    """The base class of the classes All and Any
 
     """
 
-    def __init__(self, name=None):
-        self.name = name if name is not None else 'All'
+    def __init__(self, name):
+        self.name = name
         self.selections = [ ]
 
     def __repr__(self):
@@ -25,52 +25,43 @@ class All(object):
             if hasattr(s, 'begin'): s.begin(event)
 
     def event(self, event):
-        for s in self.selections:
-            if not s(event): return False
-        return True
-
-    def __call__(self, event):
-        return self.event(event)
+        return self(event)
 
     def end(self):
         for s in self.selections:
             if hasattr(s, 'end'): s.end()
 
 ##__________________________________________________________________||
-class Any(object):
+class All(Base):
+    """select events that meet all conditions
+
+    """
+
+    def __init__(self, name=None):
+        if name is None:
+            name = 'All'
+        super(All, self).__init__(name)
+
+    def __call__(self, event):
+        for s in self.selections:
+            if not s(event): return False
+        return True
+
+##__________________________________________________________________||
+class Any(Base):
     """select events that meet any of the conditions
 
     """
 
     def __init__(self, name=None):
-        self.name = name if name is not None else 'Any'
-        self.selections = [ ]
+        if name is None:
+            name = 'Any'
+        super(Any, self).__init__(name)
 
-    def __repr__(self):
-        return '{}(name={!r}, selections={!r})'.format(
-            self.__class__.__name__,
-            self.name,
-            self.selections
-        )
-
-    def add(self, selection):
-        self.selections.append(selection)
-
-    def begin(self, event):
-        for s in self.selections:
-            if hasattr(s, 'begin'): s.begin(event)
-
-    def event(self, event):
+    def __call__(self, event):
         for s in self.selections:
             if s(event): return True
         return False
-
-    def __call__(self, event):
-        return self.event(event)
-
-    def end(self):
-        for s in self.selections:
-            if hasattr(s, 'end'): s.end()
 
 ##__________________________________________________________________||
 class Not(object):
