f389c91a8cbe886bb95b7367e246a4bb69a80faa
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-11 15:20:50 +0000

    rewrite test for build_selection() with pytest

--- tests/unit/selection/test_build_selection.py
+++ tests/unit/selection/test_build_selection.py
@@ -1,5 +1,11 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
 import sys
-import unittest
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
 
 from alphatwirl.selection import build_selection
 from alphatwirl.selection.modules import All
@@ -8,58 +14,29 @@ from alphatwirl.selection.modules import Not
 from alphatwirl.selection.modules.LambdaStr import LambdaStr
 
 ##__________________________________________________________________||
-class MockFactoryDispatcher(object):
-    def __call__(self, **kargs):
-        return kargs
+@pytest.fixture()
+def MockFactoryDispatcher(monkeypatch):
+    ret = mock.Mock()
+    module = sys.modules['alphatwirl.selection.funcs']
+    monkeypatch.setattr(module, 'FactoryDispatcher', ret)
+    return ret
 
-##__________________________________________________________________||
-class Test_buildSelection(unittest.TestCase):
+def test_call_kargs(MockFactoryDispatcher):
 
-    def setUp(self):
-        self.module = sys.modules['alphatwirl.selection.funcs']
-        self._org_FactoryDispatcher = self.module.FactoryDispatcher
-        self.module.FactoryDispatcher = MockFactoryDispatcher()
+    kargs = dict(
+        arg1=10,
+        arg2=20,
+        level=dict(factory='test_level1', arg2=2, arg3=3)
+    )
 
-    def tearDown(self):
-        self.module.FactoryDispatcher = self._org_FactoryDispatcher
+    obj = build_selection(**kargs)
 
-    def test_call_kargs(self):
+    assert MockFactoryDispatcher() is obj
 
-        kargs = dict(
-            arg1 = 10,
-            arg2 = 20,
-            level = dict(factory = 'test_level1', arg2 = 2, arg3 = 3)
-        )
-
-        obj = build_selection(**kargs)
-
-        self.assertIsNot(kargs, obj)
-        obj.pop('AllClass')
-        obj.pop('AnyClass')
-        obj.pop('NotClass')
-        obj.pop('LambdaStrClass')
-        obj.pop('aliasDict')
-        self.assertEqual(kargs, obj)
-
-    def test_call_default_modules(self):
-
-        obj = build_selection(
-            arg1 = 10,
-            arg2 = 20,
-            level = dict(factory = 'test_level1', arg2 = 2, arg3 = 3)
-            )
-
-        self.assertIs(All, obj.pop('AllClass'))
-        self.assertIs(Any, obj.pop('AnyClass'))
-        self.assertIs(Not, obj.pop('NotClass'))
-        self.assertIs(LambdaStr, obj.pop('LambdaStrClass'))
-        obj.pop('aliasDict')
-
-        expected = dict(
-            arg1 = 10,
-            arg2 = 20,
-            level = dict(factory = 'test_level1', arg2 = 2, arg3 = 3)
-        )
-        self.assertEqual(expected, obj)
+    expected = [mock.call(
+        AllClass=All, AnyClass=Any, NotClass=Not,
+        LambdaStrClass=LambdaStr, aliasDict={ },
+        arg1=10, arg2=20, level=dict(factory='test_level1', arg2=2, arg3=3))
+    ] == MockFactoryDispatcher.call_args_list
 
 ##__________________________________________________________________||
