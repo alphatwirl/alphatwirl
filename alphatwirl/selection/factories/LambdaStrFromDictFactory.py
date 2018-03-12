14c7dbe89317cf50f8530116b4de3e7f8dff1b3c
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-12 13:43:56 +0000

    remove LambdaStrFromDictFactory, replaced with LambdaStrFactory long time ago

--- alphatwirl/selection/factories/LambdaStrFromDictFactory.py
+++
@@ -1,7 +0,0 @@
-# Tai Sakuma <tai.sakuma@gmail.com>
-
-##__________________________________________________________________||
-def LambdaStrFromDictFactory(key, **kargs):
-    return kargs['LambdaStrClass'](lambda_str = kargs['aliasDict'][key].format(**kargs), name = key)
-
-##__________________________________________________________________||
