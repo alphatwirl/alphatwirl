6340cd915a81b808a9ebf51666aefe6dd0f0be03
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-12 14:31:42 +0000

    rewrite test for AllFactory with pytest

--- tests/unit/selection/factories/test_AllFactory.py
+++ tests/unit/selection/factories/test_AllFactory.py
@@ -1,15 +1,45 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import sys
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
 from alphatwirl.selection.factories.AllFactory import AllFactory
-from alphatwirl.selection.modules.basic import All
-from alphatwirl.selection.modules.basic import Any
-from alphatwirl.selection.modules.LambdaStr import LambdaStr
-import unittest
 
 ##__________________________________________________________________||
-class Test_AllFactory(unittest.TestCase):
+@pytest.fixture()
+def MockFactoryDispatcher(monkeypatch):
+    ret = mock.Mock()
+    module = sys.modules['alphatwirl.selection.factories.AllFactory']
+    monkeypatch.setattr(module, 'FactoryDispatcher', ret)
+    return ret
 
-    def test_obj(self):
-        path_cfg_list = ("ev : ev.nJet[0] >= 2", "ev : ev.nMET[0] >= 200")
-        kargs = dict(arg1 = 10, arg2 = 20, AllClass = All, LambdaStrClass = LambdaStr)
-        obj = AllFactory(path_cfg_list, name = 'test_all', **kargs)
+@pytest.fixture()
+def MockAll():
+    return mock.Mock()
+
+@pytest.mark.parametrize('kwargs, expected', [
+    pytest.param(
+        dict(
+            path_cfg_list = ("ev : ev.nJet[0] >= 2", "ev : ev.nMET[0] >= 200"),
+            name='test_all',
+        ),
+        dict(
+            name='test_all',
+        ),
+        id='simple',
+    ),
+])
+def test_call(kwargs, expected, MockAll, MockFactoryDispatcher):
+    actual = AllFactory(AllClass=MockAll, **kwargs)
+    assert [mock.call(**expected)] == MockAll.call_args_list
+    assert actual == MockAll()
+    assert [
+        mock.call(MockFactoryDispatcher()),
+        mock.call(MockFactoryDispatcher()),
+    ] == MockAll().add.call_args_list
 
 ##__________________________________________________________________||
