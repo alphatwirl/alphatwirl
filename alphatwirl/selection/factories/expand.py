eff6f4e5bb5200c1ccc07a84d00bbefa8136b341
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-13 13:21:47 +0000

    clean up the code in expand.py

--- alphatwirl/selection/factories/expand.py
+++ alphatwirl/selection/factories/expand.py
@@ -2,46 +2,37 @@
 
 ##__________________________________________________________________||
 def expand_path_cfg(path_cfg, alias_dict={ }, overriding_kargs={ }):
-
-    if isinstance(path_cfg, str):
-        return _expand_path_cfg_str(path_cfg, alias_dict, overriding_kargs)
-
-    if isinstance(path_cfg, dict):
-        return _expand_path_cfg_dict(path_cfg, alias_dict)
-
-    # assume tuple or list
-    return _expand_path_cfg_tuple(path_cfg, alias_dict, overriding_kargs)
-
-##__________________________________________________________________||
-def _expand_path_cfg_str(path_cfg, alias_dict, overriding_kargs):
-    """expand a path config given as a string
+    """expand a path config
 
     Args:
-        path_cfg (str): to be given to lambda or an alias
-        alias_dict (dict):
-        overriding_kargs (dict):
+        path_cfg (str, tuple, dict): a config for path
+        alias_dict (dict): a dict for aliases
+        overriding_kargs (dict): to be used for recursive call
     """
 
+    if isinstance(path_cfg, str):
+        return _expand_str(path_cfg, alias_dict, overriding_kargs)
+
+    if isinstance(path_cfg, dict):
+        return _expand_dict(path_cfg, alias_dict)
+
+    # assume tuple or list
+    return _expand_tuple(path_cfg, alias_dict, overriding_kargs)
+
+##__________________________________________________________________||
+def _expand_str(path_cfg, alias_dict, overriding_kargs):
+    """expand a path config given as a string
+
+    """
 
     if path_cfg in alias_dict:
+        # e.g., path_cfg = 'var_cut'
+        return _expand_str_alias(path_cfg, alias_dict, overriding_kargs)
 
-        # e.g.,
-        # path_cfg = 'var_cut'
+    # e.g., path_cfg = 'ev : {low} <= ev.var[0] < {high}'
+    return _expand_for_lambda_str(path_cfg, alias_dict, overriding_kargs)
 
-        new_path_cfg = alias_dict[path_cfg]
-        # e.g., ('ev : {low} <= ev.var[0] < {high}', {'low': 10, 'high': 200})
-
-        new_overriding_kargs = dict(alias=path_cfg)
-        # e.g., {'alias': 'var_cut'}
-
-        new_overriding_kargs.update(overriding_kargs)
-        # e.g., {'alias': 'var_cut', 'name': 'var_cut25', 'low': 25}
-
-        return expand_path_cfg(
-            new_path_cfg,
-            alias_dict=alias_dict,
-            overriding_kargs=new_overriding_kargs
-        )
+def _expand_for_lambda_str(path_cfg, alias_dict, overriding_kargs):
 
     # e.g.,
     # path_cfg = 'ev : {low} <= ev.var[0] < {high}'
@@ -63,7 +54,7 @@ def _expand_path_cfg_str(path_cfg, alias_dict, overriding_kargs):
         ret['name'] = overriding_kargs_copy.pop('name')
 
     ret.update(overriding_kargs_copy)
-    # e.g., 
+    # e.g.,
     # {
     #     'factory': 'LambdaStrFactory',
     #     'lambda_str': 'ev : {low} <= ev.var[0] < {high}',
@@ -73,14 +64,33 @@ def _expand_path_cfg_str(path_cfg, alias_dict, overriding_kargs):
 
     return ret
 
-##__________________________________________________________________||
-def _expand_path_cfg_tuple(path_cfg, alias_dict, overriding_kargs):
-    """expand a path config given as a tuple
+def _expand_str_alias(path_cfg, alias_dict, overriding_kargs):
+    """expand a path config given as a string
 
     Args:
-        path_cfg (tuple): a tuple with two elements: (str, dict) 
+        path_cfg (str): an alias
         alias_dict (dict):
         overriding_kargs (dict):
+    """
+
+    # e.g.,
+    # path_cfg = 'var_cut'
+
+    new_path_cfg = alias_dict[path_cfg]
+    # e.g., ('ev : {low} <= ev.var[0] < {high}', {'low': 10, 'high': 200})
+
+    new_overriding_kargs = dict(alias=path_cfg)
+    # e.g., {'alias': 'var_cut'}
+
+    new_overriding_kargs.update(overriding_kargs)
+    # e.g., {'alias': 'var_cut', 'name': 'var_cut25', 'low': 25}
+
+    return expand_path_cfg(new_path_cfg, alias_dict,new_overriding_kargs)
+
+##__________________________________________________________________||
+def _expand_tuple(path_cfg, alias_dict, overriding_kargs):
+    """expand a path config given as a tuple
+
     """
 
     # e.g.,
@@ -104,7 +114,7 @@ def _expand_path_cfg_tuple(path_cfg, alias_dict, overriding_kargs):
 
 
 ##__________________________________________________________________||
-def _expand_path_cfg_dict(path_cfg, alias_dict):
+def _expand_dict(path_cfg, alias_dict):
     if 'factory' in path_cfg:
         return path_cfg
 
