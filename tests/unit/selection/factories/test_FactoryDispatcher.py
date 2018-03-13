3a4dbbfa20194e8926da3f27f86263b1e090c063
Tai Sakuma <tai.sakuma@gmail.com>
2018-03-13 10:56:05 +0000

    update test for FactoryDispatcher

--- tests/unit/selection/factories/test_FactoryDispatcher.py
+++ tests/unit/selection/factories/test_FactoryDispatcher.py
@@ -1,6 +1,4 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
-import os
-import sys
 import pytest
 
 from alphatwirl.selection.factories.FactoryDispatcher import FactoryDispatcher
@@ -8,68 +6,60 @@ from alphatwirl.selection.modules.LambdaStr import LambdaStr
 from alphatwirl.selection.modules import All, Any, Not
 
 ##__________________________________________________________________||
-@pytest.fixture()
-def alias_dict():
-    return {
-        'alias1': 'ev : ev.var1[0] >= 10',
-        'alias2': ('ev : ev.var2[0] >= 20', dict(name='name2')),
-        'alias3': 'alias1',
-        'alias4': 'alias3',
-        'alias5': 'ev : ev.var4[0] == {n}',
-        'alias6': ('ev : {low} <= ev.var5[0] < {high}', dict(low=11, high=20))
-    }
-
-@pytest.fixture()
-def sys_path(monkeypatch):
-    two_up_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-    monkeypatch.syspath_prepend(two_up_dir)
+alias_dict = {
+    'alias1': 'ev : ev.var1[0] >= 10',
+    'alias2': ('ev : ev.var2[0] >= 20', dict(name='name2')),
+    'alias3': 'alias1',
+    'alias4': 'alias3',
+    'alias5': 'ev : ev.var4[0] == {n}',
+    'alias6': ('ev : {low} <= ev.var5[0] < {high}', dict(low=11, high=20))
+}
 
 ##__________________________________________________________________||
-def test_string(sys_path, alias_dict):
-    kargs = dict(
-        arg1=10, arg2=20,
-        aliasDict=alias_dict,
-        LambdaStrClass=LambdaStr
-    )
-    path_cfg = 'alias1'
-    obj = FactoryDispatcher(path_cfg=path_cfg, **kargs)
-    assert isinstance(obj, LambdaStr)
-    assert 'alias1' == obj.name
-    assert 'ev : ev.var1[0] >= 10' == obj.lambda_str
-
-##__________________________________________________________________||
-@pytest.mark.parametrize('path_cfg, kargs, expected', [
+params = [
     pytest.param(
         dict(Any=(
             'ev : ev.x[0] == 0',
             dict(All=(
                 'ev : ev.x[0] >= 1',
                 'ev : ev.y[0] >= 100',
+                'alias4',
                 )),
             dict(Not=dict(
                 Any=(
                     'ev : ev.z[0] == 0',
                     'ev : ev.w[0] >= 300',
+                    'alias5',
+                    'alias6',
                 ),
             )),
         )),
-        dict(AllClass=All, AnyClass=Any, NotClass=Not, LambdaStrClass=LambdaStr),
+        dict(
+            AllClass=All, AnyClass=Any, NotClass=Not,
+            LambdaStrClass=LambdaStr, aliasDict=alias_dict,
+            n=5242,
+        ),
         Any(selections=[
             LambdaStr('ev : ev.x[0] == 0'),
             All(selections=[
                 LambdaStr('ev : ev.x[0] >= 1'),
                 LambdaStr('ev : ev.y[0] >= 100'),
+                LambdaStr(name='alias4', lambda_str='ev : ev.var1[0] >= 10'),
             ]),
             Not(selection=Any(selections=[
                 LambdaStr('ev : ev.z[0] == 0'),
                 LambdaStr('ev : ev.w[0] >= 300'),
+                LambdaStr(name='alias5', lambda_str='ev : ev.var4[0] == 5242'),
+                LambdaStr(name='alias6', lambda_str='ev : 11 <= ev.var5[0] < 20'),
             ]),
             ),
         ]),
         id='example'
     ),
-])
-def test_dispatcher(sys_path, path_cfg, kargs, expected):
+]
+
+@pytest.mark.parametrize('path_cfg, kargs, expected', params)
+def test_dispatcher(path_cfg, kargs, expected):
     obj = FactoryDispatcher(path_cfg=path_cfg, **kargs)
     assert repr(expected) == repr(obj)
     assert str(expected) == str(obj)
