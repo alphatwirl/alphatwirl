9fba7446d1df6a8e1ed6342c7d328b97994afc71
Tai Sakuma <tai.sakuma@gmail.com>
2018-05-09 09:03:13 +0100

    add tests test_inspect_sample_0[34].py

---
+++ tests/unit/roottree/test_inspect_sample_03.py
@@ -0,0 +1,123 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import os
+import sys
+import pytest
+
+has_no_ROOT = False
+try:
+    import ROOT
+except ImportError:
+    has_no_ROOT = True
+
+if not has_no_ROOT:
+    from alphatwirl.roottree import inspect_tree
+
+##__________________________________________________________________||
+pytestmark = pytest.mark.skipif(has_no_ROOT, reason="has no ROOT")
+
+##__________________________________________________________________||
+@pytest.fixture()
+def tree():
+    input_file_name = 'sample_03.root'
+    tree_name = 'tree'
+    input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_file_name)
+
+    input_file = ROOT.TFile.Open(input_path)
+    yield input_file.Get(tree_name)
+
+
+def test_inspect(tree):
+    assert expected == inspect_tree(tree)
+
+expected = {
+    'leaves': [
+        {'compression_factor': 7.472972972972973,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bChar',
+         'size': 7.05718994140625e-05,
+         'title': 'bChar/B',
+         'type': 'Char_t',
+         'uncompressed_size': 0.0005273818969726562},
+        {'compression_factor': 7.44,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bUChar',
+         'size': 7.152557373046875e-05,
+         'title': 'bUChar/b',
+         'type': 'UChar_t',
+         'uncompressed_size': 0.0005321502685546875},
+        {'compression_factor': 7.298701298701299,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bShort',
+         'size': 7.343292236328125e-05,
+         'title': 'bShort/S',
+         'type': 'Short_t',
+         'uncompressed_size': 0.0005359649658203125},
+        {'compression_factor': 7.269230769230769,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bUShort',
+         'size': 7.43865966796875e-05,
+         'title': 'bUShort/s',
+         'type': 'UShort_t',
+         'uncompressed_size': 0.0005407333374023438},
+        {'compression_factor': 7.0886075949367084,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bInt',
+         'size': 7.534027099609375e-05,
+         'title': 'bInt/I',
+         'type': 'Int_t',
+         'uncompressed_size': 0.0005340576171875},
+        {'compression_factor': 7.0625,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bUInt',
+         'size': 7.62939453125e-05,
+         'title': 'bUInt/i',
+         'type': 'UInt_t',
+         'uncompressed_size': 0.0005388259887695312},
+        {'compression_factor': 7.037037037037037,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bFloat',
+         'size': 7.724761962890625e-05,
+         'title': 'bFloat/F',
+         'type': 'Float_t',
+         'uncompressed_size': 0.0005435943603515625},
+        {'compression_factor': 6.566666666666666,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bDouble',
+         'size': 8.58306884765625e-05,
+         'title': 'bDouble/D',
+         'type': 'Double_t',
+         'uncompressed_size': 0.0005636215209960938},
+        {'compression_factor': 6.566666666666666,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bLong64',
+         'size': 8.58306884765625e-05,
+         'title': 'bLong64/L',
+         'type': 'Long64_t',
+         'uncompressed_size': 0.0005636215209960938},
+        {'compression_factor': 6.549450549450549,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bULong64',
+         'size': 8.678436279296875e-05,
+         'title': 'bULong64/l',
+         'type': 'ULong64_t',
+         'uncompressed_size': 0.000568389892578125},
+        {'compression_factor': 7.472972972972973,
+         'countname': None,
+         'isarray': '0',
+         'name': 'bBool',
+         'size': 7.05718994140625e-05,
+         'title': 'bBool/O',
+         'type': 'Bool_t',
+         'uncompressed_size': 0.0005273818969726562}]}
+
+##__________________________________________________________________||
---
+++ tests/unit/roottree/test_inspect_sample_04.py
@@ -0,0 +1,131 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import os
+import sys
+import pytest
+
+has_no_ROOT = False
+try:
+    import ROOT
+except ImportError:
+    has_no_ROOT = True
+
+if not has_no_ROOT:
+    from alphatwirl.roottree import inspect_tree
+
+##__________________________________________________________________||
+pytestmark = pytest.mark.skipif(has_no_ROOT, reason="has no ROOT")
+
+##__________________________________________________________________||
+@pytest.fixture()
+def tree():
+    input_file_name = 'sample_04.root'
+    tree_name = 'tree'
+    input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), input_file_name)
+
+    input_file = ROOT.TFile.Open(input_path)
+    yield input_file.Get(tree_name)
+
+
+def test_inspect(tree):
+    assert expected == inspect_tree(tree)
+
+expected = {
+    'leaves': [
+        {'compression_factor': 7.413333333333333,
+         'countname': None,
+         'isarray': '0',
+         'name': 'nvar',
+         'size': 7.152557373046875e-05,
+         'title': 'nvar/I',
+         'type': 'Int_t',
+         'uncompressed_size': 0.000530242919921875},
+        {'compression_factor': 7.5813953488372094,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bChar',
+         'size': 8.20159912109375e-05,
+         'title': 'bChar[nvar]/B',
+         'type': 'Char_t',
+         'uncompressed_size': 0.000621795654296875},
+        {'compression_factor': 7.551724137931035,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bUChar',
+         'size': 8.296966552734375e-05,
+         'title': 'bUChar[nvar]/b',
+         'type': 'UChar_t',
+         'uncompressed_size': 0.0006265640258789062},
+        {'compression_factor': 7.426966292134831,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bShort',
+         'size': 8.487701416015625e-05,
+         'title': 'bShort[nvar]/S',
+         'type': 'Short_t',
+         'uncompressed_size': 0.0006303787231445312},
+        {'compression_factor': 7.4,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bUShort',
+         'size': 8.58306884765625e-05,
+         'title': 'bUShort[nvar]/s',
+         'type': 'UShort_t',
+         'uncompressed_size': 0.0006351470947265625},
+        {'compression_factor': 7.164835164835165,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bInt',
+         'size': 8.678436279296875e-05,
+         'title': 'bInt[nvar]/I',
+         'type': 'Int_t',
+         'uncompressed_size': 0.000621795654296875},
+        {'compression_factor': 7.141304347826087,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bUInt',
+         'size': 8.7738037109375e-05,
+         'title': 'bUInt[nvar]/i',
+         'type': 'UInt_t',
+         'uncompressed_size': 0.0006265640258789062},
+        {'compression_factor': 7.193548387096774,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bFloat',
+         'size': 8.869171142578125e-05,
+         'title': 'bFloat[nvar]/F',
+         'type': 'Float_t',
+         'uncompressed_size': 0.0006380081176757812},
+        {'compression_factor': 6.764705882352941,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bDouble',
+         'size': 9.72747802734375e-05,
+         'title': 'bDouble[nvar]/D',
+         'type': 'Double_t',
+         'uncompressed_size': 0.0006580352783203125},
+        {'compression_factor': 6.764705882352941,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bLong64',
+         'size': 9.72747802734375e-05,
+         'title': 'bLong64[nvar]/L',
+         'type': 'Long64_t',
+         'uncompressed_size': 0.0006580352783203125},
+        {'compression_factor': 6.747572815533981,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bULong64',
+         'size': 9.822845458984375e-05,
+         'title': 'bULong64[nvar]/l',
+         'type': 'ULong64_t',
+         'uncompressed_size': 0.0006628036499023438},
+        {'compression_factor': 7.5813953488372094,
+         'countname': 'nvar',
+         'isarray': '1',
+         'name': 'bBool',
+         'size': 8.20159912109375e-05,
+         'title': 'bBool[nvar]/O',
+         'type': 'Bool_t',
+         'uncompressed_size': 0.000621795654296875}]}
+
+##__________________________________________________________________||
