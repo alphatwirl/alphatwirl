e9d94c6ad263535ac0cb1f427f81bd8e217bb2de
Tai Sakuma <tai.sakuma@gmail.com>
2018-05-10 09:29:52 +0100

    update test for HTCondorJobSubmitter

--- tests/unit/concurrently/test_HTCondorJobSubmitter.py
+++ tests/unit/concurrently/test_HTCondorJobSubmitter.py
@@ -32,12 +32,31 @@ queue resultdir in {resultdirs}
 """
 job_desc_template_with_extra = textwrap.dedent(job_desc_template_with_extra).strip()
 
+job_desc_expected = """
+Executable = run.py
+output = results/$(resultdir)/stdout.$(cluster).$(process).txt
+error = results/$(resultdir)/stderr.$(cluster).$(process).txt
+log = results/$(resultdir)/log.$(cluster).$(process).txt
+Arguments = $(resultdir).p.gz
+should_transfer_files = YES
+when_to_transfer_output = ON_EXIT
+transfer_input_files = $(resultdir).p.gz
+transfer_output_files = results
+Universe = vanilla
+notification = Error
+getenv = True
+queue resultdir in tpd_20161129_122841_HnpcmF
+""".strip()
+
 ##__________________________________________________________________||
 @pytest.fixture()
-def subprocess():
-    proc_submit = mock.MagicMock(name='proc_condor_submit')
-    proc_submit.communicate.return_value = (b'1 job(s) submitted to cluster 1012.', b'')
+def proc_submit():
+    ret =  mock.MagicMock(name='proc_condor_submit')
+    ret.communicate.return_value = (b'1 job(s) submitted to cluster 1012.', b'')
+    return ret
 
+@pytest.fixture()
+def subprocess(proc_submit):
     proc_prio = mock.MagicMock(name='proc_condor_prio')
     proc_prio.communicate.return_value = ('', '')
     proc_prio.returncode = 0
@@ -62,11 +81,12 @@ def test_init_job_desc_extra(obj):
     obj = HTCondorJobSubmitter(job_desc_extra=job_desc_extra)
     assert job_desc_template_with_extra == obj.job_desc_template
 
-def test_run(obj, tmpdir_factory, caplog):
+def test_run(obj, tmpdir_factory, proc_submit, caplog):
     workingarea = mock.MagicMock()
     workingarea.path = str(tmpdir_factory.mktemp(''))
-    workingarea.package_path.return_value = 'aaa'
+    workingarea.package_path.return_value = 'tpd_20161129_122841_HnpcmF'
     with caplog.at_level(logging.WARNING):
         assert '1012.0' == obj.run(workingArea=workingarea, package_index=0)
+    assert [mock.call(job_desc_expected)] == proc_submit.communicate.call_args_list
 
 ##__________________________________________________________________||
