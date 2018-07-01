bb27e12ba0db0876294d25e38ac4517930803b5b
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-01 18:04:07 +0100

    rewrite test for Count with pytest

--- tests/unit/summary/test_Count.py
+++ tests/unit/summary/test_Count.py
@@ -1,66 +1,59 @@
-import unittest
-
-import copy
+# Tai Sakuma <tai.sakuma@gmail.com>
 import numpy as np
+import copy
+
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
 
 from alphatwirl.summary import Count
 
 ##__________________________________________________________________||
-class TestCount(unittest.TestCase):
+@pytest.mark.parametrize('kwargs, expected_contents', (
+    (dict(), [np.array([0, 0])]),
+    (dict(val=()), [np.array([1, 1])]),
+    (dict(val=(), weight=10), [np.array([10, 100])]),
+    (dict(contents=[np.array([1, 3])]), [np.array([1, 3])]),
+))
+def test_init(kwargs, expected_contents):
+    obj = Count(**kwargs)
+    np.testing.assert_equal(expected_contents, obj.contents)
 
-    def test_init(self):
-        obj = Count()
-        np.testing.assert_equal([np.array([0, 0])], obj.contents)
+##__________________________________________________________________||
+def test_repr():
+    obj = Count()
+    repr(obj)
 
-    def test_init_val(self):
-        obj = Count(val = ())
-        np.testing.assert_equal([np.array([1, 1])], obj.contents)
+##__________________________________________________________________||
+def test_add():
+    obj1 = Count(contents=[np.array((10, 20))])
+    obj2 = Count(contents=[np.array((30, 40))])
+    obj3 =  obj1 + obj2
+    np.testing.assert_equal([np.array([40, 60])], obj3.contents)
+    assert obj1 is not obj3
+    assert obj1.contents is not obj3.contents
+    assert obj2 is not obj3
+    assert obj2.contents is not obj3.contents
 
-    def test_init_weight(self):
-        obj = Count(val = (), weight = 10)
-        np.testing.assert_equal([np.array([10, 100])], obj.contents)
+def test_radd():
+    obj1 = Count(contents=[np.array((10, 20))])
+    assert obj1 is not sum([obj1]) # will call 0 + obj1
+    assert obj1 == sum([obj1])
 
-    def test_init_contents(self):
-        obj = Count(contents = [np.array([1, 3])])
-        np.testing.assert_equal([np.array([1, 3])], obj.contents)
+def test_radd_raise():
+    obj1 = Count(contents=[np.array((10, 20))])
+    with pytest.raises(TypeError):
+        1 + obj1
 
-    @unittest.skip("they are the same object now")
-    def test_init_contents_not_same_object(self):
-        contents = [np.array([1, 3])]
-        obj = Count(contents = contents)
-        np.testing.assert_equal(contents, obj.contents)
-        self.assertIsNot(contents, obj.contents)
-        self.assertIsNot(contents[0], obj.contents[0])
-
-    def test_repr(self):
-        obj = Count()
-        repr(obj)
-
-    def test_add(self):
-        obj1 = Count(contents = [np.array((10, 20))])
-        obj2 = Count(contents = [np.array((30, 40))])
-        obj3 =  obj1 + obj2
-        np.testing.assert_equal([np.array([40, 60])], obj3.contents)
-        self.assertIsNot(obj1, obj3)
-        self.assertIsNot(obj1.contents, obj3.contents)
-        self.assertIsNot(obj2, obj3)
-        self.assertIsNot(obj2.contents, obj3.contents)
-
-    def test_radd(self):
-        obj1 = Count(contents = [np.array((10, 20))])
-        self.assertIsNot(obj1, sum([obj1])) # will call 0 + obj1
-        self.assertEqual(obj1, sum([obj1]))
-
-    def test_radd_raise(self):
-        obj1 = Count(contents = [np.array((10, 20))])
-        self.assertRaises(TypeError, obj1.__radd__, 1)
-
-    def test_copy(self):
-        obj1 = Count(contents = [np.array((10, 20))])
-        copy1 = copy.copy(obj1)
-        self.assertEqual(obj1, copy1)
-        self.assertIsNot(obj1, copy1)
-        self.assertIsNot(obj1.contents, copy1.contents)
-        self.assertIsNot(obj1.contents[0], copy1.contents[0])
+def test_copy():
+    obj1 = Count(contents=[np.array((10, 20))])
+    copy1 = copy.copy(obj1)
+    assert obj1 == copy1
+    assert obj1 is not copy1
+    assert obj1.contents is not copy1.contents
+    assert obj1.contents[0] is not copy1.contents[0]
 
 ##__________________________________________________________________||
