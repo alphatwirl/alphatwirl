51a57d1654c64ce1be514b9b39cc0ce609830fbf
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-06 17:06:19 +0100

    add __str__()

--- alphatwirl/loop/Collector.py
+++ alphatwirl/loop/Collector.py
@@ -22,14 +22,23 @@ class Collector(object):
         self.resultsCombinationMethod = resultsCombinationMethod
         self.deliveryMethod = deliveryMethod if deliveryMethod is not None else NullDeliveryMethod()
 
-    def __repr__(self):
-        name_value_pairs = (
+        self._repr_pairs = [
             ('resultsCombinationMethod', self.resultsCombinationMethod),
             ('deliveryMethod',           self.deliveryMethod),
-        )
+        ]
+
+    def __repr__(self):
         return '{}({})'.format(
             self.__class__.__name__,
-            ', '.join(['{} = {!r}'.format(n, v) for n, v in name_value_pairs]),
+            ', '.join(['{}={!r}'.format(n, v) for n, v in self._repr_pairs]),
+        )
+
+    def __str__(self):
+        nwidth = max(len(n) for n, _ in self._repr_pairs)
+        nwidth += 4
+        return '{}:\n{}'.format(
+            self.__class__.__name__,
+            '\n'.join(['{:>{}}: {!r}'.format(n, nwidth, v) for n, v in self._repr_pairs]),
         )
 
     def collect(self, dataset_readers_list):
