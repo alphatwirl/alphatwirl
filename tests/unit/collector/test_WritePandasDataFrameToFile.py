c76251706d0e3c90c7e58a57dadc03f8e6361d1d
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-26 15:06:10 +0200

    rewrite test_WritePandasDataFrameToFile with pytest

--- tests/unit/collector/test_WritePandasDataFrameToFile.py
+++ tests/unit/collector/test_WritePandasDataFrameToFile.py
@@ -1,14 +1,18 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 import io
 
 ##__________________________________________________________________||
-hasPandas = False
+has_no_pandas = False
 try:
-    import pandas
-    from alphatwirl.collector import WritePandasDataFrameToFile
-    hasPandas = True
+    import pandas as pd
 except ImportError:
-    pass
+    has_no_pandas = True
+
+pytestmark = pytest.mark.skipif(has_no_pandas, reason="has no pandas")
+
+if not has_no_pandas:
+    from alphatwirl.collector import WritePandasDataFrameToFile
 
 ##__________________________________________________________________||
 class MockOpen(object):
@@ -18,65 +22,56 @@ class MockOpen(object):
     def __call__(self, path):
         return self._out
 
-##__________________________________________________________________||
-def mockClose(file): pass
+def mockClose(file):
+    pass
 
 ##__________________________________________________________________||
-@unittest.skipUnless(hasPandas, "has no pandas")
-class TestWritePandasDataFrameToFile(unittest.TestCase):
+@pytest.fixture()
+def out():
+    return io.BytesIO()
 
-    @unittest.skip('fail with Pandas 0.20 because of its bug about the header indent')
-    def test_deliver(self):
-        delivery = WritePandasDataFrameToFile("tbl.txt")
+@pytest.fixture()
+def obj(out):
+    ret = WritePandasDataFrameToFile("tbl.txt")
+    ret._open = MockOpen(out)
+    ret._close = mockClose
+    yield ret
 
-        out = io.BytesIO()
-        delivery._open = MockOpen(out)
-        delivery._close = mockClose
+def test_repr(obj):
+    repr(obj)
 
-        results = pandas.DataFrame(
-            {
-                'v1': [1, 2, 3],
-                'n': [4.0, 3.0, 2.0],
-                'nvar': [6.0, 9.0, 3.0],
-            },
-            columns = ['v1', 'n', 'nvar']
-            )
+@pytest.mark.xfail(reason='fail with Pandas 0.20 because of its bug about the header indent')
+def test_deliver(obj, out):
+    results = pd.DataFrame(
+        {
+            'v1': [1, 2, 3],
+            'n': [4.0, 3.0, 2.0],
+            'nvar': [6.0, 9.0, 3.0],
+        },
+        columns = ['v1', 'n', 'nvar']
+    )
 
-        delivery.deliver(results)
+    obj.deliver(results)
 
-        expected = " v1  n  nvar\n  1  4     6\n  2  3     9\n  3  2     3\n".encode()
-        self.assertEqual(expected, out.getvalue())
+    expected = " v1  n  nvar\n  1  4     6\n  2  3     9\n  3  2     3\n".encode()
+    assert expected == out.getvalue()
 
-    def test_deliver_empty_dataframe(self):
+def test_deliver_empty_dataframe(obj, out):
+    results = pd.DataFrame(
+        {
+            'v1': [ ],
+            'n': [ ],
+            'nvar': [ ],
+        },
+        columns = ['v1', 'n', 'nvar']
+        )
 
-        delivery = WritePandasDataFrameToFile("tbl.txt")
+    obj.deliver(results)
 
-        out = io.BytesIO()
-        delivery._open = MockOpen(out)
-        delivery._close = mockClose
+    expected = "v1 n nvar\n".encode()
+    assert expected == out.getvalue()
 
-        results = pandas.DataFrame(
-            {
-                'v1': [ ],
-                'n': [ ],
-                'nvar': [ ],
-            },
-            columns = ['v1', 'n', 'nvar']
-            )
-
-        delivery.deliver(results)
-
-        expected = "v1 n nvar\n".encode()
-        self.assertEqual(expected, out.getvalue())
-
-    def test_deliver_None_results(self):
-
-        delivery = WritePandasDataFrameToFile("tbl.txt")
-
-        out = io.BytesIO()
-        delivery._open = MockOpen(out)
-        delivery._close = mockClose
-
-        delivery.deliver(None)
+def test_deliver_None_results(obj, out):
+    obj.deliver(None)
 
 ##__________________________________________________________________||
