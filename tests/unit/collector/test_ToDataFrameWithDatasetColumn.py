9a7cb503c75d31716f40d880b64acccaaf32b5a8
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-26 14:30:39 +0200

    rewrite test_ToDataFrameWithDatasetColumn with pytest

--- tests/unit/collector/test_ToDataFrameWithDatasetColumn.py
+++ tests/unit/collector/test_ToDataFrameWithDatasetColumn.py
@@ -1,116 +1,109 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 
 ##__________________________________________________________________||
-hasPandas = False
+has_no_pandas = False
 try:
     import pandas as pd
-    hasPandas = True
 except ImportError:
-    pass
+    has_no_pandas = True
 
-if hasPandas:
-    from alphatwirl.collector import ToDataFrameWithDatasetColumn
+pytestmark = pytest.mark.skipif(has_no_pandas, reason="has no pandas")
 
 from .mock import MockReader, MockSummarizer
 
-##__________________________________________________________________||
-def assertDataFrameEqual(df1, df2, **kwds):
+if not has_no_pandas:
     from pandas.util.testing import assert_frame_equal
-    return assert_frame_equal(df1.sort_index(axis = 1), df2.sort_index(axis = 1), check_names = True)
+    from alphatwirl.collector import ToDataFrameWithDatasetColumn
 
 ##__________________________________________________________________||
-@unittest.skipUnless(hasPandas, "has no pandas")
-class TestToDataFrameWithDatasetColumn(unittest.TestCase):
-    def setUp(self):
-        self.addTypeEqualityFunc(pd.core.frame.DataFrame, assertDataFrameEqual)
+@pytest.fixture()
+def obj():
+    return ToDataFrameWithDatasetColumn(
+        summaryColumnNames=('htbin', 'njetbin', 'n', 'nvar'),
+        datasetColumnName = 'dataset'
+    )
 
-        self.obj = ToDataFrameWithDatasetColumn(
-            summaryColumnNames = ('htbin', 'njetbin', 'n', 'nvar'),
-            datasetColumnName = 'dataset'
-        )
+def test_repr(obj):
+    repr(obj)
 
-    def test_repr(self):
-        repr(self.obj)
+def test_example(obj):
+    reader1 = MockReader(
+        MockSummarizer(
+            [
+                (200, 2, 120, 240),
+            ]))
 
-    def test_example(self):
+    reader2 = MockReader(
+        MockSummarizer(
+            [
+                (300, 2, 490, 980),
+                (300, 3, 210, 420),
+            ]))
 
-        reader1 = MockReader(
-            MockSummarizer(
-                [
-                    (200, 2, 120, 240),
-                ]))
+    reader3 = MockReader(
+        MockSummarizer(
+            [
+                (300, 2, 20, 40),
+                (300, 3, 15, 30),
+            ]))
 
-        reader2 = MockReader(
-            MockSummarizer(
-                [
-                    (300, 2, 490, 980),
-                    (300, 3, 210, 420),
-                ]))
+    reader4 = MockReader(MockSummarizer([]))
 
-        reader3 = MockReader(
-            MockSummarizer(
-                [
-                    (300, 2, 20, 40),
-                    (300, 3, 15, 30),
-                ]))
+    dataset_readers_list = [
+        ('QCD', (reader1, reader2)),
+        ('TTJets', (reader3, )),
+        ('WJets', (reader4, )),
+        ('ZJets', ( )),
+    ]
 
-        reader4 = MockReader(MockSummarizer([]))
+    expected = pd.DataFrame(
+        {
+            'dataset': ['QCD', 'QCD', 'QCD', 'TTJets', 'TTJets'],
+            'htbin': [200, 300, 300, 300, 300],
+            'njetbin': [2, 2, 3, 2, 3],
+            'n': [120, 490, 210, 20, 15],
+            'nvar': [240, 980, 420, 40, 30],
+        })
 
-        dataset_readers_list = [
-            ('QCD', (reader1, reader2)),
-            ('TTJets', (reader3, )),
-            ('WJets', (reader4, )),
-            ('ZJets', ( )),
-        ]
+    actual = obj.combine(dataset_readers_list)
+    assert_frame_equal(expected.sort_index(axis = 1), actual.sort_index(axis = 1), check_names=True)
 
-        expected = pd.DataFrame(
-            {
-                'dataset': ['QCD', 'QCD', 'QCD', 'TTJets', 'TTJets'],
-                'htbin': [200, 300, 300, 300, 300],
-                'njetbin': [2, 2, 3, 2, 3],
-                'n': [120, 490, 210, 20, 15],
-                'nvar': [240, 980, 420, 40, 30],
-            })
+def test_combine_all_empty_counts(obj):
+    reader1 = MockReader(MockSummarizer([]))
+    reader2 = MockReader(MockSummarizer([]))
 
-        actual = self.obj.combine(dataset_readers_list)
-        self.assertEqual(expected, actual)
+    dataset_readers_list = [
+        ('data1', (reader1, )),
+        ('data2', (reader2, )),
+    ]
 
-    def test_combine_all_empty_counts(self):
+    # # this will make the index Index([], dtype='object')
+    # expected = pd.DataFrame(
+    #     {
+    #         'dataset': [ ],
+    #         'htbin': [ ],
+    #         'njetbin': [ ],
+    #         'n': [ ],
+    #         'nvar': [ ],
+    #         }
+    #     )
 
-        reader1 = MockReader(MockSummarizer([]))
-        reader2 = MockReader(MockSummarizer([]))
+    # this will make the index Int64Index([], dtype='int64')
+    expected = pd.DataFrame(
+         [],
+         columns = ('dataset', 'htbin', 'njetbin', 'n', 'nvar')
+    )
 
-        dataset_readers_list = [
-            ('data1', (reader1, )),
-            ('data2', (reader2, )),
-        ]
+    actual = obj.combine(dataset_readers_list)
+    assert_frame_equal(expected.sort_index(axis = 1), actual.sort_index(axis = 1), check_names=True)
 
-        # # this will make the index Index([], dtype='object')
-        # expected = pd.DataFrame(
-        #     {
-        #         'dataset': [ ],
-        #         'htbin': [ ],
-        #         'njetbin': [ ],
-        #         'n': [ ],
-        #         'nvar': [ ],
-        #         }
-        #     )
+def test_combine_empty_pairs(obj):
 
-        # this will make the index Int64Index([], dtype='int64')
-        expected = pd.DataFrame(
-             [],
-             columns = ('dataset', 'htbin', 'njetbin', 'n', 'nvar')
-        )
+    dataset_readers_list = [
+    ]
 
-        actual = self.obj.combine(dataset_readers_list)
-        self.assertEqual(expected, actual)
-
-    def test_combine_empty_pairs(self):
-
-        dataset_readers_list = [
-        ]
-
-        actual = self.obj.combine(dataset_readers_list)
-        self.assertEqual(None, actual)
+    actual = obj.combine(dataset_readers_list)
+    assert actual is None
 
 ##__________________________________________________________________||
