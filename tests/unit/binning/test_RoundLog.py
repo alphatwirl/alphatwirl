bcbbd5f0b8462b266c3f0c7179455cc900974ce7
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-31 13:49:16 +0100

    rewrite test_RoundLog.py with pytest

--- tests/unit/binning/test_RoundLog.py
+++ tests/unit/binning/test_RoundLog.py
@@ -1,80 +1,81 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 
 from alphatwirl.binning import RoundLog
 
 ##__________________________________________________________________||
-class TestRoundLog(unittest.TestCase):
+def test_repr():
+    obj = RoundLog()
+    repr(obj)
 
-    def test_init(self):
-        obj = RoundLog()
+def test_call():
+    obj = RoundLog()
+    assert 1.9952623149688 == pytest.approx(obj(   2))
+    assert 19.952623149688 == pytest.approx(obj(  20))
+    assert 199.52623149688 == pytest.approx(obj( 200))
 
-    def test_call(self):
-        obj = RoundLog()
-        self.assertAlmostEqual( 1.9952623149688, obj(   2))
-        self.assertAlmostEqual( 19.952623149688, obj(  20))
-        self.assertAlmostEqual( 199.52623149688, obj( 200))
+def test_next():
+    obj = RoundLog()
+    assert 2.51188643150958 == pytest.approx(obj.next(2.23872113856834))
+    assert 25.11886431509581 == pytest.approx(obj.next(22.3872113856834))
+    assert 251.18864315095848 == pytest.approx(obj.next(223.872113856834))
 
-    def test_next(self):
-        obj = RoundLog()
-        self.assertAlmostEqual( 2.51188643150958, obj.next(2.23872113856834))
-        self.assertAlmostEqual( 25.11886431509581, obj.next(22.3872113856834))
-        self.assertAlmostEqual( 251.18864315095848, obj.next(223.872113856834))
+def test_call_zero():
+    obj = RoundLog()
+    assert 0 == obj(0)
 
-    def test_call_zero(self):
-        obj = RoundLog()
-        self.assertEqual(0, obj(0))
+    assert 0 == obj.next(0) # next to 0 is 0 unless 0 is the
+                            # underflow bin
 
-        self.assertEqual(0, obj.next(0)) # next to 0 is 0 unless 0 is the
-                                         # underflow bin
+def test_call_negative():
+    obj = RoundLog()
+    assert obj(-1) is None
 
-    def test_call_negative(self):
-        obj = RoundLog()
-        self.assertIsNone(obj(-1))
+def test_valid():
+    obj = RoundLog(valid=lambda x: x >= 10)
+    assert 12.589254117941675 == pytest.approx(obj(13))
+    assert 10.0 == pytest.approx(obj(10))
+    assert obj(7) is None
 
-    def test_valid(self):
-        obj = RoundLog(valid = lambda x: x >= 10)
-        self.assertAlmostEqual( 12.589254117941675, obj(13))
-        self.assertAlmostEqual( 10.0, obj(10))
-        self.assertIsNone(obj(7))
+def test_onBoundary():
+    obj = RoundLog(0.1, 100)
+    assert 100 == obj(100)
+#
+def test_min():
+    obj = RoundLog(0.1, 100, min=10)
+    assert 100 == obj(100)
+    assert 10 == pytest.approx(obj(  10))
+    assert obj(9) is None
 
-    def test_onBoundary(self):
-        obj = RoundLog(0.1, 100)
-        self.assertEqual( 100, obj( 100))
+def test_min_underflow_bin():
+    obj = RoundLog(0.1, 100, min=10, underflow_bin=0)
+    assert 100 == obj(100)
+    assert 10 == pytest.approx(obj(10))
+    assert 0 == obj(   9)
 
-    def test_min(self):
-        obj = RoundLog(0.1, 100, min = 10)
-        self.assertEqual(  100, obj( 100))
-        self.assertAlmostEqual(   10, obj(  10))
-        self.assertEqual( None, obj(   9))
+    assert obj(10) == obj.next(0) # the next to the underflow
+                                   # bin is the bin for the min
 
-    def test_min_underflow_bin(self):
-        obj = RoundLog(0.1, 100, min = 10, underflow_bin = 0)
-        self.assertEqual(  100, obj( 100))
-        self.assertAlmostEqual(   10, obj(  10))
-        self.assertEqual( 0, obj(   9))
+def test_max():
+    obj = RoundLog(0.1, 100, max=1000)
+    assert 100 == obj(100)
+    assert obj(1000) is None
+    assert obj(5000) is None
 
-        self.assertEqual( obj(10), obj.next( 0)) # the next to the underflow
-                                                 # bin is the bin for the min
+def test_max_overflow_bin():
+    obj = RoundLog(0.1, 100, max=1000, overflow_bin=1000)
+    assert 100 == obj(100)
+    assert 1000 == obj(1000)
+    assert 1000 == obj(5000)
 
-    def test_max(self):
-        obj = RoundLog(0.1, 100, max = 1000)
-        self.assertEqual(  100, obj(  100))
-        self.assertEqual( None, obj( 1000))
-        self.assertEqual( None, obj( 5000))
+    assert 1000 == obj.next(1000) # the next to the overflow bin
+                                  # is the overflow bin
 
-    def test_max_overflow_bin(self):
-        obj = RoundLog(0.1, 100, max = 1000, overflow_bin = 1000)
-        self.assertEqual(  100, obj(  100))
-        self.assertEqual( 1000, obj( 1000))
-        self.assertEqual( 1000, obj( 5000))
+def test_inf():
+    obj = RoundLog(0.1, 100)
+    assert obj(float('inf')) is None
+    assert obj(float('-inf')) is None
+    assert obj.next(float('inf')) is None
+    assert obj.next(float('-inf')) is None
 
-        self.assertEqual( 1000, obj.next(1000)) # the next to the overflow bin
-                                                # is the overflow bin
-
-    def test_inf(self):
-        obj = RoundLog(0.1, 100)
-        self.assertIsNone(obj(float('inf')))
-        self.assertIsNone(obj(float('-inf')))
-        self.assertIsNone(obj.next(float('inf')))
-        self.assertIsNone(obj.next(float('-inf')))
 ##__________________________________________________________________||
