3c7d02a63af562ae713787ca5d955812e6936d5f
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-04 10:24:46 +0100

    update tests for ProgressPrint

--- tests/unit/progressbar/test_ProgressPrint.py
+++ tests/unit/progressbar/test_ProgressPrint.py
@@ -1,6 +1,5 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
-import sys
-
+import time
 import pytest
 
 try:
@@ -12,33 +11,26 @@ from alphatwirl import progressbar
 
 ##__________________________________________________________________||
 @pytest.fixture()
-def mocsys(monkeypatch):
-    ret = mock.Mock()
-    module = sys.modules['alphatwirl.progressbar.ProgressPrint']
-    monkeypatch.setattr(module, 'sys', ret)
-    return ret
-
-@pytest.fixture()
 def moctime(monkeypatch):
-    ret = mock.MagicMock()
-    module = sys.modules['alphatwirl.progressbar.ProgressPrint']
-    monkeypatch.setattr(module, 'time', ret)
+    ret = mock.Mock()
+    monkeypatch.setattr(time, 'time', ret)
+    ret.return_value = 1533374055.904203
     return ret
 
 @pytest.fixture()
-def obj():
+def obj(moctime):
     return progressbar.ProgressPrint()
 
 ##__________________________________________________________________||
 def test_repr(obj):
     repr(obj)
 
-def test_report(obj, mocsys, moctime):
+def test_report(obj, capsys):
     report = progressbar.ProgressReport('task1', 0, 10)
-    # obj.present(report)
-    # print 
-    # print mocsys.stdout.write.call_args_list
-    # print mocsys.method_calls
-    # print mocsys.mock_calls
+    obj.present(report)
+    captured = capsys.readouterr()
+    stdout_lines = captured.out.strip().split('\n')
+    assert 2 == len(stdout_lines)
+    assert '        0 /       10 (  0.00%) task1' == stdout_lines[1]
 
 ##__________________________________________________________________||
