17333c97ce8e2e4881a8293c8871c09845702792
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-05 13:56:07 +0100

    add binary_search()

--- tests/unit/binning/test_search.py
+++ tests/unit/binning/test_search.py
@@ -1,9 +1,10 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
 import pytest
 
-from alphatwirl.binning.search import linear_search
+from alphatwirl.binning.search import linear_search, binary_search
 
 ##__________________________________________________________________||
+@pytest.mark.parametrize('search', [linear_search, binary_search])
 @pytest.mark.parametrize('val, boundaries, expected', [
     pytest.param(0.1, [0.1], 0, id='one-boundary'),
     pytest.param(0.1, [0.1, 0.2], 0, id='two-boundaries-at-lowedge'),
@@ -15,6 +16,7 @@ from alphatwirl.binning.search import linear_search
     pytest.param(0.25, [0.1, 0.2, 0.3], 1, id='three-boundaries'),
     pytest.param(0.3, [0.1, 0.2, 0.3], 2, id='three-boundaries'),
 ])
-def test_search(val, boundaries, expected):
-    assert expected == linear_search(val, boundaries)
+def test_search(search, val, boundaries, expected):
+    assert expected == search(val, boundaries)
+
 ##__________________________________________________________________||
