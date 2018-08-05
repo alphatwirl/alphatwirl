17d92967df566634243648c1b7df6bdf74e75778
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-05 17:55:28 +0100

    add four-boundaries test

--- tests/unit/binning/test_search.py
+++ tests/unit/binning/test_search.py
@@ -15,6 +15,13 @@ from alphatwirl.binning.search import linear_search, binary_search
     pytest.param(0.2, [0.1, 0.2, 0.3], 1, id='three-boundaries'),
     pytest.param(0.25, [0.1, 0.2, 0.3], 1, id='three-boundaries'),
     pytest.param(0.3, [0.1, 0.2, 0.3], 2, id='three-boundaries'),
+    pytest.param(0.1, [0.1, 0.2, 0.3, 0.4], 0, id='four-boundaries'),
+    pytest.param(0.15, [0.1, 0.2, 0.3, 0.4], 0, id='four-boundaries'),
+    pytest.param(0.2, [0.1, 0.2, 0.3, 0.4], 1, id='four-boundaries'),
+    pytest.param(0.25, [0.1, 0.2, 0.3, 0.4], 1, id='four-boundaries'),
+    pytest.param(0.3, [0.1, 0.2, 0.3, 0.4], 2, id='four-boundaries'),
+    pytest.param(0.35, [0.1, 0.2, 0.3, 0.4], 2, id='four-boundaries'),
+    pytest.param(0.4, [0.1, 0.2, 0.3, 0.4], 3, id='four-boundaries'),
 ])
 def test_search(search, val, boundaries, expected):
     assert expected == search(val, boundaries)
