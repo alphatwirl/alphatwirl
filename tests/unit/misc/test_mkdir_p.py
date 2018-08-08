bc6c81cf26792e3fba4c77e7348ec21a2f67b8ac
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-08 15:07:10 +0100

    update test_mkdir_p

--- tests/unit/misc/test_mkdir_p.py
+++ tests/unit/misc/test_mkdir_p.py
@@ -30,11 +30,11 @@ def test_emtpy(mock_makedirs):
     mkdir_p('')
     assert [ ] == mock_makedirs.call_args_list
 
-def test_one(mock_makedirs):
+def test_success(mock_makedirs):
     mkdir_p('a/b')
     assert [mock.call('a/b')] == mock_makedirs.call_args_list
 
-def test_raise(mock_makedirs, mock_isdir, caplog):
+def test_already_exist(mock_makedirs, mock_isdir, caplog):
     mock_isdir.return_value = True
     mock_makedirs.side_effect = OSError(errno.EEXIST, 'already exist')
     with caplog.at_level(logging.DEBUG - 1):
@@ -44,5 +44,13 @@ def test_raise(mock_makedirs, mock_isdir, caplog):
     assert len(caplog.records) == 1
     assert caplog.records[0].levelno == logging.DEBUG - 1
     assert 'tried' in caplog.records[0].msg
+
+def test_raise(mock_makedirs, mock_isdir, caplog):
+    mock_isdir.return_value = False
+    mock_makedirs.side_effect = OSError
+    with pytest.raises(OSError):
+        mkdir_p('a/b')
+
+    assert [mock.call('a/b')] == mock_makedirs.call_args_list
 
 ##__________________________________________________________________||
