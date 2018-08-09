c06f6bbcdb47ecc9e3362dc6864bc457bd5742b7
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-09 13:46:05 +0100

    add decorator _removed()

---
+++ alphatwirl/misc/removal.py
@@ -0,0 +1,49 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+from __future__ import print_function
+import inspect
+import functools
+import logging
+
+##__________________________________________________________________||
+def _removed(msg=''):
+    def _decorate_class(c, msg):
+        module_name = c.__module__
+        logger = logging.getLogger(module_name)
+        class_name = c.__name__
+        name = '{}.{}'.format(module_name, class_name)
+        text = '{} is removed.'.format(name)
+        if msg:
+            text += ' ' + msg
+
+            original_init = c.__init__
+
+        def init(*args, **kwargs):
+            logger.error(text)
+            raise RuntimeError(msg)
+            return original_init(*args, **kwargs)
+
+        c.__init__ = init
+        return c
+
+    def _decorate_func(f, msg):
+        module_name = f.__module__
+        logger = logging.getLogger(module_name)
+        name = f.__name__
+        name = '{}.{}'.format(module_name, name)
+        text = '{}() is removed.'.format(name)
+        if msg:
+            text += ' ' + msg
+
+        @functools.wraps(f)
+        def g(*args, **kwargs):
+            logger.error(text)
+            raise RuntimeError(msg)
+        return g
+
+    def _imp(f):
+        if inspect.isclass(f):
+            return _decorate_class(f, msg)
+        return _decorate_func(f, msg)
+    return _imp
+
+##__________________________________________________________________||
---
+++ tests/unit/misc/test_removal.py
@@ -0,0 +1,70 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+from __future__ import print_function
+import logging
+
+import pytest
+
+try:
+   import cPickle as pickle
+except:
+   import pickle
+
+from alphatwirl.misc.removal import _removed
+
+##__________________________________________________________________||
+@_removed(msg='extra message')
+def func():
+    pass
+
+def test_func_logging(caplog):
+    with pytest.raises(RuntimeError):
+       with caplog.at_level(logging.ERROR):
+          func()
+
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == 'ERROR'
+    assert 'test_removal' in caplog.records[0].name
+    assert 'func() is removed. extra message' in caplog.records[0].msg
+
+def test_func_name():
+    assert  'func' == func.__name__
+
+def test_func_pickle():
+    pickle.dumps(func)
+
+@_removed()
+def func_without_msg():
+    pass
+
+def test_func_logging_without_msg(caplog):
+    with pytest.raises(RuntimeError):
+       with caplog.at_level(logging.ERROR):
+          func_without_msg()
+
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == 'ERROR'
+    assert 'test_removal' in caplog.records[0].name
+    assert 'func_without_msg() is removed.' in caplog.records[0].msg
+
+##__________________________________________________________________||
+@_removed(msg='extra message')
+class ClassWithInit(object):
+    def __init__(self):
+        pass
+
+@_removed(msg='extra message')
+class ClassWithoutInit(object):
+    pass
+
+@pytest.mark.parametrize('Class', (ClassWithInit, ClassWithoutInit))
+def test_class_logging(Class, caplog):
+    with pytest.raises(RuntimeError):
+       with caplog.at_level(logging.ERROR):
+          c = Class()
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == 'ERROR'
+    assert 'test_removal' in caplog.records[0].name
+    expected = '{} is removed. extra message'.format(Class.__name__)
+    assert expected in caplog.records[0].msg
+
+##__________________________________________________________________||
