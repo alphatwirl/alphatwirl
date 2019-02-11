a626353c31f5a19227b4e2983a87b641fbcbe2d8
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-11 19:04:32 +0000

    add comments (date and version) on removed classes

--- alphatwirl/roottree/BEventBuilder.py
+++ alphatwirl/roottree/BEventBuilder.py
@@ -2,6 +2,7 @@
 from alphatwirl.misc.deprecation import _removed
 
 ##__________________________________________________________________||
+# removed after v0.21.0 (2019-02-11)
 @_removed(msg='use BuildEvents instead')
 class BEventBuilder(object):
     pass
