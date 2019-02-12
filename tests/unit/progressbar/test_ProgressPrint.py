3cc387e1340e3b29b95de12cc0b57ee747273951
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-12 08:51:50 +0000

    clean up code in test_ProgressPrint.py

--- tests/unit/progressbar/test_ProgressPrint.py
+++ tests/unit/progressbar/test_ProgressPrint.py
@@ -7,26 +7,26 @@ try:
 except ImportError:
     import mock
 
-from alphatwirl import progressbar
+from alphatwirl.progressbar import ProgressPrint, ProgressReport
 
 ##__________________________________________________________________||
 @pytest.fixture()
-def moctime(monkeypatch):
+def mock_time(monkeypatch):
     ret = mock.Mock()
     monkeypatch.setattr(time, 'time', ret)
     ret.return_value = 1533374055.904203
     return ret
 
 @pytest.fixture()
-def obj(moctime):
-    return progressbar.ProgressPrint()
+def obj(mock_time):
+    return ProgressPrint()
 
 ##__________________________________________________________________||
 def test_repr(obj):
     repr(obj)
 
 def test_report(obj, capsys):
-    report = progressbar.ProgressReport('task1', 0, 10)
+    report = ProgressReport('task1', 0, 10)
     obj.present(report)
     captured = capsys.readouterr()
     stdout_lines = captured.out.strip().split('\n')
