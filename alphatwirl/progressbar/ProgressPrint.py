3c12ef3fb64cfaa1a3fc3c9b7cd8107e35e12d19
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-12 12:34:43 +0000

    clean up code in  ProgressPrint

--- alphatwirl/progressbar/ProgressPrint.py
+++ alphatwirl/progressbar/ProgressPrint.py
@@ -5,11 +5,18 @@ import sys, collections
 ##__________________________________________________________________||
 class ProgressPrint(object):
     def __init__(self):
-        self.reports = collections.OrderedDict()
+
+        self._new_taskids = [ ]
+        self._active_taskids = [ ] # in order of arrival
+        self._finishing_taskids = [ ]
+        self._complete_taskids = [ ] # in order of completion
+        self._report_dict = { }
+
         self.lines = [ ]
-        self.interval = 60.0 # [second]
+        ## self.interval = 60.0 # [second]
         self.last = [ ]
 
+        self.interval = 1.0 # [second]
         self._read_time()
 
     def __repr__(self):
@@ -18,27 +25,92 @@ class ProgressPrint(object):
         )
 
     def nreports(self):
-        return len(self.reports)
+        return len(self._active_taskids)
 
     def present(self, report):
-        self.reports[report.taskid] = report
-        if not self._need_to_update(report): return
-        self._create_lines()
-        self._print_lines()
+
+        if not self._register_report(report):
+            return
+
+        if not self._need_to_present(report):
+            return
+
+        self._present()
+
+        self._update_registry()
+
         self._read_time()
 
+    def _register_report(self, report):
+
+        if report.taskid in self._complete_taskids:
+            return False
+
+        self._report_dict[report.taskid] = report
+
+        if report.taskid in self._finishing_taskids:
+            return True
+
+        if report.last():
+            try:
+                self._active_taskids.remove(report.taskid)
+            except ValueError:
+                pass
+
+            try:
+                self._new_taskids.remove(report.taskid)
+            except ValueError:
+                pass
+
+            self._finishing_taskids.append(report.taskid)
+
+            return True
+
+        if report.taskid in self._active_taskids:
+            return True
+
+        if report.taskid in self._new_taskids:
+            return True
+
+        self._new_taskids.append(report.taskid)
+        return True
+
+    def _update_registry(self):
+        self._active_taskids.extend(self._new_taskids)
+        del self._new_taskids[:]
+
+        self._complete_taskids.extend(self._finishing_taskids)
+        del self._finishing_taskids[:]
+
+    def _need_to_present(self, report):
+
+        if self._new_taskids:
+            return True
+
+        if self._finishing_taskids:
+            return True
+
+        if self._time() - self.last_time > self.interval:
+            return True
+
+        return False
+
+    def _present(self):
+        self._create_lines()
+        self._print_lines()
+
     def _create_lines(self):
+
         self.lines = [ ]
-        taskids_to_delete = [ ]
-        for taskid, report in self.reports.items():
-            line = self.createLine(report)
-            if report.done >= report.total:
-                taskids_to_delete.append(report.taskid)
-                self.last.append(line)
-            else:
-                self.lines.append(line)
-        for taskid in taskids_to_delete:
-            del self.reports[taskid]
+        for taskid in self._active_taskids + self._new_taskids:
+            report = self._report_dict[taskid]
+            line = self._create_line(report)
+            self.lines.append(line)
+
+        for taskid in self._finishing_taskids:
+            report = self._report_dict[taskid]
+            line = self._create_line(report)
+            self.last.append(line)
 
     def _print_lines(self):
         sys.stdout.write("\n")
@@ -48,20 +120,10 @@ class ProgressPrint(object):
         sys.stdout.write("\n".join(self.lines) + "\n")
         sys.stdout.flush()
 
-    def createLine(self, report):
+    def _create_line(self, report):
         percent = float(report.done)/report.total if report.total > 0 else 1
-        bar = (':' * int(percent * 40)).ljust(40, " ")
         percent = round(percent * 100, 2)
         return " {1:8d} / {2:8d} ({0:6.2f}%) {3} ".format(percent, report.done, report.total, report.name)
-
-    def _need_to_update(self, report):
-        if self._time() - self.last_time > self.interval:
-            return True
-        if report.done == report.total:
-            return True
-        if report.done == 0:
-            return True
-        return False
 
     def _time(self):
         return time.time()
--- tests/unit/progressbar/test_ProgressPrint.py
+++ tests/unit/progressbar/test_ProgressPrint.py
@@ -34,3 +34,174 @@ def test_report(obj, capsys):
     assert '        0 /       10 (  0.00%) task1' == stdout_lines[1]
 
 ##__________________________________________________________________||
+params = [
+
+    ##
+    pytest.param(
+        ProgressReport('task1', 0, 10, 1),
+        [ ], [ ], [ ], [ ],
+        [1], [ ], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 0, 10, 1),
+        [1], [ ], [ ], [ ],
+        [1], [ ], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 0, 10, 1),
+        [ ], [1], [ ], [ ],
+        [ ], [1], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 0, 10, 1),
+        [ ], [ ], [1], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 0, 10, 1),
+        [ ], [ ], [ ], [1],
+        [ ], [ ], [ ], [1],
+        False
+    ),
+
+    ##
+    pytest.param(
+        ProgressReport('task1', 2, 10, 1),
+        [ ], [ ], [ ], [ ],
+        [1], [ ], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 2, 10, 1),
+        [1], [ ], [ ], [ ],
+        [1], [ ], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 2, 10, 1),
+        [ ], [1], [ ], [ ],
+        [ ], [1], [ ], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 2, 10, 1),
+        [ ], [ ], [1], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 2, 10, 1),
+        [ ], [ ], [ ], [1],
+        [ ], [ ], [ ], [1],
+        False
+    ),
+
+    ##
+    pytest.param(
+        ProgressReport('task1', 10, 10, 1),
+        [ ], [ ], [ ], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 10, 10, 1),
+        [1], [ ], [ ], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 10, 10, 1),
+        [ ], [1], [ ], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 10, 10, 1),
+        [ ], [ ], [1], [ ],
+        [ ], [ ], [1], [ ],
+        True
+    ),
+    pytest.param(
+        ProgressReport('task1', 10, 10, 1),
+        [ ], [ ], [ ], [1],
+        [ ], [ ], [ ], [1],
+        False
+    ),
+]
+param_names = (
+    'report, '
+    'initial_new_taskids, initial_active_taskids, '
+    'initial_finishing_taskids, initial_complete_taskids, '
+    'expected_new_taskids, expected_active_taskids, '
+    'expected_finishing_taskids, expected_complete_taskids, '
+    'expected_return'
+)
+
+@pytest.mark.parametrize(param_names, params)
+def test_register_report(
+        obj, report,
+        initial_new_taskids, initial_active_taskids,
+        initial_finishing_taskids, initial_complete_taskids,
+        expected_new_taskids, expected_active_taskids,
+        expected_finishing_taskids, expected_complete_taskids,
+        expected_return):
+
+    obj._new_taskids[:] = initial_new_taskids
+    obj._active_taskids[:] = initial_active_taskids
+    obj._finishing_taskids[:] = initial_finishing_taskids
+    obj._complete_taskids[:] = initial_complete_taskids
+
+    assert expected_return == obj._register_report(report)
+
+    assert expected_new_taskids == obj._new_taskids
+    assert expected_active_taskids == obj._active_taskids
+    assert expected_finishing_taskids == obj._finishing_taskids
+    assert expected_complete_taskids == obj._complete_taskids
+
+##__________________________________________________________________||
+params = [
+    pytest.param(
+        [ ], [ ], [ ], [ ],
+        [ ], [ ], [ ], [ ],
+    ),
+    pytest.param(
+        [ ], [10, 11, 12], [ ], [30, 31, 32],
+        [ ], [10, 11, 12], [ ], [30, 31, 32],
+    ),
+    pytest.param(
+        [1, 2], [10, 11, 12], [20, 21], [30, 31, 32],
+        [ ], [10, 11, 12, 1, 2], [ ], [30, 31, 32, 20, 21],
+    ),
+]
+param_names = (
+    'initial_new_taskids, initial_active_taskids, '
+    'initial_finishing_taskids, initial_complete_taskids, '
+    'expected_new_taskids, expected_active_taskids, '
+    'expected_finishing_taskids, expected_complete_taskids, '
+)
+
+@pytest.mark.parametrize(param_names, params)
+def test_update_registry(
+        obj,
+        initial_new_taskids, initial_active_taskids,
+        initial_finishing_taskids, initial_complete_taskids,
+        expected_new_taskids, expected_active_taskids,
+        expected_finishing_taskids, expected_complete_taskids):
+
+    obj._new_taskids[:] = initial_new_taskids
+    obj._active_taskids[:] = initial_active_taskids
+    obj._finishing_taskids[:] = initial_finishing_taskids
+    obj._complete_taskids[:] = initial_complete_taskids
+
+    obj._update_registry()
+
+    assert expected_new_taskids == obj._new_taskids
+    assert expected_active_taskids == obj._active_taskids
+    assert expected_finishing_taskids == obj._finishing_taskids
+    assert expected_complete_taskids == obj._complete_taskids
+
+##__________________________________________________________________||
