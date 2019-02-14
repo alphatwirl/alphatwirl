744b4ce91c1555ea4fab766f6065dcf1a10f1ae5
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-14 23:05:02 +0000

    add _start_monitor_if_necessary()

--- alphatwirl/progressbar/__init__.py
+++ alphatwirl/progressbar/__init__.py
@@ -23,3 +23,54 @@ def report_progress(report):
     _progress_reporter.report(report)
 
 ##__________________________________________________________________||
+_reporter = None
+_monitor = None
+
+import sys
+import atexit
+
+def _start_monitor_if_necessary():
+    global _reporter
+    global _monitor
+
+    if _reporter:
+        return
+
+    if _monitor:
+        # This shouldn't happen.
+        # But if it does, end the old monitor.
+        _monitor.end()
+        _monitor = None
+
+    presentation = _create_presentation()
+    monitor = BProgressMonitor(presentation=presentation)
+    monitor.begin()
+    _reporter = monitor.create_reporter()
+    _monitor = monitor
+
+    def _end_monitor():
+        global _monitor
+        if _monitor:
+            _monitor.end()
+            _monitor = None
+        _reporter = None
+
+    atexit.register(_end_monitor)
+
+def _create_presentation():
+    if sys.stdout.isatty():
+        return ProgressBar()
+    if is_jupyter_notebook():
+        return ProgressBarJupyter()
+    return ProgressPrint()
+
+def is_jupyter_notebook():
+    try:
+        from IPython import get_ipython
+        if 'IPKernelApp' in get_ipython().config:
+            return True
+    except:
+        pass
+    return False
+
+##__________________________________________________________________||
---
+++ tests/unit/progressbar/test_start.py
@@ -0,0 +1,84 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+from alphatwirl.progressbar import _start_monitor_if_necessary
+import alphatwirl
+
+##__________________________________________________________________||
+@pytest.fixture()
+def mock_atexit(monkeypatch):
+    ret = mock.Mock()
+    monkeypatch.setattr(alphatwirl.progressbar, 'atexit', ret)
+    return ret
+
+@pytest.fixture()
+def mock_presentation(monkeypatch):
+    ret = mock.Mock()
+    return ret
+
+@pytest.fixture(autouse=True)
+def mock_create_presentation(monkeypatch, mock_presentation):
+    ret = mock.Mock()
+    ret.return_value = mock_presentation
+    monkeypatch.setattr(alphatwirl.progressbar, '_create_presentation', ret)
+    return ret
+
+@pytest.fixture()
+def MockBProgressMonitor(monkeypatch):
+    ret = mock.Mock()
+    monkeypatch.setattr(alphatwirl.progressbar, 'BProgressMonitor', ret)
+    return ret
+
+@pytest.fixture(
+    autouse=True,
+    params=[ # were there _reporter and _monitor already there
+        (False, False), (False, True),
+        (True, False), (True, True),
+    ]
+)
+def global_variables(monkeypatch, request):
+    was_reporter, was_monitor = request.param
+
+    if was_reporter:
+        mock_reporter = mock.Mock()
+    else:
+        mock_reporter = None
+
+    if was_monitor:
+        mock_monitor = mock.Mock()
+    else:
+        mock_monitor = None
+
+    monkeypatch.setattr(alphatwirl.progressbar, '_reporter', mock_reporter)
+    monkeypatch.setattr(alphatwirl.progressbar, '_monitor', mock_monitor)
+
+def test_start_monitor_if_necessary(mock_atexit, MockBProgressMonitor, mock_presentation):
+
+    org_reporter = alphatwirl.progressbar._reporter
+    org_monitor = alphatwirl.progressbar._monitor
+
+    _start_monitor_if_necessary()
+
+    if org_reporter:
+        assert alphatwirl.progressbar._reporter is org_reporter
+        assert alphatwirl.progressbar._monitor is org_monitor
+        assert not mock_atexit.register.call_args_list
+        return
+
+    if org_monitor:
+        assert [mock.call()] == org_monitor.end.call_args_list
+
+    assert [mock.call(presentation=mock_presentation)] == MockBProgressMonitor.call_args_list
+    assert [mock.call()] == MockBProgressMonitor().begin.call_args_list
+
+    assert alphatwirl.progressbar._monitor is MockBProgressMonitor()
+    assert alphatwirl.progressbar._reporter is MockBProgressMonitor().create_reporter()
+
+    assert 1 == (len(mock_atexit.register.call_args_list))
+
+##__________________________________________________________________||
