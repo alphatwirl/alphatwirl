6f2d328dd569155ff2cd464886a8c7df91c3941a
Tai Sakuma <tai.sakuma@gmail.com>
2019-03-17 15:25:59 +0000

    change TaskPackage from namedtuple to class and make it callable

--- alphatwirl/concurrently/TaskPackage.py
+++ alphatwirl/concurrently/TaskPackage.py
@@ -1,7 +1,17 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
-import collections
 
 ##__________________________________________________________________||
-TaskPackage = collections.namedtuple('TaskPackage', 'task args kwargs')
+class TaskPackage(object):
+    """A task package
+
+    Note: This class will be replaced with `functools.partial`
+
+    """
+    def __init__(self, task, args, kwargs):
+        self.task = task
+        self.args = args
+        self.kwargs = kwargs
+    def __call__(self):
+        return self.task(*self.args, **self.kwargs)
 
 ##__________________________________________________________________||
