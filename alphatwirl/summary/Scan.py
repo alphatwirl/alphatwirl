5e8f78ed450a765b977291ccd2867cfb211a2434
Tai Sakuma <tai.sakuma@gmail.com>
2019-06-09 13:10:13 +0100

    follow pep 8

--- alphatwirl/summary/Scan.py
+++ alphatwirl/summary/Scan.py
@@ -6,7 +6,7 @@ import copy
 
 ##__________________________________________________________________||
 class Scan(object):
-    def __init__(self, val = None, weight = 1, contents = None):
+    def __init__(self, val=None, weight=1, contents=None):
 
         if contents is not None:
             self.contents = contents
@@ -20,7 +20,7 @@ class Scan(object):
 
     def __add__(self, other):
         contents = self.contents + other.contents
-        return self.__class__(contents = contents)
+        return self.__class__(contents=contents)
 
     def __radd__(self, other):
         # is called with other = 0 when e.g. sum([obj1, obj2])
@@ -29,13 +29,13 @@ class Scan(object):
         raise TypeError('unsupported: {!r} + {!r}'.format(other, self))
 
     def __repr__(self):
-        return '{}(contents = {})'.format(self.__class__.__name__, self.contents)
+        return '{}(contents={})'.format(self.__class__.__name__, self.contents)
 
     def __eq__(self, other):
         return self.contents == other.contents
 
     def __copy__(self):
         contents = list(self.contents)
-        return self.__class__(contents = contents)
+        return self.__class__(contents=contents)
 
 ##__________________________________________________________________||
