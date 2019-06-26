2ef1e1b6caffe409bc5b1000fe7866be0ecec403
Tai Sakuma <tai.sakuma@gmail.com>
2019-06-26 09:17:01 +0100

    test return of query_status_for()

--- tests/unit/concurrently/condor/test_commands.py
+++ tests/unit/concurrently/condor/test_commands.py
@@ -13,20 +13,29 @@ from alphatwirl.concurrently.condor.commands import split_list_into_chunks, quer
 ##__________________________________________________________________||
 @pytest.fixture()
 def mock_try_executing_until_succeed(monkeypatch):
-    ret = mock.MagicMock(name='try_executing_until_succeed')
+    ret = mock.MagicMock()
     module = sys.modules['alphatwirl.concurrently.condor.commands']
     monkeypatch.setattr(module, 'try_executing_until_succeed', ret)
     return ret
 
 def test_query_status_for(mock_try_executing_until_succeed):
-    ids = ['3158174', '3158175', '3158176', '3158177', '3158178', '3158179', '3158180', '3158181', '3158182', '3158183', '3158184', '3158185', '3158186', '3158187', '3158188', '3158189']
-    n_at_a_time = 5
-    query_status_for(ids, n_at_a_time=5)
+    mock_try_executing_until_succeed.side_effect = [
+        ['3158174 2', '3158175 2', '3158176 1'],
+        ['3158177 5', '3158178 1', '3158179 1'],
+        ['3158180 1']
+    ]
+    ids = ['3158174', '3158175', '3158176', '3158177', '3158178', '3158179', '3158180']
     expected = [
-        mock.call(['condor_q', '3158174', '3158175', '3158176', '3158177', '3158178', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
-        mock.call(['condor_q', '3158179', '3158180', '3158181', '3158182', '3158183', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
-        mock.call(['condor_q', '3158184', '3158185', '3158186', '3158187', '3158188', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
-        mock.call(['condor_q', '3158189', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus'])
+        ['3158174', 2], ['3158175', 2], ['3158176', 1],
+        ['3158177', 5], ['3158178', 1], ['3158179', 1],
+        ['3158180', 1]
+    ]
+    assert expected == query_status_for(ids, n_at_a_time=3)
+
+    expected = [
+        mock.call(['condor_q', '3158174', '3158175', '3158176', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
+        mock.call(['condor_q', '3158177', '3158178', '3158179', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
+        mock.call(['condor_q', '3158180', '-format', '%d.', 'ClusterId', '-format', '%d ', 'ProcId', '-format', '%-2s\n', 'JobStatus']),
     ]
     assert expected == mock_try_executing_until_succeed.call_args_list
 
